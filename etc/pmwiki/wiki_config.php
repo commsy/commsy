138238fb6eb9218296707dbab76e86c2138c5883
Iver Jackewitz <Iver.Jackewitz@rrz.uni-hamburg.de>
2008-04-10 08:19:38 +0000

    bugfix: upload password

--- etc/pmwiki/wiki_config.php
+++ etc/pmwiki/wiki_config.php
@@ -1,21 +1,29 @@
 <?php if (!defined('PmWiki')) exit();
 
 include_once('commsy_config.php');
-$WikiTitle = $COMMSY_WIKI_TITLE;
-$DefaultPasswords['admin'] = crypt($COMMSY_ADMIN_PASSWD);
-$EnableUpload = 1;
-$DefaultPasswords['upload'] = crypt($COMMSY_ADMIN_PASSWD);
-$UploadMaxSize = 1000000000;
-if (!empty($COMMSY_EDIT_PASSWD)){
+if ( !empty($COMMSY_WIKI_TITLE) ) {
+   $WikiTitle = $COMMSY_WIKI_TITLE;
+} else {
+   $WikiTitle = 'CommSyWiki';
+}
+if ( !empty($COMMSY_ADMIN_PASSWD) ) {
+   $DefaultPasswords['admin'] = crypt($COMMSY_ADMIN_PASSWD);
+}
+if ( !empty($COMMSY_UPLOAD_PASSWD) ) {
+   $EnableUpload = 1;
+   $DefaultPasswords['upload'] = crypt($COMMSY_UPLOAD_PASSWD);
+   $UploadMaxSize = 1000000000;
+}
+if ( !empty($COMMSY_EDIT_PASSWD) ) {
    $DefaultPasswords['edit'] = crypt($COMMSY_EDIT_PASSWD);
 }
-if (!empty($COMMSY_READ_PASSWD)){
+if ( !empty($COMMSY_READ_PASSWD) ) {
    $DefaultPasswords['read'] = crypt($COMMSY_READ_PASSWD);
 }
-if (!empty($COMMSY_SKIN)){
+if ( !empty($COMMSY_SKIN) ) {
    $Skin = $COMMSY_SKIN;
 }
-if (!empty($COMMSY_LANGUAGE)){
+if ( !empty($COMMSY_LANGUAGE) ) {
    XLPage($COMMSY_LANGUAGE,'PmWikiDe.XLPage');
 }
 ?>
