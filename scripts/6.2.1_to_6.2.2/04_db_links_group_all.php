57b815f6bf18c68492e88a9c9d2ada672cb7a075
Iver Jackewitz <Iver.Jackewitz@rrz.uni-hamburg.de>
2008-11-13 14:21:28 +0000

    group all problem

--- scripts/6.2.1_to_6.2.2/04_db_links_group_all.php
+++ scripts/6.2.1_to_6.2.2/04_db_links_group_all.php
@@ -62,62 +62,66 @@ if ($count_rooms < 1) {
       $row2 = mysql_fetch_row($result2);
       $group_all_id = $row2[0];
 
-      // get all users in project room
-      $query  = "SELECT user.item_id FROM user WHERE user.context_id='".$room_id."' AND user.status > 1 AND user.deletion_date IS NULL AND user.deleter_id IS NULL ORDER BY user.item_id;";
-      $result2 = mysql_query($query);
-      if ( $error = mysql_error() ) {
-         echo ('<hr>'.$error.". QUERY: ".$query.'<hr>');
-      }
-      $row2 = mysql_fetch_row($result2);
-      $user_id = $row2[0];
-      while ($user_id) {
-         // check if user is connected to group all
-         $query  = "SELECT link_items.item_id FROM link_items WHERE
-                    ((link_items.first_item_id='".$group_all_id."' AND
-                     link_items.first_item_type='group' AND
-                     link_items.second_item_id='".$user_id."' AND
-                     link_items.second_item_type='user') OR
-                    (link_items.second_item_id='".$group_all_id."' AND
-                     link_items.second_item_type='group' AND
-                     link_items.first_item_id='".$user_id."' AND
-                     link_items.first_item_type='user')) AND
-                     link_items.deletion_date IS NULL AND
-                     link_items.deleter_id IS NULL;";
-         $result3 = mysql_query($query);
+      if ( !empty($group_all_id) ) {
+
+         // get all users in project room
+         $query  = "SELECT user.item_id FROM user WHERE user.context_id='".$room_id."' AND user.status > 1 AND user.deletion_date IS NULL AND user.deleter_id IS NULL ORDER BY user.item_id;";
+         $result2 = mysql_query($query);
          if ( $error = mysql_error() ) {
             echo ('<hr>'.$error.". QUERY: ".$query.'<hr>');
          }
-         $row3 = mysql_fetch_row($result3);
-         $link_id = $row3[0];
-         if (empty($link_id)) {
-            // insert link between user and group ALL
-            $insert_query = 'INSERT INTO items ( item_id , context_id , type , deleter_id , deletion_date , modification_date )
-                             VALUES ("", '.$room_id.' , "link_item", NULL , NULL, "'.date("Y-m-d H:i:s").'")';
-            mysql_query($insert_query);
-            if ($error = mysql_error()) {
-               echo $error." QUERY: ".$insert_query;
-               $success = false;
-            }
-            $link_id = mysql_insert_id();
-
-            $insert_query = 'INSERT INTO link_items ( item_id, context_id , creator_id , deleter_id ,
-                                                      creation_date , modification_date , deletion_date , first_item_id ,
-                                                      first_item_type , second_item_id , second_item_type )
-                             VALUES ("'.$link_id.'", "'.$room_id.'", "'.$user_id.'", NULL , "'
-                                       .date("Y-m-d H:i:s").'", "'.date("Y-m-d H:i:s").'", NULL , "'.$user_id.'", '
-                                       .'"user", "'.$group_all_id.'", '
-                                       .'"group")';
-            mysql_query($insert_query);
-            if ($error = mysql_error()) {
-               echo '<br />'.$error." QUERY: ".$insert_query.'<br />'."\n";
-               $success = false;
-            }
-            $counter++;
-         }
-
-         // next user
          $row2 = mysql_fetch_row($result2);
          $user_id = $row2[0];
+         while ($user_id) {
+            // check if user is connected to group all
+            $query  = "SELECT link_items.item_id FROM link_items WHERE
+                       ((link_items.first_item_id='".$group_all_id."' AND
+                        link_items.first_item_type='group' AND
+                        link_items.second_item_id='".$user_id."' AND
+                        link_items.second_item_type='user') OR
+                       (link_items.second_item_id='".$group_all_id."' AND
+                        link_items.second_item_type='group' AND
+                        link_items.first_item_id='".$user_id."' AND
+                        link_items.first_item_type='user')) AND
+                        link_items.deletion_date IS NULL AND
+                        link_items.deleter_id IS NULL;";
+            $result3 = mysql_query($query);
+            if ( $error = mysql_error() ) {
+               echo ('<hr>'.$error.". QUERY: ".$query.'<hr>');
+            }
+            $row3 = mysql_fetch_row($result3);
+            $link_id = $row3[0];
+            if (empty($link_id)) {
+               // insert link between user and group ALL
+               $insert_query = 'INSERT INTO items ( context_id , type , deleter_id , deletion_date , modification_date )
+                                VALUES ('.$room_id.' , "link_item", NULL , NULL, "'.date("Y-m-d H:i:s").'")';
+               mysql_query($insert_query);
+               if ($error = mysql_error()) {
+                  echo $error." QUERY: ".$insert_query;
+                  $success = false;
+               } else {
+                  $link_id = mysql_insert_id();
+
+                  $insert_query = 'INSERT INTO link_items ( item_id, context_id , creator_id , deleter_id ,
+                                                            creation_date , modification_date , deletion_date , first_item_id ,
+                                                            first_item_type , second_item_id , second_item_type )
+                                   VALUES ("'.$link_id.'", "'.$room_id.'", "'.$user_id.'", NULL , "'
+                                             .date("Y-m-d H:i:s").'", "'.date("Y-m-d H:i:s").'", NULL , "'.$user_id.'", '
+                                             .'"user", "'.$group_all_id.'", '
+                                             .'"group")';
+                  mysql_query($insert_query);
+                  if ($error = mysql_error()) {
+                     echo '<br />'.$error." QUERY: ".$insert_query.'<br />'."\n";
+                     $success = false;
+                  }
+               }
+               $counter++;
+            }
+
+            // next user
+            $row2 = mysql_fetch_row($result2);
+            $user_id = $row2[0];
+         }
       }
 
       // next room
