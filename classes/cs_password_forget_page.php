22d3331827c20b9f15debcfd6646bc8440510c8d
Iver Jackewitz <Iver.Jackewitz@rrz.uni-hamburg.de>
2008-12-15 13:17:17 +0000

    bugfix: this->_ fehlte

--- classes/cs_password_forget_page.php
+++ classes/cs_password_forget_page.php
@@ -34,7 +34,7 @@ class cs_password_forget_page extends cs_left_page {
 
       $class_params= array();
       $class_params['environment'] = $this->_environment;
-      $form = $class_factory->getClass(PASSWORD_FORGET_FORM,$class_params);
+      $form = $this->_class_factory->getClass(PASSWORD_FORGET_FORM,$class_params);
       unset($class_params);
       // Load form data from postvars
       if ( !empty($this->_post_vars) ) {
