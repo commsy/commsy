e8a95254df514e7aa74eb8e6b7dc932262563223
Iver Jackewitz <Iver.Jackewitz@rrz.uni-hamburg.de>
2009-03-06 14:46:34 +0000

    bugfix fck in ie 7 under xp

--- classes/cs_html_textarea.php
+++ classes/cs_html_textarea.php
@@ -24,61 +24,74 @@
 
 class cs_html_textarea {
 
-	function getAsHTML ($name,$value='',$hsize='',$html_status=1,$tabindex='',$vsize='',$no_discussion = true) {
+   function getAsHTML ($name,$value='',$hsize='',$html_status=1,$tabindex='',$vsize='',$no_discussion = true) {
 
-		// some configurations
-		global $c_commsy_url_path;
-		if ( mb_strlen($c_commsy_url_path) > 1 and mb_substr($c_commsy_url_path,mb_strlen($c_commsy_url_path)-1) == '/') {
-		   $c_commsy_url_path = mb_substr($c_commsy_url_path,0,mb_strlen($c_commsy_url_path)-1);
-		}
-		global $c_fckeditor_url_path;
-		if ( mb_strlen($c_fckeditor_url_path) > 1 and mb_substr($c_fckeditor_url_path,mb_strlen($c_fckeditor_url_path)-1) == '/') {
-		   $c_fckeditor_url_path = mb_substr($c_fckeditor_url_path,0,mb_strlen($c_fckeditor_url_path)-1);
-		}
-		global $c_fckeditor_file_path;
-		if ( mb_strlen($c_fckeditor_file_path) > 1 and mb_substr($c_fckeditor_file_path,mb_strlen($c_fckeditor_file_path)-1) == '/') {
-		   $c_fckeditor_file_path = mb_substr($c_fckeditor_file_path,0,mb_strlen($c_fckeditor_file_path)-1);
-		}
+      // some configurations
+      global $c_commsy_url_path;
+      if ( mb_strlen($c_commsy_url_path) > 1 and mb_substr($c_commsy_url_path,mb_strlen($c_commsy_url_path)-1) == '/') {
+         $c_commsy_url_path = mb_substr($c_commsy_url_path,0,mb_strlen($c_commsy_url_path)-1);
+      }
+      global $c_fckeditor_url_path;
+      if ( mb_strlen($c_fckeditor_url_path) > 1 and mb_substr($c_fckeditor_url_path,mb_strlen($c_fckeditor_url_path)-1) == '/') {
+         $c_fckeditor_url_path = mb_substr($c_fckeditor_url_path,0,mb_strlen($c_fckeditor_url_path)-1);
+      }
+      global $c_fckeditor_file_path;
+      if ( mb_strlen($c_fckeditor_file_path) > 1 and mb_substr($c_fckeditor_file_path,mb_strlen($c_fckeditor_file_path)-1) == '/') {
+         $c_fckeditor_file_path = mb_substr($c_fckeditor_file_path,0,mb_strlen($c_fckeditor_file_path)-1);
+      }
 
-		// value translations
-		$value = str_replace('<!-- KFC TEXT -->','',$value);
-		$value = '<!-- KFC TEXT -->'.$value.'<!-- KFC TEXT -->';
-		// this is for migration of texts not insert with FCKeditor
-		$value = str_replace("\n\n",'<br/><br/>',$value);
+      // value translations
+      $value = str_replace('<!-- KFC TEXT -->','',$value);
+      $value = '<!-- KFC TEXT -->'.$value.'<!-- KFC TEXT -->';
+      // this is for migration of texts not insert with FCKeditor
+      $value = str_replace("\n\n",'<br/><br/>',$value);
 
-		// now the fckeditor object
-		include_once($c_fckeditor_file_path.'/fckeditor.php') ;
-		$oFCKeditor = new FCKeditor($name);
-		$oFCKeditor->BasePath = $c_fckeditor_url_path.'/';
-		$oFCKeditor->Config["CustomConfigurationsPath"] = $c_commsy_url_path.'/javascript/CommSyFCKEditorConfig.js';
-		$oFCKeditor->Value    = $value;
-		if ($no_discussion){
-           if ( empty($vsize) ) {
-			   $oFCKeditor->Width    = '504px';
-		   } else {
-			   $oFCKeditor->Width   = (string)round($vsize*8.5,0).'px';
-		   }
-        }
-        global $environment;
-        $current_browser = strtolower($environment->getCurrentBrowser());
-        $current_browser_version = $environment->getCurrentBrowserVersion();
-        $context_item = $environment->getCurrentContextItem();
-        if (!$context_item->isDesign7() and $current_browser == 'msie' and (strstr($current_browser_version,'5.') or (strstr($current_browser_version,'6.'))) ){
-           $oFCKeditor->Width = ' 504px';
-        }else{
-           $oFCKeditor->Width = '98%';
-        }
-		$oFCKeditor->Height   = round($hsize*13.5,0);
-		$oFCKeditor->TabIndex = $tabindex;
-		if ( $html_status == '2' ) {
-		   $oFCKeditor->ToolbarSet = 'MinCommSy';
-		} elseif ($html_status == 'homepage') {
-		   $oFCKeditor->ToolbarSet = 'homepage';
-		} else {
-		   $oFCKeditor->ToolbarSet = 'CommSy';
-		}
-   	$retour = $oFCKeditor->CreateHtml() ;
-		return $retour;
-	}
+      // now the fckeditor object
+      include_once($c_fckeditor_file_path.'/fckeditor.php') ;
+      $oFCKeditor = new FCKeditor($name);
+      $oFCKeditor->BasePath = $c_fckeditor_url_path.'/';
+      $oFCKeditor->Config["CustomConfigurationsPath"] = $c_commsy_url_path.'/javascript/CommSyFCKEditorConfig.js';
+      $oFCKeditor->Value    = $value;
+
+      // TBD
+      // das wird doch eh unten überschrieben
+      // kann das nicht weg?
+      // ij 06.03.2009
+      if ( $no_discussion ) {
+         if ( empty($vsize) ) {
+            $oFCKeditor->Width   = '504px';
+         } else {
+            $oFCKeditor->Width   = (string)round($vsize*8.5,0).'px';
+         }
+      }
+      // TBD
+
+      global $environment;
+      $current_browser = strtolower($environment->getCurrentBrowser());
+      $current_browser_version = $environment->getCurrentBrowserVersion();
+      $context_item = $environment->getCurrentContextItem();
+      if ( !$context_item->isDesign7()
+           and $current_browser == 'msie'
+           and ( strstr($current_browser_version,'5.')
+                 or strstr($current_browser_version,'6.')
+                 or strstr($current_browser_version,'7.')
+               )
+         ) {
+         $oFCKeditor->Width = '504px';
+      } else {
+         $oFCKeditor->Width = '98%';
+      }
+      $oFCKeditor->Height   = round($hsize*13.5,0);
+      $oFCKeditor->TabIndex = $tabindex;
+      if ( $html_status == '2' ) {
+         $oFCKeditor->ToolbarSet = 'MinCommSy';
+      } elseif ($html_status == 'homepage') {
+         $oFCKeditor->ToolbarSet = 'homepage';
+      } else {
+         $oFCKeditor->ToolbarSet = 'CommSy';
+      }
+      $retour = $oFCKeditor->CreateHtml() ;
+      return LF.$retour.LF;
+   }
 }
 ?>
