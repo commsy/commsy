e9adb26d4a852e6f605635f4303f28529e308c38
Johannes Schultze <schultze@effective-webwork.de>
2009-03-26 09:17:35 +0000

    Korrektur Links Raumliste "soap.php" -> "commsy.php" im generierten Link

--- classes/cs_connection_soap.php
+++ classes/cs_connection_soap.php
@@ -1649,7 +1649,7 @@ class cs_connection_soap {
                      $retour .= '         <id><![CDATA['.$item->getItemID().']]></id>'.LF;
                      global $c_commsy_domain, $c_commsy_url_path;
                      include_once('functions/curl_functions.php');
-                     $retour .= '         <url><![CDATA['.$c_commsy_domain.$c_commsy_url_path.'/'._curl(false,$item->getItemID(),'home','index',array()).']]></url>'.LF;
+                     $retour .= '         <url><![CDATA['.$c_commsy_domain.$c_commsy_url_path.'/'.str_replace('soap.php', 'commsy.php', _curl(false,$item->getItemID(),'home','index',array())).']]></url>'.LF;
                   }
                   $retour .= '      </item>'.LF;
                   $item = $list->getNext();
