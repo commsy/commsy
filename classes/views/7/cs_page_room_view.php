1970a0df9d4f0a8dd9fea0d51920396c6490181a
Matthias Finck <finck@effective-webwork.de>
2009-07-17 13:53:24 +0000

    wfs test

--- classes/views/7/cs_page_room_view.php
+++ classes/views/7/cs_page_room_view.php
@@ -1644,7 +1644,7 @@ class cs_page_room_view extends cs_page_view {
             unset($img);
          }
       }
-      global $web_feedback_url;
+/*      global $web_feedback_url;
       global $web_feedback_server_url;
       global $use_web_feedback_system;
       global $web_feedback_room_id_array;
@@ -1660,23 +1660,8 @@ class cs_page_room_view extends cs_page_view {
          $html .= '<script src="'.$web_feedback_url.'lib/feedback.js" type="text/javascript"></script>'.LF;
          $html .= '<a id="wfs_button" href="javascript:feedbackPage(\''.$web_feedback_url.'\',\''.$web_feedback_server_url.'\');"><img style="vertical-align:bottom;" id="wfs_feedback_gif" src="images/wfs.png" title="Click here to open a window with a feedback system." ></img></a>&nbsp;'.LF;
          $html .= '<!-- End of Code for WFS-Feedback -->'.LF;
-      }
+      }*/
 
-
-/*$wfs_js_url="http://wfs.conftool.net/wfs-sandbox/wfs";
-$wfs_server_url="http://wfs.conftool.net/wfs-server/";
-
-$html .="<link href=\"$wfs_js_url/styles/wfs_button.css\" rel=\"stylesheet\" type=\"text/css\">\n\n";
-
-$html .="<script src=\"$wfs_js_url/lib/jquery-latest.js\" type=\"text/javascript\" ></script>\n";
-$html .="<script src=\"$wfs_js_url/lib/jquery.field.js\" type=\"text/javascript\"></script>\n";
-$html .="<script src=\"$wfs_js_url/lib/jquery.dimensions.js\" type=\"text/javascript\"></script>\n";
-$html .="<script src=\"$wfs_js_url/lib/jquery.tooltip.js\" type=\"text/javascript\"></script>\n";
-$html .="<script src=\"$wfs_js_url/lib/feedback.js\" type=\"text/javascript\"></script>\n";
-
-$html .="<div id=\"wfs_button\" class=\"wfs_button\">\n";
-$html .="<a href=\"javascript:feedbackPage('$wfs_js_url/','$wfs_server_url');\"><img id=\"wfs_feedback_gif\" src=\"$wfs_js_url/includes/feedback.gif\" title=\"Click here to open a window with a feedback system.\" ></img></a>\n";
-$html .="</div>\n";*/
 
 
       return $html;
