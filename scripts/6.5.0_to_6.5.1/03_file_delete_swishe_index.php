656b161d2ca1530c960a1d05241d369ac84f60db
Iver Jackewitz <Iver.Jackewitz@rrz.uni-hamburg.de>
2009-09-01 12:28:18 +0000

    split swish scripts

--- scripts/6.5.0_to_6.5.1/03_file_delete_swishe_index.php
+++ scripts/6.5.0_to_6.5.1/03_file_delete_swishe_index.php
@@ -56,30 +56,6 @@ function _rm_swishe_index($dirname) {
    }
 }
 
-function _get_folder_array ($dirname) {
-   $retour = array();
-   if ( $dirHandle = opendir($dirname) ) {
-      $old_cwd = getcwd();
-      chdir($dirname);
-
-      while ($file = readdir($dirHandle)){
-         if ($file == '.' || $file == '..') continue;
-         if ( is_dir($file)
-              and is_numeric($file)
-            ) {
-            $retour[$file] = _get_folder_array($file);
-         }
-      }
-
-      closedir($dirHandle);
-      chdir($old_cwd);
-      if ( empty($retour) ) {
-         $retour = '';
-      }
-      return $retour;
-   }
-}
-
 set_time_limit(0);
 
 // init $success
@@ -88,29 +64,4 @@ $success = true;
 // headline
 $this->_flushHTML('file: delete swish-e index'.BRLF);
 $success = _rm_swishe_index('var/');
-
-global $c_indexing;
-if ( isset($c_indexing) and $c_indexing ) {
-   $this->_flushHTML('file: create swish-e index'.BRLF);
-   $folder_array = _get_folder_array('var/');
-   $folder_array2 = array();
-   foreach ( $folder_array as $portal_id => $room_id_array ) {
-      foreach ( $room_id_array as $room_id => $value ) {
-         $folder_array2[] = 'var/'.$portal_id.'/'.$room_id.'/';
-      }
-   }
-   $count = count($folder_array2);
-   if ( $count < 1 ) {
-      // nothing to do
-      $this->_flushHTML('nothing to do.'.BRLF);
-   } else {
-      // something to do
-      $this->_initProgressBar($count);
-      $ft_manager = $this->_environment->getFTSearchManager();
-      foreach ($folder_array2 as $folder) {
-         $ft_manager->buildFTIndexForIndexBase($folder);
-         $this->_updateProgressBar($count);
-      }
-   }
-}
 ?>
