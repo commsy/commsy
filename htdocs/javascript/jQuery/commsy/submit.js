3e41b8a14af285dedf533768cb0bd1e215afc720
Johannes Schultze <schultze@effective-webwork.de>
2009-09-21 14:31:35 +0000

    jQuery

--- htdocs/javascript/jQuery/commsy/submit.js
+++ htdocs/javascript/jQuery/commsy/submit.js
@@ -1,25 +1,49 @@
 jQuery(document).ready(function() {
-   jQuery("select").change(function () {
-      if(this.id == 'submit_form'){
+   //jQuery("select").change(function () {
+   //   if(this.id == 'submit_form'){
+   //      jQuery(this).parents("form").map(function () {
+   //         this.submit();
+   //      });
+   //   }
+   //});
+   jQuery("select[id='submit_form']").each(function(i) {
+      jQuery(this).change(function () {
          jQuery(this).parents("form").map(function () {
             this.submit();
          });
+      })
+   });
+   
+   //jQuery("input").change(function () {
+   //   if((this.type == 'checkbox') && (this.id == 'submit_form')){
+   //      jQuery(this).parents("form").map(function () {
+   //         this.submit();
+   //      });
+   //   }
+   //});
+   jQuery("input[id='submit_form']").each(function(i) {
+      if(this.type == 'checkbox'){
+         jQuery(this).change(function () {
+            jQuery(this).parents("form").map(function () {
+               this.submit();
+            });
+         })
       }
    });
    
-   jQuery("input").change(function () {
-      if((this.type == 'checkbox') && (this.id == 'submit_form')){
+   //jQuery("a").click(function () {
+   //   if(this.id == 'submit_form'){
+   //      jQuery(this).parents("form").map(function () {
+   //         this.submit();
+   //      });
+   //   }
+   //});
+   jQuery("a[id='submit_form']").each(function(i) {
+      jQuery(this).click(function () {
          jQuery(this).parents("form").map(function () {
             this.submit();
          });
-      }
+      })
    });
    
-   jQuery("a").click(function () {
-      if(this.id == 'submit_form'){
-         jQuery(this).parents("form").map(function () {
-            this.submit();
-         });
-      }
-   });
 });
