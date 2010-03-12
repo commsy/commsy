49d86864c0d2d6d9882410ff36d395d35e6a1545
Matthias Finck <finck@effective-webwork.de>
2010-03-12 10:08:18 +0000

    Vorbereitung von Portlettechnologie im pers�nlichen Raum

--- classes/views/7/cs_page_view.php
+++ classes/views/7/cs_page_view.php
@@ -594,10 +594,11 @@ class cs_page_view extends cs_view {
 
          $retour .= '<link rel="stylesheet" href="javascript/jQuery/src/ajaxrotator/jquery.ctrotator.css" type="text/css" media="all" />'.LF;
          $retour .= '<script type="text/javascript" src="javascript/jQuery/ajaxrotator/src/jquery.ctrotator.bridge.flickr.js"></script>'.LF;
-         $retour .= '<script type="text/javascript" src="javascript/jQuery/ajaxrotator/src/jquery.ctrotator.bridge.li.js"></script>'.LF;
+#         $retour .= '<script type="text/javascript" src="javascript/jQuery/ajaxrotator/src/jquery.ctrotator.bridge.rss20.js"></script>'.LF;
          $retour .= '<script type="text/javascript" src="javascript/jQuery/ajaxrotator/src/jquery.ctrotator.js"></script>'.LF;
-#         $retour .= '   <script type="text/javascript" src="javascript/jQuery/flickr/flickr.js"></script>'.LF;
-#         $retour .= '   <script type="text/javascript" src="javascript/jQuery/flickr/flickrpreview.js"></script>'.LF;
+
+#         $retour .= '<script type="text/javascript" src="javascript/jQuery/jquery.flip.0-9-9/jquery.flip.min.js"></script>'.LF;
+
       }
       /*********/
       }
