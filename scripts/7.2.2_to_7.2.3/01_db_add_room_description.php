b2e9c0322a8576dae723009dd0e1daeac1c5be8f
Johannes Schultze <schultze@effective-webwork.de>
2010-04-27 12:46:29 +0000

    Raumbeschreibung

--- scripts/7.2.2_to_7.2.3/01_db_add_room_description.php
+++ scripts/7.2.2_to_7.2.3/01_db_add_room_description.php
@@ -91,8 +91,58 @@ while ( $portal ) {
    	}
    	$room = $room_list->getNext();
    }
+   
+   // Backup-Tabellen
+   $zzz_room_manager = $this->_environment->getZzzRoomManager();
+   $zzz_room_manager->setContextLimit($portal->getItemID());
+   $zzz_room_manager->select();
+   $room_list = $zzz_room_manager->get();
+   $room = $room_list->getFirst();
+   while ( $room ) {
+      $description_new = $room->getDescription();
+      if(empty($description_new)){
+         $description_array = $room->getDescriptionArray();
+         $language = $room->getLanguage();
+         $description_text = '';
+         if(isset($description_array[strtoupper($language)]) and !empty($description_array[strtoupper($language)])){
+            $description_text = $description_array[strtoupper($language)];
+         } else {
+            foreach($description_array as $language_key => $description){
+               if($language_key != $language){
+                  if(isset($description_array[strtoupper($language_key)]) and !empty($description_array[strtoupper($language_key)])){
+                     $description_text = $description_array[strtoupper($language_key)];
+                  }
+               }
+            }
+         }
+         
+         $values = array();
+         preg_match('~<!-- KFC TEXT ([a-z0-9]*) -->~u',$description_text,$values);
+         if ( !empty($values[1]) ) {
+            $hash = $values[1];
+            $description_text = str_replace('<!-- KFC TEXT '.$hash.' -->','',$description_text);
+            
+            if(mb_strlen($description_text) > 1000){
+               $description_text = mb_substr($description_text, 0, 1000);
+            }
+            
+            $description_text = '<!-- KFC TEXT '.$hash.' -->'.$description_text.'<!-- KFC TEXT '.$hash.' -->';
+         } else {
+            if(mb_strlen($description_text) > 1000){
+              $description_text = mb_substr($description_text, 0, 1000);
+            }
+         }
+
+         $room->setDescription($description_text);
+         $room->save();
+      }
+      $room = $room_list->getNext();
+   }
+   
    $portal = $portal_list->getNext();
 }
+
+
 ini_set("memory_limit",$old_memory);
 
 $this->_flushHTML(BRLF);
