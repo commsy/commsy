95b9e6df1333303ed1f8e3b28796fbe54d6b59a6
Iver Jackewitz <Iver.Jackewitz@rrz.uni-hamburg.de>
2010-10-25 10:02:20 +0000

    bug: 3094707 -> readfile has problems with large files -> change to fopen

--- pages/material_getfile.php
+++ pages/material_getfile.php
@@ -22,6 +22,17 @@
 //    You have received a copy of the GNU General Public License
 //    along with CommSy.
 
+function return_bytes ($size_str)
+{
+    switch (substr ($size_str, -1))
+    {
+        case 'M': case 'm': return (int)$size_str * 1048576;
+        case 'K': case 'k': return (int)$size_str * 1024;
+        case 'G': case 'g': return (int)$size_str * 1073741824;
+        default: return $size_str;
+    }
+}
+
 // Get the translator object
 $translator = $environment->getTranslationObject();
 
@@ -90,11 +101,57 @@ if ( !empty($_GET['iid']) ) {
 
          $file->setContextID($environment->getCurrentContextID());
          if ( $file->isOnDisk() ) {
-            header('Content-type: '.$file->getMime());
+            // old style: problems with large files
+            #header('Content-type: '.$file->getMime());
             // der IE kann damit nicht bei https umgehen, alle anderen Browser schon
             // header('Pragma: no-cache');
-            header('Expires: 0');
-            @readfile($file->getDiskFileName());
+            #header('Expires: 0');
+            #@readfile($file->getDiskFileName());
+
+            $realpath = $file->getDiskFileName();
+            $mtime = ($mtime = filemtime($realpath)) ? $mtime : gmtime();
+            $size = intval(sprintf("%u", filesize($realpath)));
+            // Maybe the problem is we are running into PHPs own memory limit, so:
+            if (intval($size + 1) > return_bytes(ini_get('memory_limit')) && intval($size * 1.5) <= 1073741824) { //Not higher than 1GB
+               ini_set('memory_limit', intval($size * 1.5));
+            }
+            // Maybe the problem is Apache is trying to compress the output, so:
+            @apache_setenv('no-gzip', 1);
+            @ini_set('zlib.output_compression', 0);
+            // Maybe the client doesn't know what to do with the output so send a bunch of these headers:
+            header("Content-Type: application/force-download");
+            header('Content-Type: application/octet-stream');
+            header('Content-Type: '.$file->getMime());
+            if (strstr($_SERVER["HTTP_USER_AGENT"], "MSIE") != false) {
+               header("Content-Disposition: attachment; filename=\"" . urlencode($file->getDisplayName()) . '"; modification-date="' . date('r', $mtime) . '";');
+            } else {
+               header("Content-Disposition: attachment; filename=\"" . $file->getDisplayName() . '"; modification-date="' . date('r', $mtime) . '";');
+            }
+            // Set the length so the browser can set the download timers
+            // PROBLEMS at downloading PDFs
+            #header("Content-Length: " . (string)$size);
+            // If it's a large file we don't want the script to timeout, so:
+            set_time_limit(300);
+            // If it's a large file, readfile might not be able to do it in one go, so:
+            $chunksize = 1 * (1024 * 1024); // how many bytes per chunk
+            if ($size > $chunksize) {
+               $handle = fopen($realpath, 'rb');
+               $buffer = '';
+               while (!feof($handle)) {
+                  $buffer = fread($handle, $chunksize);
+                  echo($buffer);
+                  ob_flush();
+                  flush();
+               }
+               fclose($handle);
+            } else {
+               @readfile($realpath);
+            }
+            // Exit successfully. We could just let the script exit
+            // normally at the bottom of the page, but then blank lines
+            // after the close of the script code would potentially cause
+            // problems after the file download.
+            exit();
          } else {
             include_once('functions/error_functions.php');
             trigger_error("material_getfile: File ".$file->getDiskFileName()." does not seem to be on disk
