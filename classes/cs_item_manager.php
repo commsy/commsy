80cb90bbc88ea3d5be93e6a1ee96b94397b1dffd
Johannes Schultze <schultze@effective-webwork.de>
2011-08-02 07:27:37 +0000

    Bugfix

--- classes/cs_item_manager.php
+++ classes/cs_item_manager.php
@@ -553,6 +553,7 @@ class cs_item_manager extends cs_manager {
            $query .= ' AND '.$this->addDatabasePrefix('items').'.type != "section"';
            $query .= ' AND '.$this->addDatabasePrefix('items').'.type != "step"';
            $query .= ' AND '.$this->addDatabasePrefix('items').'.type != "user"';
+           $query .= ' AND '.$this->addDatabasePrefix('items').'.type != "assessments"';
            if (isset($this->_age_limit)) {
               $query .= ' AND '.$this->addDatabasePrefix('items').'.modification_date > DATE_SUB(CURRENT_DATE,interval '.encode(AS_DB,$this->_age_limit).' day)';
            }
