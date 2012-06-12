8523823228ec0c2eba229804b02da686bbc653b9
Christoph Schönfeld <schoenfeld@effective-webwork.de>
2012-06-12 13:22:37 +0000

    *** empty log message ***

--- scripts/indexing/search_index.php
+++ scripts/indexing/search_index.php
@@ -19,6 +19,9 @@
 			
 			ini_set('max_execution_time', 0);
 			ini_set('memory_limit', -1);
+			ini_set('mysql.connect_timeout', -1);
+			ini_set('default_socket_timeout', -1);
+				
 			error_reporting(E_ALL | E_STRICT);
 			
 			mysql_connect($DB_Hostname, $DB_Username, $DB_Password);
