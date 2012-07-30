025fa85c84655da33e2e1c1df5dbbea0d8882f8c
Matthias Finck <finck@effective-webwork.de>
2012-07-30 22:00:37 +0000

    *** empty log message ***

--- htdocs/javascript/commsy8_dojo/commsy/ckeditor.js
+++ htdocs/javascript/commsy8_dojo/commsy/ckeditor.js
@@ -10,7 +10,7 @@ define([	"dojo/_base/declare",
 	return declare(BaseClass, {
 		instance:	null,
 		node:		null,
-		
+
 		// TODO: multilanguage support
 		options: {
 			language: 'de',
@@ -20,46 +20,45 @@ define([	"dojo/_base/declare",
 			dialog_startupFocusTab: false,
 			resize_enabled: true,
 			resize_maxWidth: '100%',
+			height:'100px',
 			enterMode: CKEDITOR.ENTER_BR,
 			shiftEnterMode: CKEDITOR.ENTER_P,
 			//extraPlugins: 'CommSyImages,CommSyMDO',
 			toolbar: [
-			    ['Cut', 'Copy', 'Paste', 'PasteFromWord', '-', 'Undo', 'Redo', '-', 'Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', 'SpecialChar', '-', 'NumberedList', 'BulletedList', 'Outdent', 'Indent', 'Blockquote', '-', 'TextColor', 'BGColor', '-', 'RemoveFormat']
+			    ['Cut', 'Copy', 'Paste', 'PasteFromWord', '-', 'Undo', 'Redo', '-', 'Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', 'SpecialChar', '-', 'NumberedList', 'BulletedList', 'Outdent', 'Indent', 'Blockquote', '-', 'TextColor', 'BGColor', '-', 'RemoveFormat','-','Maximize', 'Preview']
 			    ,'/',
-			    ['Format', 'Font', 'FontSize', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'Link', 'Unlink', '-', 'Table', 'HorizontalRule', 'Smiley'],
-			    ,'/',
-			    ['Maximize', 'Preview', 'About', '-', 'Image']
+			    ['Format', 'Font', 'FontSize', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'Link', 'Unlink', '-', 'Table', 'HorizontalRule', 'Smiley','-', 'Image','About']
 			]
 		},
-		
+
 		constructor: function(options) {
 			options = options || {};
 			declare.safeMixin(this, options);
 		},
-		
+
 		create: function(node) {
 			this.node = node;
-			
+
 			/* create instance for node */
 			// get id of this object and create a hidden input field beside
 			// the id determs the form_data[]-key
 			// this will later on get the editors content, when the form is submited
 			var id = domAttr.get(node, "id");
-			
+
 			var hiddenNode = domConstruct.create('input');
 			domAttr.set(hiddenNode, "type", "hidden");
 			domAttr.set(hiddenNode, "name", "form_data[" + id + "]");
 			domConstruct.place(hiddenNode, node, "after");
-			
+
 			var data = node.innerHTML;
 			node.innerHTML = "";
-			
+
 			this.instance = CKEDITOR.appendTo(node, this.options, data);
-			
+
 			// get the form this editor belongs to
 			var nodeList =  new dojo.NodeList(node);
 			var formNode = nodeList.parents("form")[0];
-			
+
 			// on form submit, attach editor content to hidden input
 			if(formNode) {
 				On(formNode, "submit", Lang.hitch(this, function(event) {
@@ -67,15 +66,15 @@ define([	"dojo/_base/declare",
 				}));
 			}
 		},
-		
+
 		getInstance: function() {
 			return this.instance;
 		},
-		
+
 		getNode: function() {
 			return this.node;
 		},
-		
+
 		destroy: function() {
 			if(this.instance) this.instance.destroy();
 		}
