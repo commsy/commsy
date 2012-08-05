e9270cdde18c51e23edcfe7ff20d00ef8798e734
Johannes Schultze <schultze@effective-webwork.de>
2012-08-05 23:59:58 +0000

    *** empty log message ***

--- htdocs/javascript/commsy8_dojo/libs/ckeditor/plugins/about/dialogs/about.js
+++ htdocs/javascript/commsy8_dojo/libs/ckeditor/plugins/about/dialogs/about.js
@@ -3,4 +3,115 @@ Copyright (c) 2003-2012, CKSource - Frederico Knabben. All rights reserved.
 For licensing, see LICENSE.html or http://ckeditor.com/license
 */
 
-CKEDITOR.dialog.add('about',function(a){var b=a.lang.about;return{title:CKEDITOR.env.ie?b.dlgTitle:b.title,minWidth:390,minHeight:230,contents:[{id:'tab1',label:'',title:'',expand:true,padding:0,elements:[{type:'html',html:'<style type="text/css">.cke_about_container{color:#000 !important;padding:10px 10px 0;margin-top:5px}.cke_about_container p{margin: 0 0 10px;}.cke_about_container .cke_about_logo{height:81px;background-color:#fff;background-image:url('+CKEDITOR.plugins.get('about').path+'dialogs/logo_ckeditor.png);'+'background-position:center; '+'background-repeat:no-repeat;'+'margin-bottom:10px;'+'}'+'.cke_about_container a'+'{'+'cursor:pointer !important;'+'color:blue !important;'+'text-decoration:underline !important;'+'}'+'</style>'+'<div class="cke_about_container">'+'<div class="cke_about_logo"></div>'+'<p>'+'CKEditor '+CKEDITOR.version+' (revision '+CKEDITOR.revision+')<br>'+'<a href="http://ckeditor.com/">http://ckeditor.com</a>'+'</p>'+'<p>'+b.help.replace('$1','<a href="http://docs.cksource.com/CKEditor_3.x/Users_Guide/Quick_Reference">'+b.userGuide+'</a>')+'</p>'+'<p>'+b.moreInfo+'<br>'+'<a href="http://ckeditor.com/license">http://ckeditor.com/license</a>'+'</p>'+'<p>'+b.copy.replace('$1','<a href="http://cksource.com/">CKSource</a> - Frederico Knabben')+'</p>'+'</div>'}]}],buttons:[CKEDITOR.dialog.cancelButton]};});
+CKEDITOR.dialog.add( 'about', function( editor )
+{
+	var lang = editor.lang.about;
+
+	return {
+		title : 'CommSy Formatierungsmöglichkeiten',
+		minWidth : 950,
+		minHeight : 550,
+		contents : [
+			{
+				id : 'tab1',
+				label : '',
+				title : '',
+				expand : false,
+				padding : 0,
+				elements :
+				[
+					{
+						type : 'html',
+						html :
+							'<table>'+
+							'<tr>'+
+							'<td>'+
+							'<span style="font-weight: bold;">Listen:</span> <span style="font-weight: normal;"># für numerierte, - für unnumerierte Listen</span><br>'+
+							'<span style="font-weight: bold;">Auszeichnungen:</span> <span style="font-weight: normal;">*text* wird zu </span> <span style="font-weight: bold;">text</span>, <span style="font-weight: normal;">_text_ wird zu </span><span style="font-weight: normal; font-style: italic;">text</span><br>'+
+							'<span style="font-weight: bold;">Überschriften: </span><span style="font-weight: normal;">!Text,!!Text oder !!!Text erzeugt Überschiften</span><br>'+
+							'<span style="font-weight: bold;">Trennlinie: </span><span style="font-weight: normal;">--- für eine horizontale Linie</span>'+
+							'<br><hr><br><div class="bold" style="padding-bottom: 5px;">Einbettung von Medien:</div><ol style="font-weight: normal;"><li>Suchen Sie bitte die zum Medium passende Zeile (siehe unten),</li><li> kopieren diese,</li><li>fügen die Zeile in den Beitrag ein und</li><li>ersetzen die GROSSBUCHSTABEN mit eigenen Angaben.</li></ol>'+
+
+							'<span style="font-weight: bold;">Links:</span>'+
+							'<ul style="font-weight: normal; padding-left: 15px; margin: 0px;">'+
+							'    <li>(:link ZIEL text=TEXT:) für Hyperlinks</li>'+
+							'    <li>(:item NUMMER text=TEXT:) für Verweise auf Einträge in CommSy</li>'+
+							'    <li>(:file DATEINAME text=TEXT:) für hochgeladene Dateien</li>'+
+							'    <li>Argumente:'+
+							'    <ul>'+
+							'        <li>newwin: öffnet beim Klickens ein neues Browserfenster</li>'+
+							'        <li>text=TEXT: Beschriftung des Links</li>'+
+							'        <li>text=\'LANGER TEXT\': mehr als ein Wort Beschriftung</li>'+
+							'    </ul>'+
+							'    </li>'+
+							'    <li>Bsp: (:file datei.pdf text=\'Meine Datei\' newwin:)</li>'+
+							'</ul><br><span style="font-weight: bold;">Bilder:</span>'+
+							'<ul style="font-weight: normal; padding-left: 15px; margin: 0px;">'+
+							'    <li>(:image BILDNAME alt=TEXT rfloat width=ZAHL:)</li>'+
+							'    <li>Argumente:'+
+							'    <ul>'+
+							'        <li>lfloat bzw. rfloat: Textumfluss von rechts bzw. links</li>'+
+							'        <li>alt=TEXT: Beschriftung des Links</li>'+
+							'        <li>alt=\'LANGER TEXT\': mehr als ein Wort Beschriftung</li>'+
+							'        <li>width=ZAHL bzw. height=ZAHL: Breite bzw. Höhe in px</li>'+
+							'        <li>http://www.url.de/IMAGE: für Bilder direkt aus dem Internet</li>'+
+							'    </ul>'+
+							'    </li>'+
+							'    <li>Bsp: (:image bild.jpg width=150 alt=Text lfloat:)</li>'+
+							'</td>'+
+							'<td>'+
+							'</ul><br><span style="font-weight: bold;">Videos:</span>'+
+							'<ul style="font-weight: normal; padding-left: 15px; margin: 0px;">'+
+							'    <li>(:wmplayer DATEINAME play=true width=ZAHL:) für wma, wmv, avi, ...</li>'+
+							'    <li>(:quicktime DATEINAME play=true width=ZAHL:) für mov, wav, mpeg, mp4, ...</li>'+
+							'    <li>(:flash DATEINAME play=true width=ZAHL:) für swf und flv</li>'+
+							'    <li>(:youtube FILM-ID:) oder (:wmplayer FILM-ID:) für Videos auf youtube</li>'+
+							'    <li>(:googlevideo FILM-ID:) für Videos bei google</li>'+
+							'    <li>(:lecture2go DATEINAME:) für Veranstaltungsaufzeichnungen von Lecture2Go.<br> Achtung: Der DATEINAME muss immer auch einen Ordner enthalten.<br>Ein Server (server=rtmp://fms.rrz.uni-hamburg.de:ZAHL/vod) muss nur bei alten Filmen angegeben werden.</li>'+
+							'<li>(:podcampus FILM-ID:) für Filme auf <a href="http://www.podcampus.de" target="_blank">www.podcampus.de</a></li>'+
+							'    <li>Argumente:'+
+							'    <ul>'+
+							'        <li>lfloat bzw. rfloat: Textumfluss von rechts bzw. links</li>'+
+							'        <li>width=ZAHL bzw. height=ZAHL: Breite bzw. Höhe in px</li>'+
+							'        <li>play=true/false: automatischer Start (Standard ist false)</li>'+
+							'        <li>http://www.url.de/DATEINAME: für Videos direkt aus dem Internet</li>'+
+							'        <li>FILM-ID: ID aus der URL (z.B. http://de.youtube.com/watch?v=FILM-ID)</li>'+
+							'    </ul>'+
+							'    </li>'+
+							'    <li>Bsp: (:quicktime video.mpeg width=150 play=true:)</li>'+
+							'    <li>Bsp: (:youtube pQHX-SjgQvQ play=false:)</li>'+
+							'</ul><br><span style="font-weight: bold;">Musik:</span>'+
+							'<ul style="font-weight: normal; padding-left: 15px; margin: 0px;">'+
+							'<li>(:podcampus AUDIO-ID:) für Audio-Pods auf <a href="http://www.podcampus.de" target="_blank">www.podcampus.de</a></li>'+
+							'    <li>(:wmplayer DATEINAME play=true width=ZAHL:) für wma-Dateien</li>'+
+							'    <li>(:mp3 DATEINAME.mp3:) für mp3-Dateien</li>'+
+							'    <li>Argumente:'+
+							'    <ul>'+
+							'        <li>lfloat bzw. rfloat: Textumfluss von rechts bzw. links</li>'+
+							'        <li>width=ZAHL bzw. height=ZAHL: Breite bzw. Höhe in px</li>'+
+							'        <li>play=true/false: automatischer Start (Standard ist false)</li>'+
+							'        <li>http://www.url.de/DATEINAME.mp3: für Musik direkt aus dem Internet</li>'+
+							'    </ul>'+
+							'    </li>'+
+							'    <li>Bsp: (:mp3 mymusic.mp3 width=150 play=true:)</li>'+
+							'</ul><br><span style="font-weight: bold;">Dokumente:</span>'+
+							'<ul style="font-weight: normal; padding-left: 15px; margin: 0px;">'+
+							'    <li>(:slideshare DOC-ID:) für Slideshare-Präsentationen</li>'+
+							'    <li>Argumente:'+
+							'    <ul>'+
+							'        <li>DOC-ID: slideshare.net "doc="-Angabe für die Präsentation (siehe WordPress-Quelltext unten)</li>'+
+							'    </ul>'+
+							'    </li>'+
+							'    <li>Bsp: (:slideshare quicktour-1209540124077378-8:)<br>oder:<br></li>'+
+							'    <li>[slideshare id=380816&amp;doc=quicktour-1209540124077378-8] WordPress-Quelltext von slideshare.net</li>'+
+							'</ul>'+
+							'</td>'+
+							'</tr>'+
+							'</table>'
+					}
+				]
+			}
+		],
+		buttons : []
+	};
+} );
