3e71398b738d649c9f568893cb659a9a70484022
Christoph Schönfeld <schoenfeld@effective-webwork.de>
2013-05-30 15:21:11 +0200

    version update and cleanup script

---
+++ commsy/scripts/8.0.3_to_8.0.4/master_update_bash.php
@@ -0,0 +1,27 @@
+<?php
+// $Id$
+//
+// Release $Name$
+//
+// Copyright (c)2002-2007 Dirk Bloessl Matthias Finck, Dirk Fust, Oliver Hankel, Iver Jackewitz, Michael Janneck,
+// Martti Jeenicke, Detlev Krause, Irina L. Marinescu, Timo Nolte, Bernd Pape,
+// Edouard Simon, Monique Strauss, José Manuel González Vázquez
+//
+//    This file is part of CommSy.
+//
+//    CommSy is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    CommSy is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You have received a copy of the GNU General Public License
+//    along with CommSy.
+
+$bash = true;
+include_once('master_update.php');
+?>
