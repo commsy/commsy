411ee373796a8eb1e2ddcad10ac57d2ece00ce1d
Christopher Timm <timm@effective-webwork.de>
2013-08-19 12:51:59 +0200

    bugfix redirect url

--- commsy/pages/context_logout.php
+++ commsy/pages/context_logout.php
@@ -35,7 +35,8 @@ $flash = $session->getValue('flash');
 if ( $session->issetValue('root_session_id') ) {
    $root_session_id = $session->getValue('root_session_id');
 }
-if ($environment->getConfiguration('c_shibboleth_direct_login') and !empty($environment->getConfiguration('c_shibboleth_redirect_url'))){
+$config = $environment->getConfiguration('c_shibboleth_redirect_url');
+if ($environment->getConfiguration('c_shibboleth_direct_login') and !empty($config)){
 	if ($_SERVER['Shib_userId']){
 		$session_manager->delete($SID,true);
 		$session->reset();
