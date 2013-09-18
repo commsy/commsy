4cad60f5c534bbcfe1f9a67a601cd02e8a73df67
Christopher Timm <timm@effective-webwork.de>
2013-09-18 22:01:59 +0200

    #352 portfolio form template

--- commsy/htdocs/js/src/commsy/widgets/Portfolio/nls/de/PortfolioEditWidget.js
+++ commsy/htdocs/js/src/commsy/widgets/Portfolio/nls/de/PortfolioEditWidget.js
@@ -7,6 +7,8 @@ define({
 	formTitle:					"Titel",
 	formDescription:			"Beschreibung",
 	formExternalViewer:			"Freigeben für (Angabe der Kennungen durch Semikola \";\" getrennt)",
+	formExternalTemplate:		"Vorlage Freigeben für",
+	formTemplate:				"Als Vorlage verwenden",
 	titleCreate:				"Neues Portfolio anlegen",
 	titleEdit:					"Portfolio editieren"
 });
