cf621fec527f5ce24d83d4db9820b5365bf4b6d1
Christopher Timm <timm@effective-webwork.de>
2014-02-11 16:39:16 +0100

    assign user to room

--- etc/config_classes_forms.php
+++ etc/config_classes_forms.php
@@ -410,6 +410,13 @@ $class_config[$class_name]['filename']   = $class_name.'.php';
 $class_config[$class_name]['folder']     = $main_folder.$form_folder;
 $class_config[$class_name]['switchable'] = false;
 
+$class_name = 'cs_account_assignroom_form';
+define('ACCOUNT_ASSIGNROOM_FORM',$class_name);
+$class_config[$class_name]['name']       = $class_name;
+$class_config[$class_name]['filename']   = $class_name.'.php';
+$class_config[$class_name]['folder']     = $main_folder.$form_folder;
+$class_config[$class_name]['switchable'] = false;
+
 ######################
 
 $class_name = 'cs_become_member_form';
