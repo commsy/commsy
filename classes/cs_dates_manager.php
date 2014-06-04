79f4d20e5e039c179705818b170ea1a795002ac6
Johannes Schultze <schultze@effective-webwork.de>
2014-06-04 15:29:54 +0200

    Create dates and materials with annotations.

--- classes/cs_dates_manager.php
+++ classes/cs_dates_manager.php
@@ -1009,7 +1009,16 @@ class cs_dates_manager extends cs_manager implements cs_export_import_interface 
    }
    
    function import_item($top_item, $xml) {
-      
+      $item = null;
+      if ($xml != null) {
+         $item = $this->getNewItem();
+         $item->setTitle((string)$xml->title[0]);
+         $item->setDescription((string)$xml->description[0]);
+         $item->setContextId($top_item->getItemId());
+         $item->save();
+         $this->importAnnotationsFromXML($item, $xml);
+      }
+      return $item;
    }
    
    function import_sub_items($top_item, $xml) {
