46ded3b1ce61129ddf990b811be7c9303894534a
Christoph Schönfeld <schoenfeld@effective-webwork.de>
2014-06-05 11:08:12 +0200

    merge fix

--- classes/forms/cs_configuration_authentication_form.php
+++ classes/forms/cs_configuration_authentication_form.php
@@ -5,7 +5,7 @@
 //
 // Copyright (c)2002-2003 Matthias Finck, Dirk Fust, Oliver Hankel, Iver Jackewitz, Michael Janneck,
 // Martti Jeenicke, Detlev Krause, Irina L. Marinescu, Timo Nolte, Bernd Pape,
-// Edouard Simon, Monique Strauss, Jos√© Manuel Gonz√°lez V√°zquez
+// Edouard Simon, Monique Strauss, José Manuel González Vázquez
 //
 //    This file is part of CommSy.
 //
