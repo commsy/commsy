b9de5f5aedd5608c8df60c67670040dbe0c6476e
Christopher Timm <timm@effective-webwork.de>
2014-10-06 13:26:57 +0200

    fixed todo headline

--- classes/controller/index/cs_todo_index_controller.php
+++ classes/controller/index/cs_todo_index_controller.php
@@ -26,7 +26,7 @@
 			parent::processTemplate();
 
 			// assign rubric to template
-			$this->assign('room', 'rubric', CS_TOPIC_TYPE);
+			$this->assign('room', 'rubric', CS_TODO_TYPE);
 		}
 
 		/*****************************************************************************/
