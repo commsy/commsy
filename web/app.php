6c15cb615f91ce59ab202f64855d18d0448696a8
Christoph Schoenfeld <christoph@leath.de>
2014-10-18 21:49:31 +0200

    new main application file

---
+++ web/app.php
@@ -0,0 +1,15 @@
+<?php
+	// definitions for several paths
+	define('LEGACY_DIR', realpath(__DIR__ . '/../legacy'));
+	define('CLASSES_DIR', LEGACY_DIR . '/classes');
+	define('EXTERNAL_PAGES_DIR', LEGACY_DIR . '/external_pages');
+	define('FUNCTIONS_DIR', LEGACY_DIR . '/functions');
+	define('INCLUDE_DIR', LEGACY_DIR . '/include');
+	define('LIBS_DIR', LEGACY_DIR . '/libs');
+	define('PAGES_DIR', LEGACY_DIR . '/pages');
+	define('PLUGINS_DIR', LEGACY_DIR . '/plugins');
+	define('SCRIPTS_DIR', LEGACY_DIR . '/scripts');
+
+	// load required dependencies
+	
+	// update include path
