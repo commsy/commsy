baded8b6247eb0857534342884e60dff83aa4fcf
Johannes Schultze <schultze@effective-webwork.de>
2015-03-02 16:36:07 +0100

    #1484 #9157 Kopieren persönliche Ablage.

--- external_pages/MAINBGMED/cs_external_page_portal_view.php
+++ external_pages/MAINBGMED/cs_external_page_portal_view.php
@@ -3363,6 +3363,8 @@ class cs_external_page_portal_view extends cs_page_view {
    				if ( $numCopies > 0)
    				{
    					$html .= '	<span id="tm_clipboard_copies">' . $numCopies . '</span>';
+   				} else {
+      				$html .= '	<span id="tm_clipboard_copies"></span>';
    				}
    			}
 
