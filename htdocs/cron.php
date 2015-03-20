b3a5e600bf313dfeb067f8ae7d7aca4e37512477
Christoph Schönfeld <schoenfeld@effective-webwork.de>
2015-03-20 15:38:28 +0100

    old cron removed

--- htdocs/cron.php
+++ htdocs/cron.php
@@ -23,76 +23,28 @@
 //    along with CommSy.
 
 $cid = 0;
-if ( !empty($_GET['cid']) ) {
-	$cid = $_GET['cid'];
-} else if (!empty($_SERVER["argv"][1])) {
-	$cid = $_SERVER["argv"][1];
+if (!empty($_GET['cid'])) {
+    $cid = $_GET['cid'];
+} elseif (!empty($_SERVER["argv"][1])) {
+    $cid = $_SERVER["argv"][1];
 }
 
 // pretend, we work from the CommSy basedir to allow
 // giving include files without "../" prefix all the time.
 chdir('..');
 
-function runCron() {
-	@include_once('etc/commsy/development.php');
-	if ( isset($c_cron_use_new)
-			and !empty($c_cron_use_new)
-			and $c_cron_use_new
-	) {
-		include_once('cron_new.php');
-	} else {
-		include_once('cron_old.php');
-	}
-}
-
-@include_once('etc/commsy/development.php');
-function runCronNew ($cid = 0) {
-	global $c_cron_use_new;
-	global $c_cron_use_old_array;
-	$retour = false;
-
-   if ( isset($c_cron_use_new)
-        and !empty($c_cron_use_new)
-        and $c_cron_use_new
-      ) {
-	   if ( !isset($c_cron_use_old_array)
-	        or empty($c_cron_use_old_array)
-	        or empty($cid)
-	        or !in_array($cid,$c_cron_use_old_array)
-	      ) {
-         $retour = true;
-	   }
-   }
-	return $retour;
-}
-
 include_once('etc/cs_config.php');
 if (isset($c_commsy_cron_token) and !empty($c_commsy_cron_token)) {
-	// if cron token is enabled
-	if (isset($_GET['cron_token']) and ($_GET['cron_token'] === $c_commsy_cron_token)) {
-		if ( runCronNew($cid) ) {
-		   include_once('cron_new.php');
-	   } else {
-		   include_once('cron_old.php');
-	   }
-			
-	// try to get from command line - should be third parameter
-	} else if (isset($_SERVER["argv"][2]) && $_SERVER["argv"][2] === $c_commsy_cron_token) {
-		if ( runCronNew($cid) ) {
-		   include_once('cron_new.php');
-	   } else {
-		   include_once('cron_old.php');
-	   }
-	} else {
-		die("cron token does not match");
-	}
+    // if cron token is enabled
+    if (isset($_GET['cron_token']) and ($_GET['cron_token'] === $c_commsy_cron_token)) {
+        include_once('cron_new.php');
+    // try to get from command line - should be third parameter
+    } elseif (isset($_SERVER["argv"][2]) && $_SERVER["argv"][2] === $c_commsy_cron_token) {
+        include_once('cron_new.php');
+    } else {
+        die("cron token does not match");
+    }
 } else {
-	// if cron token is disabled
-	if ( runCronNew($cid) ) {
-		include_once('cron_new.php');
-	} else {
-		include_once('cron_old.php');
-	}
+    // if cron token is disabled
+    include_once('cron_new.php');
 }
-
-?>
