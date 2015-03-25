c266a8a1a9dce97aa1f3cbf492defa6ff04fc2cc
Christoph Schönfeld <schoenfeld@effective-webwork.de>
2015-03-25 10:03:46 +0100

    vagrant updated and configuration improved

--- vagrant/puphpet/shell/initial-setup.sh
+++ vagrant/puphpet/shell/initial-setup.sh
@@ -6,6 +6,7 @@ VAGRANT_CORE_FOLDER=$(echo "$1")
 
 OS=$(/bin/bash "${VAGRANT_CORE_FOLDER}/shell/os-detect.sh" ID)
 CODENAME=$(/bin/bash "${VAGRANT_CORE_FOLDER}/shell/os-detect.sh" CODENAME)
+RELEASE=$(/bin/bash "${VAGRANT_CORE_FOLDER}/shell/os-detect.sh" RELEASE)
 
 cat "${VAGRANT_CORE_FOLDER}/shell/ascii-art/self-promotion.txt"
 printf "\n"
@@ -23,13 +24,14 @@ echo "${VAGRANT_CORE_FOLDER}" > '/.puphpet-stuff/vagrant-core-folder.txt'
 # apt repos become stale, Ubuntu/Debian move stuff around and break existing
 # boxes that no longer require apt-get update. Force it one more time. Update
 # datestamp as required for future breaks.
-if [[ ! -f '/.puphpet-stuff/initial-setup-repo-update-11052014' ]]; then
+if [[ ! -f '/.puphpet-stuff/initial-setup-apt-get-update' ]]; then
     if [ "${OS}" == 'debian' ] || [ "${OS}" == 'ubuntu' ]; then
-        echo 'Running datestamped initial-setup apt-get update'
+        echo 'Running initial-setup apt-get update'
         apt-get update >/dev/null
-        touch '/.puphpet-stuff/initial-setup-repo-update-11052014'
-        echo 'Finished running datestamped initial-setup apt-get update'
+        echo 'Finished running initial-setup apt-get update'
     fi
+
+    touch '/.puphpet-stuff/initial-setup-repo-update'
 fi
 
 if [[ -f '/.puphpet-stuff/initial-setup-base-packages' ]]; then
@@ -37,10 +39,6 @@ if [[ -f '/.puphpet-stuff/initial-setup-base-packages' ]]; then
 fi
 
 if [ "${OS}" == 'debian' ] || [ "${OS}" == 'ubuntu' ]; then
-    echo 'Running initial-setup apt-get update'
-    apt-get update >/dev/null
-    echo 'Finished running initial-setup apt-get update'
-
     echo 'Installing curl'
     apt-get -y install curl >/dev/null
     echo 'Finished installing curl'
@@ -65,8 +63,16 @@ elif [[ "${OS}" == 'centos' ]]; then
     perl -p -i -e 's@#baseurl=http://mirror.centos.org/centos/\$releasever/updates/\$basearch/@baseurl=http://mirror.rackspace.com/CentOS//\$releasever/updates/\$basearch/\nenabled=1@gi' /etc/yum.repos.d/CentOS-Base.repo
     perl -p -i -e 's@#baseurl=http://mirror.centos.org/centos/\$releasever/extras/\$basearch/@baseurl=http://mirror.rackspace.com/CentOS//\$releasever/extras/\$basearch/\nenabled=1@gi' /etc/yum.repos.d/CentOS-Base.repo
 
-    yum -y --nogpgcheck install 'http://www.elrepo.org/elrepo-release-6-6.el6.elrepo.noarch.rpm' >/dev/null
-    yum -y --nogpgcheck install 'https://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm' >/dev/null
+    if [ "${RELEASE}" == 6 ]; then
+        EL_REPO='http://www.elrepo.org/elrepo-release-6-6.el6.elrepo.noarch.rpm'
+        EPEL='https://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm'
+    else
+        EL_REPO='http://www.elrepo.org/elrepo-release-7.0-2.el7.elrepo.noarch.rpm'
+        EPEL='https://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm'
+    fi
+
+    yum -y --nogpgcheck install "${EL_REPO}" >/dev/null
+    yum -y --nogpgcheck install "${EPEL}" >/dev/null
     yum -y install centos-release-SCL >/dev/null
     yum clean all >/dev/null
     yum -y check-update >/dev/null
--- vagrant/puphpet/shell/install-puppet.sh
+++ vagrant/puphpet/shell/install-puppet.sh
@@ -8,51 +8,27 @@ OS=$(/bin/bash "${VAGRANT_CORE_FOLDER}/shell/os-detect.sh" ID)
 RELEASE=$(/bin/bash "${VAGRANT_CORE_FOLDER}/shell/os-detect.sh" RELEASE)
 CODENAME=$(/bin/bash "${VAGRANT_CORE_FOLDER}/shell/os-detect.sh" CODENAME)
 
-if [ "${OS}" == 'debian' ] || [ "${OS}" == 'ubuntu' ]; then
-    RVM_RUBIES='gems'
-elif [[ "${OS}" == 'centos' ]]; then
-    RVM_RUBIES='rubies'
+# deep_merge gem required by hiera
+if [[ ! -f /.puphpet-stuff/install-deep_merge-03122015 ]]; then
+    gem install deep_merge --no-ri --no-rdoc
+    touch /.puphpet-stuff/install-deep_merge-03122015
 fi
 
-function check_puppet_symlink() {
-    if [[ -f "/usr/local/rvm/${RVM_RUBIES}/ruby-1.9.3-p547/bin/puppet" ]]; then
-        rm -f '/usr/bin/puppet'
-        ln -s "/usr/local/rvm/${RVM_RUBIES}/ruby-1.9.3-p547/bin/puppet" '/usr/bin/puppet'
-        return 0;
-    elif [[ -f "/usr/local/rvm/${RVM_RUBIES}/ruby-1.9.3-p551/bin/puppet" ]]; then
-        rm -f '/usr/bin/puppet'
-        ln -s "/usr/local/rvm/${RVM_RUBIES}/ruby-1.9.3-p551/bin/puppet" '/usr/bin/puppet'
-        return 0;
-    fi
+if [[ ! -f /.puphpet-stuff/install-activesupport-03132015 ]]; then
+    gem install activesupport --no-ri --no-rdoc
+    touch /.puphpet-stuff/install-activesupport-03132015
+fi
 
-    # Puppet not installed
-    if [ ! -L '/usr/bin/puppet' ]; then
-        rm -f '/.puphpet-stuff/install-puppet'
+if [[ ! -f /.puphpet-stuff/install-vine-03202015 ]]; then
+    gem install vine --no-ri --no-rdoc
+    touch /.puphpet-stuff/install-vine-03202015
+fi
 
-        return 0;
-    fi
-
-    PUPPET_SYMLINK=$(ls -l /usr/bin/puppet);
-
-    # If puppet symlink is old-style pointing to /usr/local/rvm/wrappers/default/ruby
-    if [ "grep '/usr/local/rvm/wrappers/default' ${PUPPET_SYMLINK}" ]; then
-        rm -f '/usr/bin/puppet'
-
-        if [[ -f "/usr/local/rvm/${RVM_RUBIES}/ruby-1.9.3-p547/bin/puppet" ]]; then
-            ln -s "/usr/local/rvm/${RVM_RUBIES}/ruby-1.9.3-p547/bin/puppet" '/usr/bin/puppet'
-        elif [[ -f "/usr/local/rvm/${RVM_RUBIES}/ruby-1.9.3-p551/bin/puppet" ]]; then
-            ln -s "/usr/local/rvm/${RVM_RUBIES}/ruby-1.9.3-p551/bin/puppet" '/usr/bin/puppet'
-        else
-            rm -f '/.puphpet-stuff/install-puppet'
-        fi
-    fi
-}
-
-check_puppet_symlink
-
-if [[ -f '/.puphpet-stuff/install-puppet' ]]; then
+if [[ -f /.puphpet-stuff/install-puppet-3.4.3 ]]; then
     exit 0
 fi
+
+rm -rf /usr/bin/puppet
 
 if [ "${OS}" == 'debian' ] || [ "${OS}" == 'ubuntu' ]; then
     apt-get -y install augeas-tools libaugeas-dev
@@ -61,17 +37,19 @@ elif [[ "${OS}" == 'centos' ]]; then
 fi
 
 echo 'Installing Puppet requirements'
-/usr/bin/gem install haml hiera facter json ruby-augeas --no-document
+gem install haml hiera facter json ruby-augeas deep_merge --no-ri --no-rdoc
 echo 'Finished installing Puppet requirements'
 
 echo 'Installing Puppet 3.4.3'
-/usr/bin/gem install puppet --version 3.4.3 --no-document
-
-if [[ -f '/usr/bin/puppet' ]]; then
-    mv /usr/bin/puppet /usr/bin/puppet-old
-fi
-
-ln -s "/usr/local/rvm/${RVM_RUBIES}/ruby-1.9.3-p*/bin/puppet" /usr/bin/puppet
+gem install puppet --version 3.4.3 --no-ri --no-rdoc
 echo 'Finished installing Puppet 3.4.3'
 
-touch '/.puphpet-stuff/install-puppet'
+cat >/usr/bin/puppet << 'EOL'
+#!/bin/bash
+
+rvm ruby-1.9.3-p551 do puppet "$@"
+EOL
+
+chmod +x /usr/bin/puppet
+
+touch /.puphpet-stuff/install-puppet-3.4.3
