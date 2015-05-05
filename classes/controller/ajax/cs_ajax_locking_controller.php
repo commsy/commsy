0c636d78dcf098d5629cedef2aa941e227b48f43
Christopher Timm <timm@effective-webwork.de>
2015-05-05 09:19:22 +0200

    #1550 #9574 edit lock

--- classes/controller/ajax/cs_ajax_locking_controller.php
+++ classes/controller/ajax/cs_ajax_locking_controller.php
@@ -45,6 +45,54 @@ class cs_ajax_locking_controller extends cs_ajax_controller {
 		echo $this->_return;
 	}
 
+	public function actionStatus()
+	{
+        $translator = $this->_environment->getTranslationObject();
+		$itemId = $this->_data['id'];
+		$return = array();
+
+		// determ the item type
+		$itemManager = $this->_environment->getItemManager();
+		$itemType = $itemManager->getItemType($itemId);
+
+		if (!empty($itemType)) {
+			// get the corresponding manager
+			$manager = $this->_environment->getManager($itemType);
+			if ($manager) {
+                $item = $manager->getItem($itemId);
+				$checkLocking = $this->_environment->getConfiguration('c_item_locking');
+          		$checkLocking = ($checkLocking) ? $checkLocking : false;
+				if ($checkLocking && method_exists($item, "getLockingDate")) {
+					$lockingDate = $item->getLockingDate();
+					if ($lockingDate) {
+						$editDate = new DateTime($lockingDate);
+						$compareDate = new DateTime();
+						$compareDate->modify("-20 minutes");
+
+						$userManager = $this->_environment->getUserManager();
+						$lockingUser = $userManager->getItem($item->getLockingUserId());
+                        $currentUser = $this->_environment->getCurrentUser();
+
+						$currentUserId = $currentUser->getRelatedPortalUserItem()->getItemId();
+						$lockingUserId = $lockingUser->getRelatedPortalUserItem()->getItemId();
+ 
+						if ($currentUserId != $lockingUserId) {
+							$return['locked'] = ($compareDate < $editDate);
+							$return['locked_user_name'] = $lockingUser->getFullName();
+							$return['locked_date'] = $translator->getDateTimeinLang($lockingDate);
+                            $return['locked_message'] = $translator->getMessage("ITEM_LOCKING_DESC", $return['locked_user_name'], $return['locked_date']);
+						}	
+					}
+				}		
+			}
+		}
+
+		$this->setSuccessfullDataReturn($return);
+		echo $this->_return;
+
+
+	}
+
 	/*
 	 * clears a lock
 	 */
