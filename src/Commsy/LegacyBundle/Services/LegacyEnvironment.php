4ce6c0106a425ba2f9033070e00930b988710458
Christoph Schönfeld <schoenfeld@effective-webwork.de>
2015-06-04 21:39:42 +0200

    classes loaded from legacy environment now have proper access to the symfony DIC

--- src/Commsy/LegacyBundle/Services/LegacyEnvironment.php
+++ src/Commsy/LegacyBundle/Services/LegacyEnvironment.php
@@ -2,6 +2,8 @@
 
 namespace Commsy\LegacyBundle\Services;
 
+use Symfony\Component\DependencyInjection\ContainerInterface as Container;
+
 class LegacyEnvironment
 {
     private $environment;
@@ -13,11 +15,17 @@ class LegacyEnvironment
     private $legacyAppPath;
 
     /**
+     * Symfony service container
+     */
+    private $serviceContainer;
+
+    /**
      * @param String             $legacyAppPath
      */
-    public function __construct($legacyAppPath)
+    public function __construct($legacyAppPath, Container $container)
     {
         $this->legacyAppPath = $legacyAppPath;
+        $this->serviceContainer = $container;
     }
 
     public function getEnvironment()
@@ -31,6 +39,9 @@ class LegacyEnvironment
             include_once('etc/cs_constants.php');
             include_once('etc/cs_config.php');
             include_once('functions/misc_functions.php');
+
+            global $symfonyContainer;
+            $symfonyContainer = $this->serviceContainer;
 
             include_once('classes/cs_environment.php');
             global $environment;
