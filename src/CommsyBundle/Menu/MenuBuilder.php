68cbc4d9a0586a989922b95f9fc9f727ac16342d
Christopher Timm <timm@effective-webwork.de>
2015-06-17 12:43:53 +0200

    dont show rubric navigation if no roomId is given

--- src/CommsyBundle/Menu/MenuBuilder.php
+++ src/CommsyBundle/Menu/MenuBuilder.php
@@ -30,79 +30,82 @@ class MenuBuilder
 
         $menu->addChild('dashboard', array(
             'label' => 'DASHBOARD',
-            'route' => 'commsy_room_home',
-            'routeParameters' => array('roomId' => $roomId),
+            'route' => 'commsy_dashboard_index',
             'extras' => array('icon' => 'uk-icon-home uk-icon-small')
         ));
 
         // add divider
         $menu->addChild('')->setAttribute('class', 'uk-nav-divider');
 
-        $menu->addChild('room_navigation', array(
-            'label' => 'Raum-Navigation',
-            'route' => 'commsy_room_home',
-            'routeParameters' => array('roomId' => $roomId),
-            'extras' => array('icon' => 'uk-icon-home uk-icon-small')
-        ));
+        if ($roomId)
+        {
+            $menu->addChild('room_navigation', array(
+                'label' => 'Raum-Navigation',
+                'route' => 'commsy_room_home',
+                'routeParameters' => array('roomId' => $roomId),
+                'extras' => array('icon' => 'uk-icon-home uk-icon-small')
+            ));
 
-        // add divider
-        $menu->addChild(' ')->setAttribute('class', 'uk-nav-divider');
+            // add divider
+            $menu->addChild(' ')->setAttribute('class', 'uk-nav-divider');
 
-        $menu->addChild('announcement', array(
-            'label' => 'ANKÜNDIGUNGEN',
-            'route' => 'commsy_announcement_list',
-            'routeParameters' => array('roomId' => $roomId),
-            'extras' => array('icon' => 'uk-icon-home uk-icon-small')
-        ));
+            $menu->addChild('announcement', array(
+                'label' => 'ANKÜNDIGUNGEN',
+                'route' => 'commsy_announcement_list',
+                'routeParameters' => array('roomId' => $roomId),
+                'extras' => array('icon' => 'uk-icon-home uk-icon-small')
+            ));
 
-        $menu->addChild('date', array(
-            'label' => 'TERMINE',
-            'route' => 'commsy_date_list',
-            'routeParameters' => array('roomId' => $roomId),
-            'extras' => array('icon' => 'uk-icon-calendar uk-icon-small')
-        ));
+            $menu->addChild('date', array(
+                'label' => 'TERMINE',
+                'route' => 'commsy_date_list',
+                'routeParameters' => array('roomId' => $roomId),
+                'extras' => array('icon' => 'uk-icon-calendar uk-icon-small')
+            ));
 
-        $menu->addChild('material', array(
-            'label' => 'MATERIALIEN',
-            'route' => 'commsy_material_list',
-            'routeParameters' => array('roomId' => $roomId),
-            'extras' => array('icon' => 'uk-icon-file-o uk-icon-small')
-        ));
+            $menu->addChild('material', array(
+                'label' => 'MATERIALIEN',
+                'route' => 'commsy_material_list',
+                'routeParameters' => array('roomId' => $roomId),
+                'extras' => array('icon' => 'uk-icon-file-o uk-icon-small')
+            ));
 
-        $menu->addChild('discussion', array(
-            'label' => 'DISKUSSIONEN',
-            'route' => 'commsy_discussion_list',
-            'routeParameters' => array('roomId' => $roomId),
-            'extras' => array('icon' => 'uk-icon-comments-o uk-icon-small')
-        ));
+            $menu->addChild('discussion', array(
+                'label' => 'DISKUSSIONEN',
+                'route' => 'commsy_discussion_list',
+                'routeParameters' => array('roomId' => $roomId),
+                'extras' => array('icon' => 'uk-icon-comments-o uk-icon-small')
+            ));
 
-        $menu->addChild('person', array(
-            'label' => 'PERSONEN',
-            'route' => 'commsy_person_list',
-            'routeParameters' => array('roomId' => $roomId),
-            'extras' => array('icon' => 'uk-icon-users uk-icon-small')
-        ));
+            $menu->addChild('person', array(
+                'label' => 'PERSONEN',
+                'route' => 'commsy_person_list',
+                'routeParameters' => array('roomId' => $roomId),
+                'extras' => array('icon' => 'uk-icon-users uk-icon-small')
+            ));
 
-        $menu->addChild('group', array(
-            'label' => 'GRUPPEN',
-            'route' => 'commsy_group_list',
-            'routeParameters' => array('roomId' => $roomId),
-            'extras' => array('icon' => 'uk-icon-home uk-icon-small')
-        ));
+            $menu->addChild('group', array(
+                'label' => 'GRUPPEN',
+                'route' => 'commsy_group_list',
+                'routeParameters' => array('roomId' => $roomId),
+                'extras' => array('icon' => 'uk-icon-home uk-icon-small')
+            ));
 
-        $menu->addChild('todo', array(
-            'label' => 'AUFGABEN',
-            'route' => 'commsy_todo_list',
-            'routeParameters' => array('roomId' => $roomId),
-            'extras' => array('icon' => 'uk-icon-check uk-icon-small')
-        ));
+            $menu->addChild('todo', array(
+                'label' => 'AUFGABEN',
+                'route' => 'commsy_todo_list',
+                'routeParameters' => array('roomId' => $roomId),
+                'extras' => array('icon' => 'uk-icon-check uk-icon-small')
+            ));
 
-        $menu->addChild('topic', array(
-            'label' => 'THEMEN',
-            'route' => 'commsy_topic_list',
-            'routeParameters' => array('roomId' => $roomId),
-            'extras' => array('icon' => 'uk-icon-home uk-icon-small')
-        ));
+            $menu->addChild('topic', array(
+                'label' => 'THEMEN',
+                'route' => 'commsy_topic_list',
+                'routeParameters' => array('roomId' => $roomId),
+                'extras' => array('icon' => 'uk-icon-home uk-icon-small')
+            ));
+        }
+        
 
 
 
