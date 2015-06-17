08bec1dfbd50592dcd099d656ff290f5d71c62e7
Johannes Schultze <schultze@effective-webwork.de>
2015-06-17 10:28:09 +0200

    New link to home. Only applied if module is "home", function is "index" and no other parameters are given.

--- legacy/functions/curl_functions.php
+++ legacy/functions/curl_functions.php
@@ -147,7 +147,7 @@ function curl( $context_id, $module, $function, $parameter, $fragment='', $fileh
  */
 function _curl( $amp_flag, $context_id, $module, $function, $parameter, $fragment='', $filehack='', $file='' ) {
    if ($module == 'home' && $function == 'index' && empty($parameter)) {
-      return '/room/'.$context_id;
+      return '/commsy.php/room/'.$context_id;
    }
    
    if ( empty($file) ) {
