79e4171f00465fde11a0deea5deab8000fc83168
Christopher Timm <timm@effective-webwork.de>
2015-06-17 15:41:54 +0200

    add room service

---
+++ src/Commsy/LegacyBundle/Utils/RoomService.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Commsy\LegacyBundle\Utils;
+
+use Commsy\LegacyBundle\Services\LegacyEnvironment;
+
+class RoomService
+{
+    private $legacyEnvironment;
+
+    public function __construct(LegacyEnvironment $legacyEnvironment)
+    {
+        $this->legacyEnvironment = $legacyEnvironment;
+    }
+
+    public function getRubricInformation($roomId)
+    {
+        $legacyEnvironment = $this->legacyEnvironment->getEnvironment();
+        
+        // get the rooms rubric configuration
+        $roomManager = $legacyEnvironment->getRoomManager();
+        $roomItem = $roomManager->getItem($roomId);
+        $homeConfiguration = $roomItem->getHomeConf();
+
+        $rubrics = array();
+        if (!empty($homeConfiguration)) {
+            $rubricConfigurations = explode(',', $homeConfiguration);
+            
+            foreach ($rubricConfigurations as $rubricConfiguration) {
+                list($rubricName) = explode('_', $rubricConfiguration);
+                $rubrics[] = $rubricName;
+            }
+        }
+        return $rubrics;
+
+    }
+}
