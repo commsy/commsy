2e7ccbce973b6a3b4e78cb3bcac5338c7ce334a2
Christoph Schönfeld <schoenfeld@effective-webwork.de>
2015-06-23 01:38:22 +0200

    basic material list pagination basic filtering

---
+++ src/Commsy/LegacyBundle/Utils/MaterialManager.php
@@ -0,0 +1,41 @@
+<?php
+namespace Commsy\LegacyBundle\Utils;
+
+use Symfony\Component\Form\Form;
+
+use Commsy\LegacyBundle\Services\LegacyEnvironment;
+
+class MaterialManager
+{
+    private $legacyEnvironment;
+
+    private $materialManager;
+
+    public function __construct(LegacyEnvironment $legacyEnvironment)
+    {
+        $this->legacyEnvironment = $legacyEnvironment->getEnvironment();
+
+        $this->materialManager = $this->legacyEnvironment->getMaterialManager();
+        $this->materialManager->reset();
+    }
+
+    public function getListMaterials($roomId)
+    {
+        $this->materialManager->setContextLimit($roomId);
+
+        $this->materialManager->select();
+        $materialList = $this->materialManager->get();
+
+        return $materialList->to_array();
+    }
+
+    public function setFilterConditions(Form $filterForm)
+    {
+        $formData = $filterForm->getData();
+
+        // activated
+        if ($formData['activated']) {
+            $this->materialManager->showNoNotActivatedEntries();
+        }
+    }
+}
--- src/CommsyBundle/Controller/MaterialController.php
+++ src/CommsyBundle/Controller/MaterialController.php
@@ -7,6 +7,8 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Request;
 
+use CommsyBundle\Filter\MaterialFilterType;
+
 class MaterialController extends Controller
 {
     /**
@@ -24,8 +26,50 @@ class MaterialController extends Controller
      */
     public function listAction($roomId, Request $request)
     {
-        return array(
-            'roomId' => $roomId
+        // get the material manager service
+        $materialManager = $this->get('commsy_legacy.material_manager');
+
+        // setup filter form
+        $defaultFilterValues = array(
+            'activated' => true
         );
+        $form = $this->createForm(new MaterialFilterType(), $defaultFilterValues, array(
+            'action' => $this->generateUrl('commsy_material_list', array('roomId' => $roomId)),
+            'method' => 'GET',
+        ));
+
+        // check query for form data
+        if ($request->query->has($form->getName())) {
+            // manually bind values from the request
+            $form->submit($request->query->get($form->getName()));
+        }
+
+        // set filter conditions in material manager
+        $materialManager->setFilterConditions($form);
+
+        // get material list from manager service 
+        $materials = $materialManager->getListMaterials($roomId);
+
+        $paginator = $this->get('knp_paginator');
+        $pagination = $paginator->paginate(
+            $materials,
+            $request->query->getInt('page', 1),
+            10
+        );
+
+        return array(
+            'roomId' => $roomId,
+            'pagination' => $pagination,
+            'form' => $form->createView()
+        );
+    }
+
+    /**
+     * @Route("/room/{roomId}/material/{itemId}")
+     * @Template()
+     */
+    public function detailAction($roomId, $itemId, Request $request)
+    {
+        return array();
     }
 }
