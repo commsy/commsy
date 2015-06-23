467e07985f62d202cffe1e87ff5404881c4b9865
Christoph Schönfeld <schoenfeld@effective-webwork.de>
2015-06-23 23:42:38 +0200

    custom dbal array type, that can convert commsy's serialized extras fields

---
+++ src/CommsyBundle/DBAL/Types/ArrayType.php
@@ -0,0 +1,39 @@
+<?php
+namespace CommsyBundle\DBAL\Types;
+
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\ArrayType as BaseType;
+
+class ArrayType extends BaseType
+{
+    const MBARRAY = 'mb_array';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function convertToPHPValue($value, AbstractPlatform $platform)
+    {
+        if ($value === null) {
+            return null;
+        }
+
+        $value = (is_resource($value)) ? stream_get_contents($value) : $value;
+
+        $value = preg_replace('!s:(\d+):"(.*?)";!se', "'s:'.strlen('$2').':\"$2\";'", $value);
+
+        $val = unserialize($value);
+        if ($val === false && $value != 'b:0;') {
+            throw ConversionException::conversionFailed($value, $this->getName());
+        }
+
+        return $val;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return self::MBARRAY;
+    }
+}
