5f7e45db3043e6c5ac481c171fc636aa289f17e1
Johannes Schultze <schultze@effective-webwork.de>
2015-06-24 14:44:17 +0200

    Group detail view.

--- src/CommsyBundle/Controller/LinkController.php
+++ src/CommsyBundle/Controller/LinkController.php
@@ -19,65 +19,40 @@ class LinkController extends Controller
      */
     public function linkAction($roomId, $itemId)
     {
-        /*// get the user manager service
-        $userManager = $this->get('commsy.user_service');
-
-        // setup filter form
-        $defaultFilterValues = array(
-            'activated' => true
-        );
-        $form = $this->createForm(new UserFilterType(), $defaultFilterValues, array(
-            'action' => $this->generateUrl('commsy_user_list', array('roomId' => $roomId)),
-            'method' => 'GET',
-        ));
-
-        // check query for form data
-        if ($request->query->has($form->getName())) {
-            // manually bind values from the request
-            $form->submit($request->query->get($form->getName()));
-        }
-
-        // set filter conditions in user manager
-        $userManager->setFilterConditions($form);
-
-        // get material list from manager service 
-        $materials = $userManager->getListUsers($roomId);
-
-        $paginator = $this->get('knp_paginator');
-        $pagination = $paginator->paginate(
-            $materials,
-            $request->query->getInt('page', 1),
-            10
-        );
-
-        return array(
-            'roomId' => $roomId,
-            'pagination' => $pagination,
-            'form' => $form->createView()
-        );*/
-        
         $itemService = $this->get('commsy.item_service');
         $item = $itemService->getItem($itemId);
-        $ids = $item->getAllLinkeditemIDArray();
         
         $labelService = $this->get('commsy.label_service');
         
         $groups = array();
-        $linkedItems = array();
-        foreach ($ids as $id) {
-            $tempItem = $itemService->getItem($id);
-            if ($tempItem->getItemType() == 'label') {
-                $tempLabel = $labelService->getLabel($id);
-                if ($tempLabel->getLabelType() == 'group') {
-                    $groups[] = $tempLabel;
+        $users = array();
+        
+        if ($item->getItemType() == 'label') {
+            $tempLabel = $labelService->getLabel($item->getItemId());
+            if ($tempLabel->getLabelType() == 'group') {
+                $groupService = $this->get('commsy.group_service');
+                $group = $groupService->getGroup($tempLabel->getItemID());
+                $membersList = $group->getMemberItemList();
+                $users = $membersList->to_array();
+            }
+        } else {
+            $ids = $item->getAllLinkeditemIDArray();
+            foreach ($ids as $id) {
+                $tempItem = $itemService->getItem($id);
+                if ($tempItem->getItemType() == 'label') {
+                    $tempLabel = $labelService->getLabel($id);
+                    if ($tempLabel->getLabelType() == 'group') {
+                        $groups[] = $tempLabel;
+                    }
                 }
-            } else {
-                $linkedItems[] = $tempItem;
             }
         }
-
+        
+        dump($users);
+        
         return array(
-            'groups' => $groups
+            'groups' => $groups,
+            'users' => $users
         );
     }
 }
