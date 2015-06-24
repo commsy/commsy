75526199f81415b5c6d072ad04a4cafd880a0c1e
Christoph Schönfeld <schoenfeld@effective-webwork.de>
2015-06-24 17:46:12 +0200

    KnpPaginatorBundle removed

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,7 +21,6 @@ class AppKernel extends Kernel
             new Sp\BowerBundle\SpBowerBundle(),
             new Knp\Bundle\MenuBundle\KnpMenuBundle(),
             new Lexik\Bundle\FormFilterBundle\LexikFormFilterBundle(),
-            new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
             
             new CommsyBundle\CommsyBundle(),
             new Commsy\LegacyBundle\CommsyLegacyBundle(),
