19c7313802cfb3edefcc188b75b5b00a59bf43bf
Johannes Schultze <schultze@effective-webwork.de>
2015-06-25 09:31:25 +0200

    Changed Group:index to Group:detail.

--- src/CommsyBundle/Controller/GroupController.php
+++ src/CommsyBundle/Controller/GroupController.php
@@ -12,24 +12,6 @@ use CommsyBundle\Filter\GroupFilterType;
 class GroupController extends Controller
 {
     /**
-     * @Route("/room/{roomId}/group/{itemId}")
-     * @Template()
-     */
-    public function indexAction($roomId, $itemId, Request $request)
-    {
-        $groupService = $this->get('commsy.group_service');
-        $group = $groupService->getGroup($itemId);
-        
-        $membersList = $group->getMemberItemList();
-        $members = $membersList->to_array();
-        
-        return array(
-            'group' => $group,
-            'members' => $members
-        );
-    }
-
-    /**
      * @Route("/room/{roomId}/group")
      * @Template()
      */
@@ -62,6 +44,60 @@ class GroupController extends Controller
         return array(
             'roomId' => $roomId,
             'form' => $form->createView()
+        );
+    }
+    
+    /**
+     * @Route("/room/{roomId}/group/feed/{start}")
+     * @Template()
+     */
+    public function feedAction($roomId, $max = 10, $start = 0, Request $request)
+    {
+        // setup filter form
+        $defaultFilterValues = array(
+            'activated' => true
+        );
+        $form = $this->createForm(new GroupFilterType(), $defaultFilterValues, array(
+            'action' => $this->generateUrl('commsy_group_list', array('roomId' => $roomId)),
+            'method' => 'GET',
+        ));
+
+        // check query for form data
+        if ($request->query->has($form->getName())) {
+            // manually bind values from the request
+            $form->submit($request->query->get($form->getName()));
+        }
+
+        // get the material manager service
+        $groupService = $this->get('commsy.group_service');
+
+        // set filter conditions in material manager
+        $groupService->setFilterConditions($form);
+
+        // get material list from manager service 
+        $groups = $groupService->getListGroups($roomId, $max, $start);
+
+        return array(
+            'roomId' => $roomId,
+            'groups' => $groups,
+        );
+    }
+    
+    /**
+     * @Route("/room/{roomId}/group/{itemId}")
+     * @Template()
+     */
+    public function detailAction($roomId, $itemId, Request $request)
+    {
+        $groupService = $this->get('commsy.group_service');
+        $group = $groupService->getGroup($itemId);
+        
+        $membersList = $group->getMemberItemList();
+        $members = $membersList->to_array();
+        
+        return array(
+            'group' => $group,
+            'members' => $members
         );
     }
 }
