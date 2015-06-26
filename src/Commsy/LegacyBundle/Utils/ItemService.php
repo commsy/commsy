d778824f41c7a4177ec5e5f2ca73297eb9923ffd
Christoph Schönfeld <schoenfeld@effective-webwork.de>
2015-06-26 11:23:53 +0200

    ItemVoter implementing SEE and EDIT permissions

--- src/Commsy/LegacyBundle/Utils/ItemService.php
+++ src/Commsy/LegacyBundle/Utils/ItemService.php
@@ -14,13 +14,33 @@ class ItemService
 
     public function __construct(LegacyEnvironment $legacyEnvironment)
     {
-        $this->legacyEnvironment = $legacyEnvironment;
-        $this->itemManager = $this->legacyEnvironment->getEnvironment()->getitemManager();
+        $this->legacyEnvironment = $legacyEnvironment->getEnvironment();
+        $this->itemManager = $this->legacyEnvironment->getItemManager();
     }
 
     public function getItem($itemId)
     {
         $item = $this->itemManager->getItem($itemId);
         return $item;
+    }
+
+    public function getTypedItem($itemId)
+    {
+        $item = $this->getItem($itemId);
+
+        if ($item) {
+            $type = $item->getItemType();
+
+            if ($type == 'label') {
+                $manager = $this->legacyEnvironment->getLabelManager();
+            } else {
+                $manager = $this->legacyEnvironment->getManager($type);
+            }
+
+            $item = $manager->getItem($item->getItemID());
+            return $item;
+        }
+
+        return null;
     }
 }
---
+++ src/CommsyBundle/Security/Authorization/Voter/ItemVoter.php
@@ -0,0 +1,86 @@
+<?php
+namespace CommsyBundle\Security\Authorization\Voter;
+
+use Symfony\Component\Security\Core\Authorization\Voter\VoterInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+use Commsy\LegacyBundle\Services\LegacyEnvironment;
+use Commsy\LegacyBundle\Utils\ItemService;
+
+class ItemVoter implements VoterInterface
+{
+    const SEE = 'ITEM_SEE';
+    const EDIT = 'ITEM_EDIT';
+
+    private $legacyEnvironment;
+    private $itemService;
+
+    public function __construct(LegacyEnvironment $legacyEnvironment, ItemService $itemService)
+    {
+        $this->legacyEnvironment = $legacyEnvironment->getEnvironment();
+        $this->itemService = $itemService;
+    }
+
+    public function supportsAttribute($attribute)
+    {
+        return in_array($attribute, array(
+            self::SEE,
+            self::EDIT,
+        ));
+    }
+
+    public function supportsClass($class)
+    {
+        return true;
+    }
+
+    public function vote(TokenInterface $token, $itemId, array $attributes)
+    {
+        // check if the voter is used correct, only allow one attribute
+        // this isn't a requirement, it's just one easy way for you to
+        // design your voter
+        if (1 !== count($attributes)) {
+            throw new \InvalidArgumentException('Only one attribute is allowed');
+        }
+
+        // set the attribute to check against
+        $attribute = $attributes[0];
+
+        // check if the given attribute is covered by this voter
+        if (!$this->supportsAttribute($attribute)) {
+            return VoterInterface::ACCESS_ABSTAIN;
+        }
+
+        // get current logged in user
+        // $user = $token->getUser();
+
+        // make sure there is a user object (i.e. that the user is logged in)
+        // if (!$user instanceof UserInterface) {
+        //     return VoterInterface::ACCESS_DENIED;
+        // }
+        
+        $currentUser = $this->legacyEnvironment->getCurrentUserItem();
+        $item = $this->itemService->getTypedItem($itemId);
+
+        if ($item) {
+            switch ($attribute) {
+                case self::SEE:
+                    if ($item->maySee($currentUser)) {
+                        return VoterInterface::ACCESS_GRANTED;
+                    }
+
+                    break;
+
+                case self::EDIT:
+                    if ($item->mayEdit($currentUser)) {
+                        return VoterInterface::ACCESS_GRANTED;
+                    }
+
+                    break;
+            }
+        }
+
+        return VoterInterface::ACCESS_DENIED;
+    }
+}
