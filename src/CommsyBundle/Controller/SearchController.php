7bfe3c0c9bb495017713e38cd5934e6d52a06031
Christoph Schönfeld <schoenfeld@effective-webwork.de>
2015-06-29 20:18:17 +0200

    whip search config / controller

--- src/CommsyBundle/Controller/SearchController.php
+++ src/CommsyBundle/Controller/SearchController.php
@@ -6,6 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+use Elastica\Query\Filtered;
+use Elastica\Query\MatchAll;
+use Elastica\Filter\Term;
+use Elastica\Filter\Range;
+use Elastica\Filter\Bool;
 
 class SearchController extends Controller
 {
@@ -21,5 +28,58 @@ class SearchController extends Controller
         $user = $finder->find($searchTerm);
         dump($user);
         return array('user' => $user);
+    }
+
+    /**
+    * @Route("/room/{roomId}/search/quick")
+    * @Template
+    */
+    public function quickAction($roomId, Request $request)
+    {
+        $results = array(
+        );
+
+        $query = $request->get('search', null);
+
+        if ($query) {
+            $finder = $this->get('fos_elastica.finder.commsy.user');
+
+            $boolFilter = new Bool();
+
+            $contextTerm = new Term();
+            $contextTerm->setTerm('contextId', $roomId);
+            $boolFilter->addMust($contextTerm);
+
+            $boolFilter->addMust(new Range('status', array('gte' => 2)));
+
+            $filteredQuery = new Filtered();
+            $filteredQuery->setQuery(new MatchAll());
+            $filteredQuery->setFilter($boolFilter);
+
+            
+
+            $users = $finder->find($filteredQuery);
+
+            $dataset = array();
+
+            foreach ($users as $user) {
+                $username = $user->getFirstname() . ' ' . $user->getLastname();
+
+                $dataset = array(
+                    'title' => $username,
+                    'text' => '',
+                    'url' => '#',
+                );
+
+                $results[] = $dataset;
+            }
+        }
+
+        $response = new JsonResponse();
+        $response->setData(array(
+            'results' => $results,
+        ));
+
+        return $response;
     }
 }
