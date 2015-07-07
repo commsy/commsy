6590e49ad90271deb13a0823bd5c788d71acdda9
Christoph Schönfeld <schoenfeld@effective-webwork.de>
2015-07-08 00:05:53 +0200

    upload.js fixed and progress implementation added

--- src/CommsyBundle/Resources/public/js/upload.js
+++ src/CommsyBundle/Resources/public/js/upload.js
@@ -4,19 +4,7 @@
 
     var settings = {
         action: '/',
-        allow: '*.*',
-
-        loadstart: function() {
-
-        },
-
-        progress: function(percent) {
-
-        },
-
-        allcomplete: function(response) {
-
-        }
+        allow: '*.*'
     };
 
     // var select = UIkit.uploadSelect($("#upload-select"), settings);
@@ -24,9 +12,33 @@
 
     UIkit.on('beforeready.uk.dom', function() {
         $('.upload').each(function() {
-            var select = UI.uploadSelect(this, settings);
-            var drop = UI.uploadDrop($(this).find('input'), settings);
+            var progressbar = $(this).siblings('.uk-progress').first();
+            var bar = progressbar.find('.uk-progress-bar');
 
+            var callbacks = {
+                loadstart: function() {
+                    bar.css("width", "0%").text("0%");
+                    progressbar.removeClass("uk-hidden");
+                },
+
+                progress: function(percent) {
+                    percent = Math.ceil(percent);
+                    bar.css("width", percent+"%").text(percent+"%");
+                },
+
+                allcomplete: function(response) {
+                    bar.css("width", "100%").text("100%");
+
+                    setTimeout(function(){
+                        progressbar.addClass("uk-hidden");
+                    }, 250);
+                }
+            };
+
+            var merged = $.extend(settings, callbacks);
+
+            var select = UI.uploadSelect($(this).find('input'), merged);
+            var drop = UI.uploadDrop(this, merged);
         });
     });
 
