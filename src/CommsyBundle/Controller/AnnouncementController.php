67c7d1a980cc23671303517a5ee1f750664131eb
Johannes Schultze <schultze@effective-webwork.de>
2015-07-13 12:46:00 +0200

    Announcements

--- src/CommsyBundle/Controller/AnnouncementController.php
+++ src/CommsyBundle/Controller/AnnouncementController.php
@@ -7,25 +7,119 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Request;
 
+use CommsyBundle\Filter\AnnouncementFilterType;
+
 class AnnouncementController extends Controller
 {
     /**
-     * @Route("/room/{roomId}/announcement/{announcementId}")
+     * @Route("/room/{roomId}/material/feed/{start}")
      * @Template()
      */
-    public function indexAction($roomId, $announcementId, Request $request)
-    {   
-        return array();
-    }
+    public function feedAction($roomId, $max = 10, $start = 0, Request $request)
+    {
+        // setup filter form
+        $defaultFilterValues = array(
+            'activated' => true
+        );
+        $filterForm = $this->createForm(new AnnouncementFilterType(), $defaultFilterValues, array(
+            'action' => $this->generateUrl('commsy_announcement_list', array('roomId' => $roomId)),
+        ));
 
+        // get the material manager service
+        $announcementService = $this->get('commsy_legacy.announcement_service');
+
+        // apply filter
+        $filterForm->handleRequest($request);
+        if ($filterForm->isValid()) {
+            // set filter conditions in material manager
+            $announcementService->setFilterConditions($filterForm);
+        }
+
+        // get material list from manager service 
+        $announcements = $announcementService->getListAnnouncements($roomId, $max, $start);
+
+        $readerService = $this->get('commsy.reader_service');
+
+        $readerList = array();
+        foreach ($announcements as $item) {
+            $reader = $readerService->getLatestReader($item->getItemId());
+            if ( empty($reader) ) {
+               $readerList[$item->getItemId()] = 'new';
+            } elseif ( $reader['read_date'] < $item->getModificationDate() ) {
+               $readerList[$item->getItemId()] = 'changed';
+            }
+        }
+
+        return array(
+            'roomId' => $roomId,
+            'announcements' => $announcements,
+            'readerList' => $readerList
+        );
+    }
+    
     /**
      * @Route("/room/{roomId}/announcement")
      * @Template()
      */
     public function listAction($roomId, Request $request)
     {
-    	return array(
-            'roomId' => $roomId
+        // setup filter form
+        $defaultFilterValues = array(
+            'activated' => true
+        );
+        $filterForm = $this->createForm(new AnnouncementFilterType(), $defaultFilterValues, array(
+            'action' => $this->generateUrl('commsy_announcement_list', array('roomId' => $roomId)),
+        ));
+
+        // get the material manager service
+        $announcementService = $this->get('commsy_legacy.announcement_service');
+
+        // apply filter
+        $filterForm->handleRequest($request);
+        if ($filterForm->isValid()) {
+            // set filter conditions in material manager
+            $announcementService->setFilterConditions($filterForm);
+        }
+
+        return array(
+            'roomId' => $roomId,
+            'form' => $filterForm->createView(),
+        );
+    }
+    
+    /**
+     * @Route("/room/{roomId}/material/{itemId}")
+     * @Template()
+     */
+    public function detailAction($roomId, $itemId, Request $request)
+    {
+        $announcementService = $this->get('commsy_legacy.announcement_service');
+        $itemService = $this->get('commsy.item_service');
+        
+        $announcement = $announcementService->getAnnouncement($itemId);
+        
+        $itemArray = array($announcement);
+
+        $readerService = $this->get('commsy.reader_service');
+        
+        $readerList = array();
+        $modifierList = array();
+        foreach ($itemArray as $item) {
+            $reader = $readerService->getLatestReader($item->getItemId());
+            if ( empty($reader) ) {
+               $readerList[$item->getItemId()] = 'new';
+            } elseif ( $reader['read_date'] < $item->getModificationDate() ) {
+               $readerList[$item->getItemId()] = 'changed';
+            }
+            
+            $modifierList[$item->getItemId()] = $itemService->getAdditionalEditorsForItem($item);
+        }
+        
+        return array(
+            'roomId' => $roomId,
+            'announcement' => $announcementService->getAnnouncement($itemId),
+            'readerList' => $readerList,
+            'modifierList' => $modifierList
         );
     }
 }
