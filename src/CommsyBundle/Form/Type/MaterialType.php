dbd5fb8d2eda8b57c8823d0c513483826831a509
Johannes Schultze <schultze@effective-webwork.de>
2015-07-17 15:36:19 +0200

    Material form: start of dynamic form for bibliographic information.

--- src/CommsyBundle/Form/Type/MaterialType.php
+++ src/CommsyBundle/Form/Type/MaterialType.php
@@ -5,11 +5,13 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Doctrine\ORM\EntityManager;
 
 use Commsy\LegacyBundle\Services\LegacyEnvironment;
+use CommsyBundle\Entity\Materials;
 
 class MaterialType extends AbstractType
 {
@@ -40,6 +42,21 @@ class MaterialType extends AbstractType
                 ),
                 'translation_domain' => 'material',
             ))
+            ->add('biblio_select', 'choice', array(
+                'choices'  => array(
+                    'thesis' => 'Thesis',
+                    'term' => 'Term paper'
+                ),
+                'required' => false,
+            ))
+            ->add('biblio', 'textarea', array(
+                'label' => 'Biblio',
+                'attr' => array(
+                    'placeholder' => 'Biblio',
+                    'class' => 'uk-form-width-large',
+                ),
+                'translation_domain' => 'material',
+            ))
             ->add('files', 'file', array(
                 'label' => 'Files',
                 'attr' => array(
@@ -57,6 +74,39 @@ class MaterialType extends AbstractType
                 'translation_domain' => 'form',
             ))
         ;
+        
+        
+        $formModifier = function (FormInterface $form, Materials $material = null) {
+            $form->add('biblio', 'textarea', array(
+                'label' => 'Biblio',
+                'attr' => array(
+                    'placeholder' => 'Biblio',
+                    'class' => 'uk-form-width-large',
+                ),
+                'translation_domain' => 'material',
+            ));
+        };
+
+        $builder->addEventListener(
+            FormEvents::PRE_SET_DATA,
+            function (FormEvent $event) use ($formModifier) {
+                $formModifier($event->getForm(), new Materials());
+            }
+        );
+
+        $builder->get('biblio_select')->addEventListener(
+            FormEvents::POST_SUBMIT,
+            function (FormEvent $event) use ($formModifier) {
+                // It's important here to fetch $event->getForm()->getData(), as
+                // $event->getData() will get you the client data (that is, the ID)
+                $material = $event->getForm()->getData();
+
+                // since we've added the listener to the child, we'll have to pass on
+                // the parent to the callback functions!
+                $formModifier($event->getForm()->getParent(), $material);
+            }
+        );
+        
     }
 
     public function configureOptions(OptionsResolver $resolver)
