fb5d8dd10efade9f3e4ff721ff31afb52c3e79d6
Christopher Timm <timm@effective-webwork.de>
2016-01-27 22:32:53 +0100

    linked form layout

--- app/Resources/assets/js/linked_items.js
+++ app/Resources/assets/js/linked_items.js
@@ -35,37 +35,10 @@
         onClickItem: function(element) {
             var $this = this;
 
-            // console.log(this.options);
-
             // toggle linked items
-            if (element.parents('#linksSubTab')[0]) {
-                $("#itemsLinkedList").append(element[0]);
-
-                if (element.hasClass('cs-linked-item-selected')) {
-                    element.removeClass('cs-linked-item-selected');
-                }
-
-                // element.addClass('cs-linked-item-selected');
-                element.children('div').addClass('cs-linked-item-selected');
-                // element.children('div').toggleClass('cs-linked-item');
-
-                //unset checkbox
-                element.find('input[type="checkbox"]').prop("checked", true);
-
-            } else if (element.parents('#itemsLinkedListWrapper')[0]) {
-                $("#linksSubTab ul").append(element[0]);
-
-                element.children('div').removeClass('cs-linked-item-selected');
-                // element.children('div').removeClass('cs-linked-item');
-                // remove grid style
-                element.removeAttr('style');
-                element.removeAttr('data-grid-prepared');
-
-                // set checkbox
-                element.find  ('input[type="checkbox"]').prop("checked", false);
-            }
-
-
+            var checkbox = element.find('input[type=checkbox]')[0];
+            $(checkbox).prop("checked", !$(checkbox).prop("checked"));
+            
         }
     });
 
