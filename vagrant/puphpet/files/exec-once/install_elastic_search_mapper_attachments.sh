cb64131563d3deb899463ab8225d11089491d56d
Christoph Schönfeld <schoenfeld@effective-webwork.de>
2016-04-08 22:48:54 +0200

    vagrant / elastic search

--- vagrant/puphpet/files/exec-once/install_elastic_search_mapper_attachments.sh
+++ vagrant/puphpet/files/exec-once/install_elastic_search_mapper_attachments.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/usr/share/elasticsearch/bin/plugin install elasticsearch/elasticsearch-mapper-attachments/2.4.3
+/usr/share/elasticsearch/bin/plugin install elasticsearch/elasticsearch-mapper-attachments/3.1.2
