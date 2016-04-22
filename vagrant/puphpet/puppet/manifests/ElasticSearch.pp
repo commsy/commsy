809d0a361bf19a73a9557fc3f9ad1999abce7cc1
Christoph Schönfeld <schoenfeld@effective-webwork.de>
2016-04-22 18:27:31 +0200

    elastic fix

--- vagrant/puphpet/puppet/manifests/ElasticSearch.pp
+++ vagrant/puphpet/puppet/manifests/ElasticSearch.pp
@@ -7,6 +7,28 @@ class puphpet_elasticsearch (
   }
 
   $settings = $elasticsearch['settings']
+  $version  = $elasticsearch['settings']['version']
+
+  $url_base = 'https://download.elasticsearch.org/elasticsearch/elasticsearch'
+
+  case $::osfamily {
+    'debian': {
+      $url = "${url_base}/elasticsearch-${version}.deb"
+    }
+    'redhat': {
+      $noarch_rpm = ''
+
+      # Versions less than 2.0.0 have .noarch on the rpm
+      if $version =~ /^1\./ {
+        $noarch_rpm = '.noarch'
+      }
+
+      $url = "${url_base}/elasticsearch-${version}${noarch_rpm}.rpm"
+    }
+    default: {
+      fail('Unrecognized operating system for Elastic Search')
+    }
+  }
 
   if ! defined(Class['java']) and $settings['java_install'] {
     class { 'java':
@@ -14,10 +36,11 @@ class puphpet_elasticsearch (
     }
   }
 
-  $merged = merge($settings, {
+  $merged = delete(merge($settings, {
     'java_install' => false,
+    'package_url'  => $url,
     require        => Class['puphpet::firewall::post'],
-  })
+  }), 'version')
 
   create_resources('class', { 'elasticsearch' => $merged })
 
