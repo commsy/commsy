a531be17c456c7f83c407d02fa276f41fa56d6c5
Johannes Schultze <schultze@effective-webwork.de>
2016-07-01 16:07:38 +0200

    #2887.

---
+++ src/CommsyBundle/Form/Type/DateDetailType.php
@@ -0,0 +1,94 @@
+<?php
+namespace CommsyBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
+use CommsyBundle\Form\Type\Custom\DateTimeSelectType;
+
+use CommsyBundle\Form\Type\Event\AddBibliographicFieldListener;
+
+class DateDetailType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('start', DateTimeSelectType::class, array(
+                'constraints' => array(
+                ),
+                'label' => 'startdate',
+                'attr' => array(
+                    'placeholder' => 'startdate',
+                    'class' => 'uk-form-width-medium',
+                ),
+                'translation_domain' => 'material',
+            ))
+            ->add('end', DateTimeSelectType::class, array(
+                'constraints' => array(
+                ),
+                'label' => 'enddate',
+                'attr' => array(
+                    'placeholder' => 'enddate',
+                    'class' => 'uk-form-width-medium',
+                ),
+                'translation_domain' => 'material',
+            ))
+            ->add('place', TextType::class, array(
+                'constraints' => array(
+                ),
+                'label' => 'place',
+                'attr' => array(
+                    'placeholder' => 'place',
+                    'class' => 'uk-form-width-medium',
+                ),
+                'translation_domain' => 'material',
+            ))
+            ->add('save', SubmitType::class, array(
+                'attr' => array(
+                    'class' => 'uk-button-primary',
+                ),
+                'label' => 'save',
+                'translation_domain' => 'form',
+            ))
+            ->add('cancel', SubmitType::class, array(
+                'attr' => array(
+                    'class' => 'uk-button-primary',
+                    'formnovalidate' => '',
+                ),
+                'label' => 'cancel',
+                'translation_domain' => 'form',
+            ))
+        ;
+        
+    }
+
+    /**
+     * Configures the options for this type.
+     * 
+     * @param  OptionsResolver $resolver The resolver for the options
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver
+            ->setRequired([])
+        ;
+    }
+
+    /**
+     * Returns the prefix of the template block name for this type.
+     * The block prefix defaults to the underscored short class name with the "Type" suffix removed
+     * (e.g. "UserProfileType" => "user_profile").
+     * 
+     * @return string The prefix of the template block name
+     */
+    public function getBlockPrefix()
+    {
+        return 'date';
+    }
+}
