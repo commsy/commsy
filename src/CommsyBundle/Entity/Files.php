1123013d92d42b4b8a0ef8641ce739365341e3a3
Christopher Timm <timm@effective-webwork.de>
2016-07-01 14:40:52 +0200

    remove base64 encoding

--- src/CommsyBundle/Entity/Files.php
+++ src/CommsyBundle/Entity/Files.php
@@ -124,14 +124,12 @@ class Files
     {
         $fileExt = substr(strrchr($this->filename,'.'),1);
         $filePath = 'files/' . $this->room->getContextId() . '/' . $this->contextId . '_/' . $this->filesId. '.' . $fileExt;
-        
+
         if (file_exists($filePath)) {
-            return base64_encode(
-                    file_get_contents(
+            return file_get_contents(
                         $filePath, 
                         'r'
-                    )
-                );
+                    );
         } else {
             return null;
         }
