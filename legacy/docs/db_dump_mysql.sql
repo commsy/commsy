0517a517e2bf58f2981f5476e5aec23be86cac6c
Christoph Schönfeld <schoenfeld@effective-webwork.de>
2016-07-28 23:18:43 +0200

    log_error table removed

--- legacy/docs/db_dump_mysql.sql
+++ legacy/docs/db_dump_mysql.sql
@@ -575,30 +575,6 @@ CREATE TABLE `log_archive` (
 
 
 
-# Export von Tabelle log_error
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `log_error`;
-
-CREATE TABLE `log_error` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `datetime` datetime NOT NULL,
-  `number` int(11) NOT NULL,
-  `type` varchar(255) NOT NULL,
-  `message` mediumtext NOT NULL,
-  `url` varchar(255) DEFAULT NULL,
-  `referer` varchar(255) DEFAULT NULL,
-  `file` varchar(255) NOT NULL,
-  `line` int(11) NOT NULL,
-  `context` int(11) NOT NULL,
-  `module` varchar(255) NOT NULL,
-  `function` varchar(255) NOT NULL,
-  `user` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
-
-
 # Export von Tabelle materials
 # ------------------------------------------------------------
 
