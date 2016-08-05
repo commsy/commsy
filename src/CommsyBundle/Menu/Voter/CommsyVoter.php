a77ecd8f8e1319a505a20ff497213bb96dc05b0b
Johannes Schultze <schultze@effective-webwork.de>
2016-08-05 22:17:37 +0200

    FEATURE #52 - Markierung der Rubrik in "Navigation"/"CommSy-Pfosten".

---
+++ src/CommsyBundle/Menu/Voter/CommsyVoter.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace CommsyBundle\Menu\Voter;
+
+use Knp\Menu\ItemInterface;
+use Knp\Menu\Matcher\Voter\VoterInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+/**
+ * Voter based on the uri
+ */
+class CommsyVoter implements VoterInterface
+{
+    private $requestStack;
+
+    public function __construct(RequestStack $requestStack)
+    {
+        $this->requestStack = $requestStack;
+    }
+
+    public function matchItem(ItemInterface $item)
+    {
+        $roomId = $this->requestStack->getCurrentRequest()->attributes->get('roomId');
+        $controllerArray = explode('_', $this->requestStack->getCurrentRequest()->attributes->get('_route'));
+
+        if ($controllerArray[2] == 'detail') {
+            if (stristr($item->getUri(), 'room/'.$roomId.'/'.$controllerArray[1])) {
+                return true;
+            }
+        } else {
+            if (stristr($item->getUri(), 'room/'.$roomId.'/'.$controllerArray[1].'/'.$controllerArray[2])) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
