9b96b72da5eb673f28e74bf5c6385f8231c36694
Arved Solth <solth@effective-webwork.de>
2017-01-04 14:31:45 +0100

    BUG #318 (various bugs in rubric 'Institutionen') fixed

---
+++ src/Commsy/LegacyBundle/Utils/InstitutionService.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Commsy\LegacyBundle\Utils;
+
+use Symfony\Component\Form\Form;
+
+use Commsy\LegacyBundle\Services\LegacyEnvironment;
+
+class InstitutionService
+{
+    private $legacyEnvironment;
+
+    private $institutionManager;
+
+    public function __construct(LegacyEnvironment $legacyEnvironment)
+    {
+        $this->legacyEnvironment = $legacyEnvironment;
+
+        $this->institutionManager = $this->legacyEnvironment->getEnvironment()->getInstitutionManager();
+        $this->institutionManager->reset();
+    }
+
+
+    public function getCountArray($roomId)
+    {
+        $this->institutionManager->setContextLimit($roomId);
+        $this->institutionManager->select();
+        $countInstitution = array();
+        $countInstitutionArray['count'] = sizeof($this->institutionManager->get()->to_array());
+        $this->institutionManager->select();
+        $countInstitutionArray['countAll'] = $this->institutionManager->getCountAll();
+
+        return $countInstitutionArray;
+    }
+
+
+    public function getInstitution($itemId)
+    {
+        $institution = $this->institutionManager->getItem($itemId);
+        return $institution;
+    }
+
+    public function getListInstitutions($roomId, $max = NULL, $start = NULL, $sort = NULL)
+    {
+        $this->institutionManager->setContextLimit($roomId);
+        if ($max !== NULL && $start !== NULL) {
+            $this->institutionManager->setIntervalLimit($start, $max);
+        }
+
+        if ($sort) {
+            $this->institutionManager->setSortOrder($sort);
+        }
+
+        $this->institutionManager->select();
+        $institutionList = $this->institutionManager->get();
+
+        return $institutionList->to_array();
+    }
+
+    public function setFilterConditions(Form $filterForm)
+    {
+        $formData = $filterForm->getData();
+
+        // activated
+        if ($formData['activated']) {
+            $this->institutionManager->showNoNotActivatedEntries();
+        }
+    }
+
+    public function getNewInstitution()
+    {
+        $institution = $this->institutionManager->getNewItem();
+        $institution->setLabelType(CS_INSTITUTION_TYPE);
+        return $institution;
+    }
+}
