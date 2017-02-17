57c3c08f69e2f000a281f87cdc48ec1b7edbcc8c
Christoph Schönfeld <schoenfeld@effective-webwork.de>
2017-02-17 20:23:18 +0100

    js edit can now handle html responses indicating invalid form states

--- app/Resources/assets/js/edit.js
+++ app/Resources/assets/js/edit.js
@@ -42,7 +42,7 @@
                 });
 
             $this.registerArticleEvents(element);
-            
+
             if ($this.options.draft == '1') {
                 $this.onClickEdit($(element).find('div.cs-edit'));
             }
@@ -100,91 +100,121 @@
                 // replace article html
                 article.html($(result));
 
-                let buttonpressed;
-                $('button').click(function() {
-                    buttonpressed = $(this).attr('name');
-                });
+                $this.handleFormSubmit(article);
 
-                // override form submit behaviour
-                article.find('form').submit(function (event) {
+            });
+        },
+
+        handleFormSubmit: function(article) {
+            let $this = this;
+
+            // override form submit behaviour
+            article.find('button').click(function (event) {
+                let $button = $(this);
+
+                // cancel is not handled via ajax
+                if ($button.attr('name').includes('cancel')) {
                     event.preventDefault();
 
-                    editButtons.addClass('cs-edit');
-                    $("#cs-additional-actions")
-                        .removeClass('uk-hidden')
-                        .parent().find("button.uk-button").removeClass("uk-text-muted");
+                    // cancel editing a NEW entry => return to list view
+                    if($("#breadcrumb-nav .current.last").text().trim() == "") {
+                        let pathParts = window.location.pathname.split("/");
+                        pathParts.pop();
+                        window.location.href = pathParts.join("/");
+                    }
+                    // cancel editing an EXISTING entry => return to detail view of the entry
+                    else {
+                        // trigger reload of the current URL
+                        // We are using the Location.reload() method, since
+                        // setting window.location.href might not result in a reload, if
+                        // there is an anchor currently set
+                        window.location.reload(true);
+                    }
+                } else {
+                    let form = $(this).closest('form');
+                    if (form[0].checkValidity()) {
+                        event.preventDefault();
 
-                    editButtons.each(function(){
-                        $(this).find('a').attr('title', $(this).data('edit-title'));
-                    });
+                        // editButtons.addClass('cs-edit');
+                        // $("#cs-additional-actions")
+                        //     .removeClass('uk-hidden')
+                        //     .parent().find("button.uk-button").removeClass("uk-text-muted");
+                        //
+                        // editButtons.each(function(){
+                        //     $(this).find('a').attr('title', $(this).data('edit-title'));
+                        // });
 
-                    $(article).find('.cs-save-spinner').toggleClass('uk-hidden', false);
+                        // $(article).find('.cs-save-spinner').toggleClass('uk-hidden', false);
 
-                    $('div[id^="cke_"]div[role="application"]').each(function () {
-                       let $textarea = $(this).attr('id').replace('cke_', '');
-                       $('#'+$textarea).val(CKEDITOR.instances[$textarea].getData());
-                    });
+                        // sync cke content
+                        $('div[id^="cke_"]div[role="application"]').each(function () {
+                            let $textarea = $(this).attr('id').replace('cke_', '');
+                            $('#'+$textarea).val(CKEDITOR.instances[$textarea].getData());
+                        });
 
-                    if (buttonpressed.includes("cancel")) {
-                        // cancel editing a NEW entry => return to list view
-                        if($("#breadcrumb-nav .current.last").text().trim() == "") {
-                            let pathParts = window.location.pathname.split("/");
-                            pathParts.pop();
-                            window.location.href = pathParts.join("/");
-                        }
-                        // cancel editing an EXISTING entry => return to detail view of the entry
-                        else {
-                            // trigger reload of the current URL
-                            // We are using the Location.reload() method, since
-                            // setting window.location.href might not result in a reload, if
-                            // there is an anchor currently set
-                            window.location.reload(true);
-                        }
-                    } else{
+                        let formData = form.serializeArray();
+                        formData.push({ name: this.name, value: this.value });
+
                         // submit the form manually
                         $.ajax({
                             url: $this.options.editUrl,
                             type: "POST",
-                            data: $(this).serialize()+'&'+buttonpressed+'=true'
+                            data: formData
                         })
                         .done(function(result, statusText, xhrObject) {
-                            article.html($(result));
+                            let $result = $(result);
 
-                            $this.registerArticleEvents(article);
-
-                            let title = $(result).find('.uk-article-title');
-                            if (title !== null && title.text()) {
-                                // material/todo/discussion title edited
-                                if($this.options.editUrl.includes(window.location.pathname.split("/").pop())) {
-                                    $('.uk-breadcrumb').find('.last').find('span').html(title.text());
-                                }
-                                // section/step/article title edited
-                                else {
-                                    let editParts = $this.options.editUrl.split("/");
-                                    let anchor = $("a[href='#" + partMapping[editParts[editParts.length-3]] + editParts[editParts.length-2] + "']");
-                                    anchor.text(anchor.html().trim().split(" ")[0] + " " + title.text());
-                                }
+                            if ($result.find('ul.form-errors').length) {
+                                article.html($result);
+                                $this.handleFormSubmit(article);
+                            } else {
+                                window.location.reload(true);
                             }
 
-                            let workflow = $(result).find('.cs-workflow-traffic-light').html();
-                            if (workflow !== null) {
-                                $('.uk-article').find('.cs-workflow-traffic-light').html(workflow);
-                            }
 
-                            let sections = $(result).find('#section-list');
-                            if (sections !== null) {
-                                let counter = 0;
-                                sections.find("li").each(function() {
-                                    let section_container = $($(this).find("a:first").attr('href')).closest('article').parent().detach();
-                                    section_container.attr("id", "section_"+counter);
-                                    $("#section-content").append( section_container );
-                                    counter++;
-                                })
-                            }
-                            window.location.reload(true);
+
+
+
+
+                            // $this.registerArticleEvents(article);
+                            //
+                            // let title = $(result).find('.uk-article-title');
+                            // if (title !== null && title.text()) {
+                            //     // material/todo/discussion title edited
+                            //     if($this.options.editUrl.includes(window.location.pathname.split("/").pop())) {
+                            //         $('.uk-breadcrumb').find('.last').find('span').html(title.text());
+                            //     }
+                            //     // section/step/article title edited
+                            //     else {
+                            //         let editParts = $this.options.editUrl.split("/");
+                            //         let anchor = $("a[href='#" + partMapping[editParts[editParts.length-3]] + editParts[editParts.length-2] + "']");
+                            //         anchor.text(anchor.html().trim().split(" ")[0] + " " + title.text());
+                            //     }
+                            // }
+                            //
+                            // let workflow = $(result).find('.cs-workflow-traffic-light').html();
+                            // if (workflow !== null) {
+                            //     $('.uk-article').find('.cs-workflow-traffic-light').html(workflow);
+                            // }
+                            //
+                            // let sections = $(result).find('#section-list');
+                            // if (sections !== null) {
+                            //     let counter = 0;
+                            //     sections.find("li").each(function() {
+                            //         let section_container = $($(this).find("a:first").attr('href')).closest('article').parent().detach();
+                            //         section_container.attr("id", "section_"+counter);
+                            //         $("#section-content").append( section_container );
+                            //         counter++;
+                            //     })
+                            // }
+                            //
+                            // // only redirect if there was no error
+                            // if (!$(result).find('ul.form-errors').length) {
+                            //     window.location.reload(true);
+                            // }
                         });
                     }
-                });
+                }
             });
         }
     });
