47bed6496137922511b81ce1e243982e92288cac
Johannes Schultze <schultze@effective-webwork.de>
2017-02-27 21:49:40 +0100

    Mediawiki.

--- src/CommsyMediawikiBundle/Services/MediawikiService.php
+++ src/CommsyMediawikiBundle/Services/MediawikiService.php
@@ -5,22 +5,37 @@ namespace CommsyMediawikiBundle\Services;
 use Symfony\Component\Form\Form;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
+use Commsy\LegacyBundle\Services\LegacyEnvironment;
+
 class MediawikiService
 {
     protected $wikiApiUrl;
+
+    protected $legacyEnvironment;
     
-    public function __construct(ContainerInterface $container)
+    public function __construct(ContainerInterface $container, LegacyEnvironment $legacyEnvironment)
     {
         $this->container = $container;
         $this->wikiApiUrl = $this->container->getParameter('commsy.mediawiki.url').$this->container->getParameter('commsy.mediawiki.apiPath');
+
+        $this->legacyEnvironment = $legacyEnvironment;
     }
 
     public function createWiki($roomId)
     {
-        $url = $this->wikiApiUrl.'?action=commsy&function=createwiki&session-id=123&context-id=123&format=json';
+        $url = $this->wikiApiUrl.'?action=commsy&function=createwiki&session-id='.$this->legacyEnvironment->getEnvironment()->getSessionID().'&context-id='.$roomId.'&format=json';
         
         $restClient = $this->container->get('circle.restclient');
         
+        $json = $restClient->get($url)->getContent();
+    }
+
+    public function deleteWiki($roomId)
+    {
+        $url = $this->wikiApiUrl.'?action=commsy&function=deletewiki&session-id='.$this->legacyEnvironment->getEnvironment()->getSessionID().'&context-id='.$roomId.'&format=json';
+
+        $restClient = $this->container->get('circle.restclient');
+
         $json = $restClient->get($url)->getContent();
     }
 }
