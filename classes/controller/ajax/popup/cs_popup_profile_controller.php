f1142217f76ecb9672120c70889d674913f5e10c
Johannes Schultze <schultze@effective-webwork.de>
2017-05-20 14:14:37 +0200

    #2623.

--- classes/controller/ajax/popup/cs_popup_profile_controller.php
+++ classes/controller/ajax/popup/cs_popup_profile_controller.php
@@ -112,6 +112,11 @@ class cs_popup_profile_controller implements cs_popup_controller {
 
 						$current_user->reject();
 						$current_user->save();
+                        // remove link from group room
+                        if($current_context->isGroupRoom()) {
+                            $group_item = $current_context->getLinkedGroupItem();
+                            $group_item->removeMember($current_user->getRelatedUserItemInContext($group_item->getContextID()));
+                        }
 
 						// set return
 						$this->_popup_controller->setSuccessfullItemIDReturn($current_user->getItemID());
