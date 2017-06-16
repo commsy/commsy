ac8d916c18ba25225a2052c98ea8cc32c9f7bee8
Johannes Schultze <schultze@effective-webwork.de>
2017-06-16 22:56:07 +0200

    Added form to edit calendars.

--- src/CommsyBundle/Controller/CalendarController.php
+++ src/CommsyBundle/Controller/CalendarController.php
@@ -3,8 +3,63 @@
 namespace CommsyBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Component\HttpFoundation\Request;
+
+use CommsyBundle\Form\Type\CalendarEditType;
+use CommsyBundle\Entity\Calendars;
+
+use CommsyBundle\Event\CommsyEditEvent;
 
 class CalendarController extends Controller
 {
+    /**
+     * @Route("/room/{roomId}/calendar/edit/{calendarId}")
+     * @Template()
+     * @Security("is_granted('CALENDARS_EDIT')")
+     */
+    public function editAction($roomId, $calendarId = null, Request $request)
+    {
+        $legacyEnvironment = $this->get('commsy_legacy.environment')->getEnvironment();
 
+        $roomManager = $legacyEnvironment->getRoomManager();
+        $roomItem = $roomManager->getItem($roomId);
+
+        $em = $this->getDoctrine()->getManager();
+        $repository = $em->getRepository('CommsyBundle:Calendars');
+
+        if ($calendarId) {
+            $calendar = $repository->findOneById($calendarId);
+        } else {
+            $calendar = new Calendars();
+            $calendar->setContextId($roomId);
+        }
+
+        $editForm = $this->createForm(CalendarEditType::class, $calendar);
+
+
+        $editForm->handleRequest($request);
+        if ($editForm->isValid()) {
+            // persist changes / delete hashtag
+
+            return $this->redirectToRoute('commsy_calendar_edit', [
+                'roomId' => $roomId,
+            ]);
+        }
+
+        $calendars = $repository->findAll($roomId);
+
+        $dispatcher = $this->get('event_dispatcher');
+        $dispatcher->dispatch('commsy.edit', new CommsyEditEvent(null));
+
+        return [
+            'editForm' => $editForm->createView(),
+            'roomId' => $roomId,
+            'calendars' => $calendars,
+            'calendarId' => $calendarId,
+        ];
+    }
 }
---
+++ src/CommsyBundle/Repository/CalendarsRepository.php
@@ -0,0 +1,8 @@
+<?php
+namespace CommsyBundle\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+class CalendarsRepository extends EntityRepository
+{
+}
