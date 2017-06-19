516a9672ef9c4a72f9b11e71e10e866683665a81
Johannes Schultze <schultze@effective-webwork.de>
2017-06-19 21:40:05 +0200

    Migration creates calendars.

--- app/DoctrineMigrations/Version20170616103508.php
+++ app/DoctrineMigrations/Version20170616103508.php
@@ -31,7 +31,24 @@ class Version20170616103508 extends AbstractMigration
         ');
 
         $this->addSql('
+            CREATE TABLE zzz_calendars (
+                id int(11) NOT NULL AUTO_INCREMENT,
+                context_id int(11) NOT NULL,
+                title varchar(255) NOT NULL,
+                color varchar(255) NOT NULL,
+                external_url varchar(255) NULL,
+                default_calendar TINYINT NOT NULL DEFAULT 0,
+                PRIMARY KEY (id)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8
+        ');
+
+        $this->addSql('
             ALTER TABLE dates
+            ADD calendar_id int(11) NULL;
+        ');
+
+        $this->addSql('
+            ALTER TABLE zzz_dates
             ADD calendar_id int(11) NULL;
         ');
     }
@@ -46,9 +63,95 @@ class Version20170616103508 extends AbstractMigration
 
         $this->addSql('DROP TABLE IF EXISTS calendars');
 
+        $this->addSql('DROP TABLE IF EXISTS zzz_calendars');
+
         $this->addSql('
             ALTER TABLE dates
             DROP calendar_id;
         ');
+
+        $this->addSql('
+            ALTER TABLE zzz_dates
+            DROP calendar_id;
+        ');
+    }
+
+    public function postUp(Schema $schema) {
+        $this->migrateColorsToCalendars('room');
+
+        $this->migrateColorsToCalendars('zzz_room');
+    }
+
+    private function migrateColorsToCalendars ($dTable) {
+        $queryBuilder = $this->connection->createQueryBuilder();
+
+        // find all rooms
+        $qb = $queryBuilder
+            ->select('r.item_id')
+            ->from($dTable, 'r');
+        $rooms = $qb->execute();
+
+        // Add calendars for each room -> one default + one for each color
+        foreach ($rooms as $room) {
+            // default calendar
+
+            $this->write('updating calendars for room '.$room['item_id']);
+
+            $queryBuilder
+                ->insert('calendars')
+                ->values(
+                    array(
+                        'id' => '?',
+                        'context_id' => '?',
+                        'title' => '?',
+                        'color' => '?',
+                        'external_url' => '?',
+                        'default_calendar' => '?'
+                    )
+                )
+                ->setParameter(0, '')
+                ->setParameter(1, $room['item_id'])
+                ->setParameter(2, 'Calendar')
+                ->setParameter(3, '#ffffff')
+                ->setParameter(4, '')
+                ->setParameter(5, '1')
+                ->execute();
+            ;
+
+            $colorArray = [
+                            '#999999' => 'Grey',
+                            '#CC0000' => 'Red',
+                            '#FF6600' => 'Orange',
+                            '#FFCC00' => 'Gold',
+                            '#FFFF66' => 'Yellow',
+                            '#33CC00' => 'Green',
+                            '#00CCCC' => 'Turquoise',
+                            '#3366FF' => 'Blue',
+                            '#6633FF' => 'Purple',
+                            '#CC33CC' => 'Magenta'
+                          ];
+            foreach ($colorArray as $color => $name) {
+                $queryBuilder
+                    ->insert('calendars')
+                    ->values(
+                        array(
+                            'id' => '?',
+                            'context_id' => '?',
+                            'title' => '?',
+                            'color' => '?',
+                            'external_url' => '?',
+                            'default_calendar' => '?'
+                        )
+                    )
+                    ->setParameter(0, '')
+                    ->setParameter(1, $room['item_id'])
+                    ->setParameter(2, $name)
+                    ->setParameter(3, $color)
+                    ->setParameter(4, '')
+                    ->setParameter(5, '0')
+                    ->execute();
+                ;
+            }
+        }
     }
 }
