30a05bf2406d0be4bc641cb4402c0af619cf54f1
Johannes Schultze <schultze@effective-webwork.de>
2017-07-06 19:07:28 +0200

    Delete old dates from items table when updating external calendar.

--- src/CommsyBundle/Command/ExternalCalendarsCommand.php
+++ src/CommsyBundle/Command/ExternalCalendarsCommand.php
@@ -2,6 +2,9 @@
 
 namespace CommsyBundle\Command;
 
+use Doctrine\Common\Collections\Expr\Expression;
+use Doctrine\ORM\Query\Expr\Join;
+use Doctrine\ORM\Query\ResultSetMapping;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -51,12 +54,29 @@ class ExternalCalendarsCommand extends ContainerAwareCommand
                 $output->writeln('<info>... ' . $calendar->getTitle() . '</info>');
 
                 // delete old entries from database
-                $entityManager = $container->get('doctrine.orm.entity_manager');
-                $repository = $entityManager->getRepository('CommsyBundle:Dates');
-                $repository->createQueryBuilder('dates')
-                    ->delete()
+                $entityManagerDates = $container->get('doctrine.orm.entity_manager');
+                $repositoryDates = $entityManagerDates->getRepository('CommsyBundle:Dates');
+                $oldDateItems = $repositoryDates->createQueryBuilder('dates')
+                    ->select()
                     ->where('dates.calendarId = :calendarId')
                     ->setParameter('calendarId', $calendar->getId())
+                    ->getQuery()
+                    ->getResult();
+
+                $removeIds = array();
+                foreach ($oldDateItems as $oldDateItem) {
+                    $removeIds[] = $oldDateItem->getItemId();
+                    $entityManagerDates->remove($oldDateItem);
+                }
+                $entityManagerDates->flush();
+
+
+                $entityManagerItems = $container->get('doctrine.orm.entity_manager');
+                $repositoryItems = $entityManagerItems->getRepository('CommsyBundle:Items');
+                $repositoryItems->createQueryBuilder('items')
+                    ->delete()
+                    ->where("items.itemId IN(:removeIds)")
+                    ->setParameter('removeIds', $removeIds)
                     ->getQuery()
                     ->getResult();
 
