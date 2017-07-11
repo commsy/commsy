b79cce33062fce6250f454414ac799528796118b
Johannes Schultze <schultze@effective-webwork.de>
2017-07-11 21:30:31 +0200

    Small changes.

--- src/CommsyBundle/Controller/CalDAVController.php
+++ src/CommsyBundle/Controller/CalDAVController.php
@@ -22,10 +22,23 @@ class CalDAVController extends Controller
      * @Template()
      */
     public function caldavAction($portalId, Request $request) {
+        $this->caldavServer($portalId)->exec();
+    }
 
-        //$pdo = new \PDO('sqlite:data/db.sqlite');
-        //$pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+    /**
+     * @Route("/{portalId}/caldav/{userId}/")
+     * @Template()
+     */
+    public function caldavPrincipalAction($portalId, $userId, Request $request) {
+        $this->caldavServer($portalId)->exec();
+    }
 
+    //Mapping PHP errors to exceptions
+    function exception_error_handler($errno, $errstr, $errfile, $errline ) {
+        throw new \ErrorException($errstr, 0, $errno, $errfile, $errline);
+    }
+
+    private function caldavServer ($portalId) {
         $commsyPdo = new \PDO('mysql:dbname=commsy;host=commsy_db', 'commsy', 'commsy');
         $pdo = new \PDO('mysql:dbname=caldav;host=commsy_db', 'commsy', 'commsy');
 
@@ -65,82 +78,33 @@ class CalDAVController extends Controller
         $server->addPlugin($caldavPlugin);
 
         // CardDAV plugin
-        $carddavPlugin = new CardDAV\Plugin();
-        $server->addPlugin($carddavPlugin);
+        #$carddavPlugin = new CardDAV\Plugin();
+        #$server->addPlugin($carddavPlugin);
 
         // ACL plugin
-        $aclPlugin = new DAVACL\Plugin();
-        $server->addPlugin($aclPlugin);
+        #$aclPlugin = new DAVACL\Plugin();
+        #$server->addPlugin($aclPlugin);
 
         // Support for html frontend
         $browser = new DAV\Browser\Plugin();
         $server->addPlugin($browser);
 
         // And off we go!
-        $server->exec();
+        //$server->exec();
+
+        return $server;
     }
+
+
+
+    // --- Methods used for Browser access --- //
 
     /**
      * @Route("/{portalId}/caldav/calendars/")
      * @Template()
      */
     public function caldavCalendarsAction($portalId, Request $request) {
-
-        //$pdo = new \PDO('sqlite:data/db.sqlite');
-        //$pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-
-        $commsyPdo = new \PDO('mysql:dbname=commsy;host=commsy_db', 'commsy', 'commsy');
-        $pdo = new \PDO('mysql:dbname=caldav;host=commsy_db', 'commsy', 'commsy');
-
-        set_error_handler(array($this, "exception_error_handler"));
-
-        // Files we need
-        //require_once '../../../vendor/autoload.php';
-
-        // Backends
-        $authBackend = new PDO($commsyPdo, $this->container, $portalId);
-        $authBackend->setRealm('CommSy');
-
-        $principalBackend = new DAVACL\PrincipalBackend\PDO($pdo);
-        $calendarBackend = new CalDAV\Backend\PDO($pdo);
-
-        // Directory tree
-        $tree = array(
-            new DAVACL\PrincipalCollection($principalBackend),
-            new CalDAV\CalendarRoot($principalBackend, $calendarBackend)
-        );
-
-
-        // The object tree needs in turn to be passed to the server class
-        $server = new DAV\Server($tree);
-
-        // You are highly encouraged to set your WebDAV server base url. Without it,
-        // SabreDAV will guess, but the guess is not always correct. Putting the
-        // server on the root of the domain will improve compatibility.
-        $server->setBaseUri('/app_dev.php/'.$portalId.'/caldav/');
-
-        // Authentication plugin
-        $authPlugin = new DAV\Auth\Plugin($authBackend,'SabreDAV');
-        $server->addPlugin($authPlugin);
-
-        // CalDAV plugin
-        $caldavPlugin = new CalDAV\Plugin();
-        $server->addPlugin($caldavPlugin);
-
-        // CardDAV plugin
-        $carddavPlugin = new CardDAV\Plugin();
-        $server->addPlugin($carddavPlugin);
-
-        // ACL plugin
-        $aclPlugin = new DAVACL\Plugin();
-        $server->addPlugin($aclPlugin);
-
-        // Support for html frontend
-        $browser = new DAV\Browser\Plugin();
-        $server->addPlugin($browser);
-
-        // And off we go!
-        $server->exec();
+        $this->caldavServer($portalId)->exec();
     }
 
     /**
@@ -148,66 +112,6 @@ class CalDAVController extends Controller
      * @Template()
      */
     public function caldavPrincipalsAction($portalId, Request $request) {
-
-        //$pdo = new \PDO('sqlite:data/db.sqlite');
-        //$pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-
-        $commsyPdo = new \PDO('mysql:dbname=commsy;host=commsy_db', 'commsy', 'commsy');
-        $pdo = new \PDO('mysql:dbname=caldav;host=commsy_db', 'commsy', 'commsy');
-
-        set_error_handler(array($this, "exception_error_handler"));
-
-        // Files we need
-        //require_once '../../../vendor/autoload.php';
-
-        // Backends
-        $authBackend = new PDO($commsyPdo, $this->container, $portalId);
-        $authBackend->setRealm('CommSy');
-
-        $principalBackend = new DAVACL\PrincipalBackend\PDO($pdo);
-        $calendarBackend = new CalDAV\Backend\PDO($pdo);
-
-        // Directory tree
-        $tree = array(
-            new DAVACL\PrincipalCollection($principalBackend),
-            new CalDAV\CalendarRoot($principalBackend, $calendarBackend)
-        );
-
-
-        // The object tree needs in turn to be passed to the server class
-        $server = new DAV\Server($tree);
-
-        // You are highly encouraged to set your WebDAV server base url. Without it,
-        // SabreDAV will guess, but the guess is not always correct. Putting the
-        // server on the root of the domain will improve compatibility.
-        $server->setBaseUri('/app_dev.php/'.$portalId.'/caldav/');
-
-        // Authentication plugin
-        $authPlugin = new DAV\Auth\Plugin($authBackend,'SabreDAV');
-        $server->addPlugin($authPlugin);
-
-        // CalDAV plugin
-        $caldavPlugin = new CalDAV\Plugin();
-        $server->addPlugin($caldavPlugin);
-
-        // CardDAV plugin
-        $carddavPlugin = new CardDAV\Plugin();
-        $server->addPlugin($carddavPlugin);
-
-        // ACL plugin
-        $aclPlugin = new DAVACL\Plugin();
-        $server->addPlugin($aclPlugin);
-
-        // Support for html frontend
-        $browser = new DAV\Browser\Plugin();
-        $server->addPlugin($browser);
-
-        // And off we go!
-        $server->exec();
-    }
-
-    //Mapping PHP errors to exceptions
-    function exception_error_handler($errno, $errstr, $errfile, $errline ) {
-        throw new \ErrorException($errstr, 0, $errno, $errfile, $errline);
+        $this->caldavServer($portalId)->exec();
     }
 }
