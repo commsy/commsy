8ad6572f6a430dc8ada1bf2951ffad19fe3307e4
Christoph Schönfeld <schoenfeld@effective-webwork.de>
2017-07-12 23:39:38 +0200

    fix

--- src/CommsyBundle/Twig/Extension/MarkupExtension.php
+++ src/CommsyBundle/Twig/Extension/MarkupExtension.php
@@ -70,6 +70,11 @@ class MarkupExtension extends \Twig_Extension
         $RFC1738_CHARS = "A-Za-z0-9\?:@&=/;_\.\+!\*'(,%\$~#-";
         $RFC2822_CHARS = "A-Za-z0-9!#\$%&'\*\+/=\?\^_`{\|}~-";
 
+        preg_match('~<!-- KFC TEXT [a-z0-9]* -->~u', $text, $values);
+        foreach ($values as $key => $value) {
+            $text = str_replace($value,'COMMSY_FCKEDITOR'.$key.' ',$text);
+        }
+        $text = ' ' . $text;
 
         $url_string = '^(?<=([\s|\n|>|\(]{1}))((http://|https://|ftp://|www\.)'; //everything starting with http, https or ftp followed by "://" or www. is a url and will be avtivated
         $url_string .= "([" . $RFC1738_CHARS . "]+?))"; //All characters allowed for FTP an HTTP URL's by RFC 1738 (non-greedy because of potential trailing punctuation marks)
@@ -85,6 +90,10 @@ class MarkupExtension extends \Twig_Extension
         // mailto. A space or a linebreak has to be in front of everymail link. No links in bigger words (especially in urls) will be activated
         $text = preg_replace('^( |\^|>|\n)(mailto:)?(([' . $RFC2822_CHARS . ']+(\.[' . $RFC2822_CHARS . ']+)*)@([' . $RFC2822_CHARS . ']+(\.[' . $RFC2822_CHARS . ']+)*\.([A-z]{2,})))^u', '$1<a href="mailto:$3">$3</a>', $text);
         $text = substr($text, 1, strlen($text));
+
+        foreach ($values as $key => $value) {
+            $text = str_replace('COMMSY_FCKEDITOR'.$key.' ',$value,$text);
+        }
 
         return $text;
     }
