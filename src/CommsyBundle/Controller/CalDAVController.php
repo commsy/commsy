dce1b956277aba14cff39be0ceacacc51550dfbe
Johannes Schultze <schultze@effective-webwork.de>
2017-07-25 20:38:07 +0200

    Enabled CalDAV server for use with or without app_dev.php.

--- src/CommsyBundle/Controller/CalDAVController.php
+++ src/CommsyBundle/Controller/CalDAVController.php
@@ -151,7 +151,12 @@ class CalDAVController extends Controller
         // You are highly encouraged to set your WebDAV server base url. Without it,
         // SabreDAV will guess, but the guess is not always correct. Putting the
         // server on the root of the domain will improve compatibility.
-        $server->setBaseUri('/'.$portalId.'/');
+        $prefix = '';
+        if ($_SERVER['SCRIPT_NAME'] == '/app_dev.php') {
+            $prefix = '/app_dev.php';
+        }
+
+        $server->setBaseUri($prefix.'/'.$portalId.'/');
 
         // Authentication plugin
         $authPlugin = new DAV\Auth\Plugin($authBackend,'SabreDAV');
