9308082de03d1901ab06ae60990e5a08eb7bee91
Christoph Schönfeld <schoenfeld@effective-webwork.de>
2017-08-15 22:11:35 +0200

    elastic mapping and custom property listener for populating

--- src/CommsyBundle/EventListener/ElasticCustomPropertyListener.php
+++ src/CommsyBundle/EventListener/ElasticCustomPropertyListener.php
@@ -38,6 +38,22 @@ class ElasticCustomPropertyListener implements EventSubscriberInterface
         if (isset($fields['annotations'])) {
             $this->addAnnotations($event);
         }
+
+        if (isset($fields['files'])) {
+            $this->addFilesContent($event);
+        }
+
+        if (isset($fields['discussionarticles'])) {
+            $this->addDiscussionArticles($event);
+        }
+
+        if (isset($fields['steps'])) {
+            $this->addSteps($event);
+        }
+
+        if (isset($fields['sections'])) {
+            $this->addSections($event);
+        }
     }
 
     private function addHashtags(TransformEvent $event)
@@ -51,7 +67,9 @@ class ElasticCustomPropertyListener implements EventSubscriberInterface
 
             $hashtag = $hashtags->getFirst();
             while ($hashtag) {
-                $objectHashtags[] = $hashtag->getName();
+                if (!$hashtag->isDeleted()) {
+                    $objectHashtags[] = $hashtag->getName();
+                }
 
                 $hashtag = $hashtags->getNext();
             }
@@ -73,7 +91,9 @@ class ElasticCustomPropertyListener implements EventSubscriberInterface
 
             $tag = $tags->getFirst();
             while ($tag) {
-                $objectTags[] = $tag->getTitle();
+                if (!$tag->isDeleted()) {
+                    $objectTags[] = $tag->getTitle();
+                }
 
                 $tag = $tags->getNext();
             }
@@ -95,13 +115,123 @@ class ElasticCustomPropertyListener implements EventSubscriberInterface
 
             $annotation = $annotations->getFirst();
             while ($annotation) {
-                $objectTags[] = $annotation->getDescription();
+                if (!$annotation->isDeleted()) {
+                    $objectTags[] = $annotation->getDescription();
+                }
 
                 $annotation = $annotations->getNext();
             }
 
             if (!empty($objectTags)) {
                 $event->getDocument()->set('annotations', $objectTags);
+            }
+        }
+    }
+
+    private function addFilesContent(TransformEvent $event)
+    {
+        $itemManager = $this->legacyEnvironment->getItemManager();
+        $item = $itemManager->getItem($event->getObject()->getItemId());
+
+        $files = $item->getFileList();
+        if ($files->isNotEmpty()) {
+            $fileContents = [];
+
+            $file = $files->getFirst();
+            while ($file) {
+                if (!$file->isDeleted()) {
+                    $content = $file->getContentBase64();
+                    if (!empty($content)) {
+                        $fileContents[] = $content;
+                    }
+                }
+
+                $file = $files->getNext();
+            }
+
+            if (!empty($fileContents)) {
+                $event->getDocument()->set('files', $fileContents);
+            }
+        }
+    }
+
+    public function addDiscussionArticles($event)
+    {
+        $discussionManager = $this->legacyEnvironment->getDiscussionManager();
+        $discussion = $discussionManager->getItem($event->getObject()->getItemId());
+
+        $articles = $discussion->getAllArticles();
+        if ($articles->isNotEmpty()) {
+            $articleContents = [];
+
+            $article = $articles->getFirst();
+            while ($article) {
+                if (!$article->isDeleted() && !$article->isDraft()) {
+                    $articleContents[] = [
+                        'subject' => $article->getSubject(),
+                        'description' => $article->getDescription(),
+                    ];
+                }
+
+                $article = $articles->getNext();
+            }
+
+            if (!empty($articleContents)) {
+                $event->getDocument()->set('discussionarticles', $articleContents);
+            }
+        }
+    }
+
+    public function addSteps($event)
+    {
+        $todoManager = $this->legacyEnvironment->getTodoManager();
+        $todo = $todoManager->getItem($event->getObject()->getItemId());
+
+        $steps = $todo->getStepItemList();
+        if ($steps->isNotEmpty()) {
+            $stepContents = [];
+
+            $step = $steps->getFirst();
+            while ($step) {
+                if (!$step->isDeleted() && !$step->isDraft()) {
+                    $stepContents[] = [
+                        'title' => $step->getTitle(),
+                        'description' => $step->getDescription(),
+                    ];
+                }
+
+                $step = $steps->getNext();
+            }
+
+            if (!empty($stepContents)) {
+                $event->getDocument()->set('steps', $stepContents);
+            }
+        }
+    }
+
+    public function addSections($event)
+    {
+        $materialManager = $this->legacyEnvironment->getMaterialManager();
+        $material = $materialManager->getItem($event->getObject()->getItemId());
+
+        $sections = $material->getSectionList();
+        if ($sections->isNotEmpty()) {
+            $sectionContents = [];
+
+            $section = $sections->getFirst();
+            while ($section) {
+                if (!$section->isDeleted() && !$section->isDraft()) {
+                    $sectionContents[] = [
+                        'title' => $section->getTitle(),
+                        'description' => $section->getDescription(),
+                    ];
+                }
+
+                $section = $sections->getNext();
+            }
+
+            if (!empty($sectionContents)) {
+                $event->getDocument()->set('steps', $sectionContents);
             }
         }
     }
