53a2d33f6205ae051f6c7eda6a86da791bb82ac6
Johannes Schultze <schultze@effective-webwork.de>
2017-09-08 14:48:43 +0200

    Added service for room categories.

---
+++ src/CommsyBundle/Services/RoomCategoriesService.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace CommsyBundle\Services;
+
+use CommsyBundle\Entity\Calendars;
+use Doctrine\ORM\EntityManager;
+use Symfony\Component\DependencyInjection\ContainerInterface as Container;
+use Sabre\VObject;
+
+class RoomCategoriesService
+{
+
+    /**
+     * @var EntityManager $em
+     */
+    private $em;
+
+    private $serviceContainer;
+
+    public function __construct(EntityManager $entityManager, Container $container)
+    {
+        $this->em = $entityManager;
+        $this->serviceContainer = $container;
+    }
+
+    public function getListRoomCategories ($contextId) {
+        $result = array();
+
+        $repository = $this->em->getRepository('CommsyBundle:RoomCategories');
+        $query = $repository->createQueryBuilder('room_categories')
+            ->select()
+            ->where('room_categories.context_id = :context_id')
+            ->setParameter('context_id', $contextId)
+            ->getQuery();
+        $roomCategories = $query->getResult();
+
+        foreach ($roomCategories as $roomCategory) {
+            $result[] = $roomCategory;
+        }
+
+        return $result;
+    }
+
+    public function getRoomCategory ($roomCategoryId) {
+        $repository = $this->em->getRepository('CommsyBundle:RoomCategories');
+        $query = $repository->createQueryBuilder('room_categories')
+            ->select()
+            ->where('room_categories.id = :roomCategoryId')
+            ->setParameter('roomCategoryId', $roomCategoryId)
+            ->getQuery();
+
+        return $calendars = $query->getResult();
+    }
+}
