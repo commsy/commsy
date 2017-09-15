06ed8a8dd666e1cc4357cd0a9d021bced889ab92
Christopher Timm <timm@effective-webwork.de>
2017-09-15 14:31:48 +0200

    fix missing curly brace

--- src/CommsyBundle/EventListener/CommsyActivityListener.php
+++ src/CommsyBundle/EventListener/CommsyActivityListener.php
@@ -37,26 +37,27 @@ class CommsyActivityListener
                     $countRequest = false;
                 }
 
-                if ($countRequest)
+                if ($countRequest) {
                     $environment = $this->legacyEnvironment->getEnvironment();
 
-                $activity_points = 1;
-                $context_item_current = $environment->getCurrentContextItem();
-                if (isset($context_item_current)) {
-                    $context_item_current->saveActivityPoints($activity_points);
-                    if ($context_item_current->isProjectRoom()
-                        or $context_item_current->isCommunityRoom()
-                        or $environment->inPrivateRoom()
-                        or $environment->inGroupRoom()
-                    ) {
+                    $activity_points = 1;
+                    $context_item_current = $environment->getCurrentContextItem();
+                    if (isset($context_item_current)) {
+                        $context_item_current->saveActivityPoints($activity_points);
+                        if ($context_item_current->isProjectRoom()
+                            or $context_item_current->isCommunityRoom()
+                            or $environment->inPrivateRoom()
+                            or $environment->inGroupRoom()
+                        ) {
 
-                        // archiving
-                        $context_item_current->saveLastLogin();
+                            // archiving
+                            $context_item_current->saveLastLogin();
 
-                        $current_portal_item = $environment->getCurrentPortalItem();
-                        if (isset($current_portal_item)) {
-                            $current_portal_item->saveActivityPoints($activity_points);
-                            unset($current_portal_item);
+                            $current_portal_item = $environment->getCurrentPortalItem();
+                            if (isset($current_portal_item)) {
+                                $current_portal_item->saveActivityPoints($activity_points);
+                                unset($current_portal_item);
+                            }
                         }
                     }
                 }
