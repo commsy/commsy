15737d17acf1f198ece04d9d13bfe317fd1acaeb
Christopher Timm <timm@effective-webwork.de>
2017-09-15 14:26:28 +0200

    filter image for activity count

--- src/CommsyBundle/EventListener/CommsyActivityListener.php
+++ src/CommsyBundle/EventListener/CommsyActivityListener.php
@@ -28,13 +28,23 @@ class CommsyActivityListener
             $request = $event->getRequest();
             if (!$request->isXmlHttpRequest()) {
 
-                $environment = $this->legacyEnvironment->getEnvironment();
+                $countRequest = true;
+                if (preg_match('~\/room\/(\d)+\/user\/(\d)+\/image~', $request->getUri())) {
+                    $countRequest = false;
+                } else if (preg_match('~\/room\/(\d)+\/theme\/background~', $request->getUri())) {
+                    $countRequest = false;
+                } else if (preg_match('~\/room\/(\d)+\/logo~', $request->getUri())) {
+                    $countRequest = false;
+                }
+
+                if ($countRequest)
+                    $environment = $this->legacyEnvironment->getEnvironment();
 
                 $activity_points = 1;
                 $context_item_current = $environment->getCurrentContextItem();
-                if ( isset($context_item_current) ) {
+                if (isset($context_item_current)) {
                     $context_item_current->saveActivityPoints($activity_points);
-                    if ( $context_item_current->isProjectRoom()
+                    if ($context_item_current->isProjectRoom()
                         or $context_item_current->isCommunityRoom()
                         or $environment->inPrivateRoom()
                         or $environment->inGroupRoom()
@@ -44,7 +54,7 @@ class CommsyActivityListener
                         $context_item_current->saveLastLogin();
 
                         $current_portal_item = $environment->getCurrentPortalItem();
-                        if ( isset($current_portal_item) ) {
+                        if (isset($current_portal_item)) {
                             $current_portal_item->saveActivityPoints($activity_points);
                             unset($current_portal_item);
                         }
