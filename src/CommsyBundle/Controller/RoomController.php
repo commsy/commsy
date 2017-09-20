95644d3c81a1843a2627083307d944a2d6a112d6
Arved Solth <solth@effective-webwork.de>
2017-09-20 22:16:09 +0200

    Enter archived rooms from all rooms list

--- src/CommsyBundle/Controller/RoomController.php
+++ src/CommsyBundle/Controller/RoomController.php
@@ -464,7 +464,6 @@ class RoomController extends Controller
         foreach ($rooms as $room) {
             $projectsMemberStatus[$room->getItemId()] = $this->memberStatus($room);
         }
-
         return [
             'roomId' => $roomId,
             'portal' => $portalItem,
@@ -1184,13 +1183,13 @@ class RoomController extends Controller
         return $templates;
     }
 
-    private function memberStatus($item)
+    private function memberStatus($roomItem)
     {
         $status = 'closed';
         $legacyEnvironment = $this->get('commsy_legacy.environment')->getEnvironment();
         $currentUser = $legacyEnvironment->getCurrentUserItem();
         $roomService = $this->get('commsy_legacy.room_service');
-        $item = $roomService->getRoomItem($item->getItemId());
+        $item = $roomService->getRoomItem($roomItem->getItemId());
 
         if ($item) {
             $relatedUserArray = $currentUser->getRelatedUserList()->to_array();
@@ -1222,7 +1221,25 @@ class RoomController extends Controller
                 $status = 'rejected';
             }
         } else {
+
+            $legacyEnvironment->activateArchiveMode();
+
+            $item = $roomService->getRoomItem($roomItem->getItemId());
             $status = 'archived';
+
+            $currentUser = $legacyEnvironment->getCurrentUserItem();
+            $relatedUserArray = $currentUser->getRelatedUserList()->to_array();
+
+            foreach ($relatedUserArray as $relatedUser) {
+                if ($relatedUser->getContextId() == $item->getItemId()) {
+                    $roomUser = $relatedUser;
+                }
+            }
+            if ($currentUser->isRoot() || (!empty($roomUser) && $item->mayEnter($roomUser))) {
+                $status = 'enter_archived';
+            }
+
+            $legacyEnvironment->deactivateArchiveMode();
         }
         return $status;
     }
