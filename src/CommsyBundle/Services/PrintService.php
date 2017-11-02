7c63750c09a15c345c2cdfd86d5b367f52cbb1a0
Christoph Schönfeld <schoenfeld@effective-webwork.de>
2017-11-02 17:12:29 +0100

    print proxy support

--- src/CommsyBundle/Services/PrintService.php
+++ src/CommsyBundle/Services/PrintService.php
@@ -79,6 +79,16 @@ class PrintService
         $this->serviceContainer->get('knp_snappy.pdf')->setOption('images',true);
         $this->serviceContainer->get('knp_snappy.pdf')->setOption('load-media-error-handling','ignore');
         $this->serviceContainer->get('knp_snappy.pdf')->setOption('load-error-handling','ignore');
+
+        if ($this->serviceContainer->hasParameter('proxy_ip')) {
+            if ($this->serviceContainer->hasParameter('proxy_port')) {
+                $proxyIp = $this->serviceContainer->getParameter('proxy_ip');
+                $proxyPort = $this->serviceContainer->getParameter('proxy_port');
+                $proxy = 'http://' . $proxyIp . ':' . $proxyPort;
+
+                $this->serviceContainer->get('knp_snappy.pdf')->setOption('proxy', $proxy);
+            }
+        }
         
         // set cookie for authentication - needed to request images
         $this->serviceContainer->get('knp_snappy.pdf')->setOption('cookie', [
