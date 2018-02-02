7bdd0c2754aaf2edeec929359b6d9ef1b30c11ae
Christoph Schönfeld <schoenfeld@effective-webwork.de>
2018-02-02 11:39:02 +0100

    missing import fixed

--- src/CommsyBundle/Controller/ContextController.php
+++ src/CommsyBundle/Controller/ContextController.php
@@ -5,6 +5,7 @@ namespace CommsyBundle\Controller;
 use CommsyBundle\Filter\ProjectFilterType;
 use CommsyBundle\Form\Type\ContextRequestType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
