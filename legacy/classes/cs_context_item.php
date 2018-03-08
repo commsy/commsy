edef7b9df014b80d7ab82885fccc8ef9a522d534
Matthias Steffens <mat@extracts.de>
2018-03-08 16:30:37 +0100

    when removing unallowed rubrics, also ensure that we're dealing with a valid rubric configuration which has non-empty values on both sides of the underscore (like "date_short" or "date_show")

--- legacy/classes/cs_context_item.php
+++ legacy/classes/cs_context_item.php
@@ -1862,15 +1862,18 @@ class cs_context_item extends cs_item {
       }
     }
 
-    // if a plugin is deleted, remove it from HomeConf
+    // if a plugin is deleted, or a rubric configuration is faulty, remove it from HomeConf
     $retour = array();
     $rubric_array = explode(',',$rubricsString);
     foreach ( $rubric_array as $rubric ) {
-      $rubric2_array = explode('_',$rubric);
-      if ( in_array($rubric2_array[0],$this->_default_rubrics_array) ) {
-        $retour[] = implode('_',$rubric2_array);
+      list($rubricType, $rubricConf) = explode('_',$rubric);
+      if ( !empty($rubricType) && !empty($rubricConf) &&
+          in_array($rubricType,$this->_default_rubrics_array) ) {
+        $retour[] = $rubricType.'_'.$rubricConf;
       }
     }
+
+    $rubricsString = "";
     if ( !empty($retour) ) {
       $rubricsString = implode(',',$retour);
     }
