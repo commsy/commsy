6825664859b4ac003f9cab449185cd186bd62850
Christoph Schönfeld <schoenfeld@effective-webwork.de>
2018-03-14 01:28:28 +0100

    dummy tests

---
+++ tests/api/CreateUserCest.php
@@ -0,0 +1,18 @@
+<?php
+
+
+class CreateUserCest
+{
+    public function _before(ApiTester $I)
+    {
+    }
+
+    public function _after(ApiTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(ApiTester $I)
+    {
+    }
+}
