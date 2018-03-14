52dce3a88e8813512ba03cd53f23d3f8b294c9d7
Christoph Schönfeld <schoenfeld@effective-webwork.de>
2018-03-15 00:39:00 +0100

    testsuite environment

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -37,7 +37,7 @@ class AppKernel extends Kernel
             new CommsyMediawikiBundle\CommsyMediawikiBundle(),
         ];
 
-        if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
+        if (in_array($this->getEnvironment(), array('dev', 'test', 'testsuite'), true)) {
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
