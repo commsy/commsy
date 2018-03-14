0fe7c8499ccc847f6861e37bd1b5a00ff8830d58
Christoph Schönfeld <schoenfeld@effective-webwork.de>
2018-03-14 01:17:40 +0100

    codeception test

---
+++ tests/acceptance/SigninCest.php
@@ -0,0 +1,27 @@
+<?php
+
+
+class SigninCest
+{
+    public function _before(AcceptanceTester $I)
+    {
+    }
+
+    public function _after(AcceptanceTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(AcceptanceTester $I)
+    {
+//        $I->am('user');
+//        $I->wantTo('login to website');
+//        $I->lookForwardTo('access website features for logged-in users');
+//
+//        $I->amOnPage('/');
+//        $I->fillField('user_id', 'test');
+//        $I->fillField('password', 'test');
+//        $I->click('option');
+//        $I->see('test');
+    }
+}
