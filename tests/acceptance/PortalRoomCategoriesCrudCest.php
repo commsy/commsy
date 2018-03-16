ebff4ad700c81cfd9b69085240cdc446e4e6588c
Christoph Schönfeld <schoenfeld@effective-webwork.de>
2018-03-16 01:18:32 +0100

    language independent login click

--- tests/acceptance/PortalRoomCategoriesCrudCest.php
+++ tests/acceptance/PortalRoomCategoriesCrudCest.php
@@ -15,11 +15,10 @@ class PortalRoomCategoriesCrudCest
     public function viewPage(AcceptanceTester $I)
     {
         $I->amOnPage('/');
-        $I->click('a[title="Sprache auf Deutsch umstellen"]');
 
         $I->fillField('input[name="user_id"]', 'root');
         $I->fillField('input[name="password"]', 'root');
-        $I->click('input[value="Anmelden"]');
+        $I->click('form[name="login"] input[type="submit"]');
         $I->see('Portal initialisieren');
 
         $I->click('Portal initialisieren');
