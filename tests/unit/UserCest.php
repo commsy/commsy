f441d451f3c38c55db26c8a1d5a11dd97d2483f0
Christoph Schönfeld <schoenfeld@effective-webwork.de>
2018-04-11 15:57:14 +0200

    test description added

--- tests/unit/UserCest.php
+++ tests/unit/UserCest.php
@@ -11,7 +11,16 @@ class UserCest
     {
     }
 
-    // tests
+    /**
+     * This test verifies that the getEmail() method works on guest user items. Due to the fact that the behaviour
+     * of such items differs from a normal user item, special handling is required. For example, a guest user does not
+     * have a related portal user item.
+     *
+     * This test was introduced to fix a regression that throws a fatal error when a guest user was trying to enter
+     * a community room on a very specific portal configuration but is expected to be a general problem.
+     *
+     * @param UnitTester $I
+     */
     public function guestUserEmailTest(UnitTester $I)
     {
         /** @var \cs_environment $legacyEnvironment */
