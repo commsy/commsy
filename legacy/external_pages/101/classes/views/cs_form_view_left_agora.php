dd83432cbdbb01a9cd4b33d402568117658d5f0b
Matthias Steffens <mat@extracts.de>
2018-05-18 16:50:13 +0200

    the custom AGORA form view now inherits from cs_form_view_left instead of cs_view

--- legacy/external_pages/101/classes/views/cs_form_view_left_agora.php
+++ legacy/external_pages/101/classes/views/cs_form_view_left_agora.php
@@ -1,35 +1,17 @@
 <?PHP
-// $Id$
-//
-// Release $Name$
-//
-// Copyright (c)2002-2003 Matthias Finck, Dirk Fust, Oliver Hankel, Iver Jackewitz, Michael Janneck,
-// Martti Jeenicke, Detlev Krause, Irina L. Marinescu, Timo Nolte, Bernd Pape,
-// Edouard Simon, Monique Strauss, José Manuel González Vázquez
-//
-//    This file is part of CommSy.
-//
-//    CommSy is free software; you can redistribute it and/or modify
-//    it under the terms of the GNU General Public License as published by
-//    the Free Software Foundation; either version 2 of the License, or
-//    (at your option) any later version.
-//
-//    CommSy is distributed in the hope that it will be useful,
-//    but WITHOUT ANY WARRANTY; without even the implied warranty of
-//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//    GNU General Public License for more details.
-//
-//    You have received a copy of the GNU General Public License
-//    along with CommSy.
-
-/** upper class of the form view
+/** include upper class of the form view
  */
-// $this->includeClass(VIEW);
+global $symfonyContainer;
+$environment = $symfonyContainer->get('commsy_legacy.environment')->getEnvironment();
+$classFactory = $environment->getClassFactory();
+$classFactory->includeClass(FORM_LEFT_VIEW);
 
-/** class for a form view in commsy-style
- * this class implements a form view
+// TODO: remove all code from `cs_form_view_left` that hasn't been overridden
+
+/** Overridden to implement a form view for the custom AGORA portal theme.
+ * @author CommSy Development Group
  */
-class cs_form_view_left_agora extends cs_view {
+class cs_form_view_left_agora extends cs_form_view_left {
 
    /**
     * string - containing the URL where data will post to
@@ -78,8 +60,8 @@ class cs_form_view_left_agora extends cs_view {
     *
     * @param array params parameters in an array of this class
     */
-   function cs_form_view_left_agora ($params) {
-      $this->cs_view($params);
+   function __construct($params) {
+      $this->cs_form_view_left($params);
    }
 
    /** set URL of the form view
