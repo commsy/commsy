f778ac385f43b2c73c2a23c04cc742b26343812f
Christoph Schönfeld <schoenfeld@effective-webwork.de>
2018-07-26 11:41:26 +0200

    test added to check if a section item can be transformed by the material transformer

---
+++ tests/unit/TransformerCest.php
@@ -0,0 +1,28 @@
+<?php
+
+
+class TransformerCest
+{
+    public function _before(UnitTester $I)
+    {
+    }
+
+    public function _after(UnitTester $I)
+    {
+    }
+    
+    public function transformSectionTest(UnitTester $I)
+    {
+        $legacyEnvironment = $I->grabService('commsy_legacy.environment')->getEnvironment();
+
+        $sectionManager = $legacyEnvironment->getSectionManager();
+        $section = $sectionManager->getNewItem();
+
+        $I->assertInstanceOf(\cs_section_item::class, $section);
+
+        $msTransformer = $I->grabService('commsy_legacy.transformer.material');
+        $dataArray = $msTransformer->transform($section);
+
+        $I->assertNotEmpty($dataArray);
+    }
+}
