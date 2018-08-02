1d6707b1bf9bceb1d55ba5033c204da96ebce748
Christoph Schönfeld <schoenfeld@effective-webwork.de>
2018-08-02 15:11:27 +0200

    old templates and javascript files removed

--- web/js/src/commsy/sniff.js
+++
@@ -1,7 +0,0 @@
-define(["dojo/has"], function(has) {
-	
-    // check for windows os
-    has.add("isWindows", function() {
-        return (navigator.appVersion.indexOf("Win")!=-1);
-    });
-});
--- web/js/src/commsy/widgets/MyWidgets/nls/de/MyWidgetsRoomwideSearch.js
+++
@@ -1,6 +0,0 @@
-define({
-	searchExtended:				"Erweitert",
-	searchRooms:				"Suche in Räumen:",
-	searchRubrics:				"Suche in Rubriken:",
-	title:						"Raumübergreifende Suche"
-});
