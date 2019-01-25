98ee023ae56fa636ff1c8c25e7a8aef7e14fbef5
Matthias Steffens <mat@extracts.de>
2019-01-25 19:10:00 +0100

    as part of #2110, added a doctrine migration which adds a "public" column to the "tag" tables

---
+++ app/DoctrineMigrations/Version20190125123633.php
@@ -0,0 +1,50 @@
+<?php declare(strict_types=1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190125123633 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('
+            ALTER TABLE tag
+            ADD public TINYINT NOT NULL DEFAULT 0;
+        ');
+
+        $this->addSql('
+            ALTER TABLE zzz_tag
+            ADD public TINYINT NOT NULL DEFAULT 0;
+        ');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('
+            ALTER TABLE tag
+            DROP public;
+        ');
+
+        $this->addSql('
+            ALTER TABLE zzz_tag
+            DROP public;
+        ');
+    }
+}
