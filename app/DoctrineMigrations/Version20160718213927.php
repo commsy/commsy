602b084af004a71274ccdb70fdb3af667af390bc
Matthias Steffens <mat@extracts.de>
2019-01-25 19:47:53 +0100

    existing migration files are now based on "Doctrine\Migrations\AbstractMigration" instead of the deprecated "Doctrine\DBAL\Migrations\AbstractMigration", and their syntax mirrors again the current migration template as generated by "doctrine:migrations:generate"

--- app/DoctrineMigrations/Version20160718213927.php
+++ app/DoctrineMigrations/Version20160718213927.php
@@ -1,8 +1,8 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace Application\Migrations;
 
-use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
 
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
@@ -11,7 +11,7 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20160718213927 extends AbstractMigration implements ContainerAwareInterface
+final class Version20160718213927 extends AbstractMigration implements ContainerAwareInterface
 {
     private $container;
 
@@ -23,7 +23,7 @@ class Version20160718213927 extends AbstractMigration implements ContainerAwareI
     /**
      * @param Schema $schema
      */
-    public function up(Schema $schema)
+    public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
@@ -44,7 +44,7 @@ class Version20160718213927 extends AbstractMigration implements ContainerAwareI
     /**
      * @param Schema $schema
      */
-    public function down(Schema $schema)
+    public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
--- app/DoctrineMigrations/Version20170225121940.php
+++ app/DoctrineMigrations/Version20170225121940.php
@@ -1,19 +1,19 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace Application\Migrations;
 
-use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
 
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20170225121940 extends AbstractMigration
+final class Version20170225121940 extends AbstractMigration
 {
     /**
      * @param Schema $schema
      */
-    public function up(Schema $schema)
+    public function up(Schema $schema) : void
     {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
@@ -30,7 +30,7 @@ class Version20170225121940 extends AbstractMigration
     /**
      * @param Schema $schema
      */
-    public function down(Schema $schema)
+    public function down(Schema $schema) : void
     {
         $this->throwIrreversibleMigrationException();
     }
