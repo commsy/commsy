d19795ff9a3eaef5dde26400a0fbadcc73ecd06d
Christoph Schönfeld <schoenfeld@effective-webwork.de>
2019-02-21 00:17:07 +0100

    select2 dependency added

--- assets/js/common_css.js
+++ assets/js/common_css.js
@@ -4,3 +4,4 @@ require('nprogress/nprogress.css');
 require('fullcalendar/dist/fullcalendar.css');
 require('tooltipster/dist/css/tooltipster.bundle.css');
 require('video.js/dist/video-js.css');
+require('select2/dist/css/select2.css');
