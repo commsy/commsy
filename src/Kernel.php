92e6e04803d42c7953dfdf9672576de140df21d1
Christoph Schönfeld <schoenfeld@effective-webwork.de>
2019-04-01 19:24:28 +0200

    Old Bundle classes migrated

--- src/Kernel.php
+++ src/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use App\DependencyInjection\Compiler\DatabaseChecksPass;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\Config\Resource\FileResource;
@@ -44,5 +45,10 @@ class Kernel extends BaseKernel
         $routes->import($confDir.'/{routes}/'.$this->environment.'/**/*'.self::CONFIG_EXTS, '/', 'glob');
         $routes->import($confDir.'/{routes}/*'.self::CONFIG_EXTS, '/', 'glob');
         $routes->import($confDir.'/{routes}'.self::CONFIG_EXTS, '/', 'glob');
+    }
+
+    protected function build(ContainerBuilder $container): void
+    {
+        $container->addCompilerPass(new DatabaseChecksPass());
     }
 }
