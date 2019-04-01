e1e46dd6b848659869707bdde8ab9a2c0fb7b1cf
Christoph Schönfeld <schoenfeld@effective-webwork.de>
2019-04-01 16:17:28 +0200

    SensioGeneratorBundle does not support Symfony 4, introducing MakerBundle as an alternative.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -40,7 +40,7 @@ class AppKernel extends Kernel
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
-            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+            $bundles[] = new Symfony\Bundle\MakerBundle\MakerBundle();
             $bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
         }
 
