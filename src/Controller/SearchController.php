commsy/commsy

commit d4cc1cb5a1b2ccc6cdcc1cf629318f6a3ae2398b

2417: the date range form options of the search filter form are now based on the DateRangeFilterType from the Lexik FormFilterBundle; this groups the created from/until & modified from/until fields for better clarity

src/Controller/SearchController.php
@@ -358,28 +358,33 @@ class SearchController extends BaseController
         // creator parameter
         $searchData->setSelectedCreator($searchParams['selectedCreator'] ?? "all");
 
-        // creation_date_from parameter
-        if (!empty($searchParams['creation_date_from'])) {
-            $date = \DateTime::createFromFormat('Y-m-d', $searchParams['creation_date_from'])->setTime(0, 0, 0);
-            $searchData->setCreationDateFrom($date);
+        // date ranges based on Lexik\Bundle\FormFilterBundle\Filter\Form\Type\DateRangeFilterType
+        // creation_date_range parameter
+        if (!empty($searchParams['creation_date_range'])) {
+            $creationDateRange = [];
+            if (!empty($searchParams['creation_date_range']['left_date'])) {
+                $date = \DateTime::createFromFormat('Y-m-d', $searchParams['creation_date_range']['left_date'])->setTime(0, 0, 0);
+                $creationDateRange[0] = $date;
+            }
+            if (!empty($searchParams['creation_date_range']['right_date'])) {
+                $date = \DateTime::createFromFormat('Y-m-d', $searchParams['creation_date_range']['right_date'])->setTime(0, 0, 0);
+                $creationDateRange[1] = $date;
+            }
+            $searchData->setCreationDateRange($creationDateRange);
         }
 
-        // creation_date_until parameter
-        if (!empty($searchParams['creation_date_until'])) {
-            $date = \DateTime::createFromFormat('Y-m-d', $searchParams['creation_date_until'])->setTime(23, 59, 59);
-            $searchData->setCreationDateUntil($date);
-        }
-
-        // modification_date_from parameter
-        if (!empty($searchParams['modification_date_from'])) {
-            $date = \DateTime::createFromFormat('Y-m-d', $searchParams['modification_date_from'])->setTime(0, 0, 0);
-            $searchData->setModificationDateFrom($date);
-        }
-
-        // modification_date_until parameter
-        if (!empty($searchParams['modification_date_until'])) {
-            $date = \DateTime::createFromFormat('Y-m-d', $searchParams['modification_date_until'])->setTime(23, 59, 59);
-            $searchData->setModificationDateUntil($date);
+        // modification_date_range parameter
+        if (!empty($searchParams['modification_date_range'])) {
+            $modificationDateRange = [];
+            if (!empty($searchParams['modification_date_range']['left_date'])) {
+                $date = \DateTime::createFromFormat('Y-m-d', $searchParams['modification_date_range']['left_date'])->setTime(0, 0, 0);
+                $modificationDateRange[0] = $date;
+            }
+            if (!empty($searchParams['modification_date_range']['right_date'])) {
+                $date = \DateTime::createFromFormat('Y-m-d', $searchParams['modification_date_range']['right_date'])->setTime(0, 0, 0);
+                $modificationDateRange[1] = $date;
+            }
+            $searchData->setModificationDateRange($modificationDateRange);
         }
 
         return $searchData;

src/Filter/SearchFilterType.php
@@ -66,41 +66,39 @@ class SearchFilterType extends AbstractType
                 'label' => 'Creator',
                 'required' => false,
             ])
-            ->add('creation_date_from', Filters\DateFilterType::class, [
+            ->add('creation_date_range', Filters\DateRangeFilterType::class, [
                 'attr' => [
                     'onchange' => 'this.form.submit()',
                 ],
-                'label' => 'Created from',
+                'label' => 'Created from/until',
+                'left_date_options' => [
+                    'label' => 'from',
+                    'input'  => 'datetime',
+                    'widget' => 'single_text',
+                ],
+                'right_date_options' => [
+                    'label' => 'until',
+                    'input'  => 'datetime',
+                    'widget' => 'single_text',
+                ],
                 'required' => false,
-                'input'  => 'datetime',
-                'widget' => 'single_text',
             ])
-            ->add('creation_date_until', Filters\DateFilterType::class, [
+            ->add('modification_date_range', Filters\DateRangeFilterType::class, [
                 'attr' => [
                     'onchange' => 'this.form.submit()',
                 ],
-                'label' => 'Created until',
-                'required' => false,
-                'input'  => 'datetime',
-                'widget' => 'single_text',
-            ])
-            ->add('modification_date_from', Filters\DateFilterType::class, [
-                'attr' => [
-                    'onchange' => 'this.form.submit()',
+                'label' => 'Modified from/until',
+                'left_date_options' => [
+                    'label' => 'from',
+                    'input'  => 'datetime',
+                    'widget' => 'single_text',
                 ],
-                'label' => 'Modified from',
-                'required' => false,
-                'input'  => 'datetime',
-                'widget' => 'single_text',
-            ])
-            ->add('modification_date_until', Filters\DateFilterType::class, [
-                'attr' => [
-                    'onchange' => 'this.form.submit()',
+                'right_date_options' => [
+                    'label' => 'until',
+                    'input'  => 'datetime',
+                    'widget' => 'single_text',
                 ],
-                'label' => 'Modified until',
                 'required' => false,
-                'input'  => 'datetime',
-                'widget' => 'single_text',
             ])
             ->add('selectedRubric', Types\ChoiceType::class, [
                 'attr' => [

src/Model/SearchData.php
@@ -186,6 +186,58 @@ class SearchData
     }
 
     /**
+     * @return array|null an array of two items, start & end date, which may be \DateTime objects or null
+     */
+    public function getCreationDateRange(): ?array
+    {
+        return [
+            $this->getCreationDateFrom(),
+            $this->getCreationDateUntil(),
+            ];
+    }
+
+    /**
+     * @param array|null $creationDateRange an array of two items, start & end date, which may be \DateTime objects or null
+     * @return SearchData
+     */
+    public function setCreationDateRange(?array $creationDateRange): SearchData
+    {
+        if (isset($creationDateRange[0]) && $creationDateRange[0] instanceof \DateTime) {
+            $this->setCreationDateFrom($creationDateRange[0]);
+        }
+        if (isset($creationDateRange[1]) && $creationDateRange[1] instanceof \DateTime) {
+            $this->setCreationDateUntil($creationDateRange[1]);
+        }
+        return $this;
+    }
+
+    /**
+     * @return array|null an array of two items, start & end date, which may be \DateTime objects or null
+     */
+    public function getModificationDateRange(): ?array
+    {
+        return [
+            $this->getModificationDateFrom(),
+            $this->getModificationDateUntil(),
+            ];
+    }
+
+    /**
+     * @param array|null $modificationDateRange an array of two items, start & end date, which may be \DateTime objects or null
+     * @return SearchData
+     */
+    public function setModificationDateRange(?array $modificationDateRange): SearchData
+    {
+        if (isset($modificationDateRange[0]) && $modificationDateRange[0] instanceof \DateTime) {
+            $this->setModificationDateFrom($modificationDateRange[0]);
+        }
+        if (isset($modificationDateRange[1]) && $modificationDateRange[1] instanceof \DateTime) {
+            $this->setModificationDateUntil($modificationDateRange[1]);
+        }
+        return $this;
+    }
+
+    /**
      * @return \DateTime|null
      */
     public function getCreationDateFrom(): ?\DateTime

templates/search/results.html.twig
@@ -129,10 +129,10 @@
                                     "search[phrase]": "{{ searchData.phrase }}",
                                     "search[all_rooms]": "{{ searchData.allRooms ? 1 : 0 }}",
                                     "search[selectedCreator]": "{{ searchData.selectedCreator }}",
-                                    "search[creation_date_from]": "{{ searchData.creationDateFrom ? searchData.creationDateFrom.format('Y-m-d') : '' }}",
-                                    "search[creation_date_until]": "{{ searchData.creationDateUntil ? searchData.creationDateUntil.format('Y-m-d') : '' }}",
-                                    "search[modification_date_from]": "{{ searchData.modificationDateFrom ? searchData.modificationDateFrom.format('Y-m-d') : '' }}",
-                                    "search[modification_date_until]": "{{ searchData.modificationDateUntil ? searchData.modificationDateUntil.format('Y-m-d') : '' }}",
+                                    "search[creation_date_range][left_date]": "{{ searchData.creationDateFrom ? searchData.creationDateFrom.format('Y-m-d') : '' }}",
+                                    "search[creation_date_range][right_date]": "{{ searchData.creationDateUntil ? searchData.creationDateUntil.format('Y-m-d') : '' }}",
+                                    "search[modification_date_range][left_date]": "{{ searchData.modificationDateFrom ? searchData.modificationDateFrom.format('Y-m-d') : '' }}",
+                                    "search[modification_date_range][right_date]": "{{ searchData.modificationDateUntil ? searchData.modificationDateUntil.format('Y-m-d') : '' }}",
                                     "search[selectedRubric]": "{{ searchData.selectedRubric }}"
                                 }
                         }'>

translations/search.de.xlf
@@ -46,21 +46,13 @@
                 <source>Creator</source>
                 <target>ErstellerIn</target>
             </trans-unit>
-            <trans-unit id="search-filter-created-from">
-                <source>Created from</source>
-                <target>Erstellt von</target>
+            <trans-unit id="search-filter-created-range">
+                <source>Created from/until</source>
+                <target>Erstellt von/bis</target>
             </trans-unit>
-            <trans-unit id="search-filter-created-until">
-                <source>Created until</source>
-                <target>Erstellt bis</target>
-            </trans-unit>
-            <trans-unit id="search-filter-modified-from">
-                <source>Modified from</source>
-                <target>Geändert von</target>
-            </trans-unit>
-            <trans-unit id="search-filter-modified-until">
-                <source>Modified until</source>
-                <target>Geändert bis</target>
+            <trans-unit id="search-filter-modified-range">
+                <source>Modified from/until</source>
+                <target>Geändert von/bis</target>
             </trans-unit>
             <trans-unit id="search-filter-rubric">
                 <source>Rubric</source>

translations/search.en.xlf
@@ -46,21 +46,13 @@
                 <source>Creator</source>
                 <target>Creator</target>
             </trans-unit>
-            <trans-unit id="search-filter-created-from">
-                <source>Created from</source>
-                <target>Created from</target>
+            <trans-unit id="search-filter-created-range">
+                <source>Created from/until</source>
+                <target>Created from/until</target>
             </trans-unit>
-            <trans-unit id="search-filter-created-until">
-                <source>Created until</source>
-                <target>Created until</target>
-            </trans-unit>
-            <trans-unit id="search-filter-modified-from">
-                <source>Modified from</source>
-                <target>Modified from</target>
-            </trans-unit>
-            <trans-unit id="search-filter-modified-until">
-                <source>Modified until</source>
-                <target>Modified until</target>
+            <trans-unit id="search-filter-modified-range">
+                <source>Modified from/until</source>
+                <target>Modified from/until</target>
             </trans-unit>
             <trans-unit id="search-filter-rubric">
                 <source>Rubric</source>

commit 87d2d3cd818b448eac1d3451a9d3d9deb970c70b

2417: fix a bug introduced in d4cc1cb where results for "created/modified until" searches via the search filter form didn't include the specified day itself

src/Controller/SearchController.php
@@ -367,7 +367,7 @@ class SearchController extends BaseController
                 $creationDateRange[0] = $date;
             }
             if (!empty($searchParams['creation_date_range']['right_date'])) {
-                $date = \DateTime::createFromFormat('Y-m-d', $searchParams['creation_date_range']['right_date'])->setTime(0, 0, 0);
+                $date = \DateTime::createFromFormat('Y-m-d', $searchParams['creation_date_range']['right_date'])->setTime(23, 59, 59);
                 $creationDateRange[1] = $date;
             }
             $searchData->setCreationDateRange($creationDateRange);
@@ -381,7 +381,7 @@ class SearchController extends BaseController
                 $modificationDateRange[0] = $date;
             }
             if (!empty($searchParams['modification_date_range']['right_date'])) {
-                $date = \DateTime::createFromFormat('Y-m-d', $searchParams['modification_date_range']['right_date'])->setTime(0, 0, 0);
+                $date = \DateTime::createFromFormat('Y-m-d', $searchParams['modification_date_range']['right_date'])->setTime(23, 59, 59);
                 $modificationDateRange[1] = $date;
             }
             $searchData->setModificationDateRange($modificationDateRange);