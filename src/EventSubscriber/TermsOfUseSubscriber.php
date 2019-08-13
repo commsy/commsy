faf971df8bfbba4fb53dcf7ef8983825301e6a52
Christoph Schönfeld <schoenfeld@effective-webwork.de>
2019-08-13 15:54:18 +0200

    check for valid user - some requests, for example when getting user images, are unable to get the portal user from the current context

--- src/EventSubscriber/TermsOfUseSubscriber.php
+++ src/EventSubscriber/TermsOfUseSubscriber.php
@@ -44,18 +44,20 @@ class TermsOfUseSubscriber implements EventSubscriberInterface
         if ($portal && $portal->withAGB()) {
             $portalUser = $this->legacyEnvironment->getCurrentUserItem()->getRelatedPortalUserItem();
 
-            $portalToUDate = new \DateTime($portal->getAGBChangeDate());
-            $userAcceptedData = new \DateTime($portalUser->getAGBAcceptanceDate());
+            if ($portalUser) {
+                $portalToUDate = new \DateTime($portal->getAGBChangeDate());
+                $userAcceptedData = new \DateTime($portalUser->getAGBAcceptanceDate());
 
-            if (!$portalUser->isRoot() && $userAcceptedData < $portalToUDate) {
-                // Redirect to tou site
-                if ($event->getRequest()->attributes->get('_route') !== 'app_tou_accept' &&
-                    $event->getRequest()->attributes->get('_route') !== 'app_profile_deleteaccount' &&
-                    $event->getRequest()->attributes->get('_route') !== 'app_logout_logout') {
-                    $event->setResponse(new RedirectResponse($this->urlGenerator->generate('app_tou_accept', [
-                        'roomId' => $portal->getItemID(),
-                        'redirect' => $event->getRequest()->getRequestUri(),
-                    ])));
+                if (!$portalUser->isRoot() && $userAcceptedData < $portalToUDate) {
+                    // Redirect to tou site
+                    if ($event->getRequest()->attributes->get('_route') !== 'app_tou_accept' &&
+                        $event->getRequest()->attributes->get('_route') !== 'app_profile_deleteaccount' &&
+                        $event->getRequest()->attributes->get('_route') !== 'app_logout_logout') {
+                        $event->setResponse(new RedirectResponse($this->urlGenerator->generate('app_tou_accept', [
+                            'roomId' => $portal->getItemID(),
+                            'redirect' => $event->getRequest()->getRequestUri(),
+                        ])));
+                    }
                 }
             }
         }
@@ -65,18 +67,20 @@ class TermsOfUseSubscriber implements EventSubscriberInterface
             if ($currentContext->withAGB()) {
                 $contextUser = $this->legacyEnvironment->getCurrentUserItem();
 
-                $contextToUDate = new \DateTime($currentContext->getAGBChangeDate());
-                $userAcceptedData = new \DateTime($contextUser->getAGBAcceptanceDate());
+                if ($contextUser) {
+                    $contextToUDate = new \DateTime($currentContext->getAGBChangeDate());
+                    $userAcceptedData = new \DateTime($contextUser->getAGBAcceptanceDate());
 
-                if (!$contextUser->isRoot() && $userAcceptedData < $contextToUDate) {
-                    // Redirect to tou site
-                    if ($event->getRequest()->attributes->get('_route') !== 'app_tou_accept' &&
-                        $event->getRequest()->attributes->get('_route') !== 'app_profile_deleteroomprofile' &&
-                        $event->getRequest()->attributes->get('_route') !== 'app_logout_logout') {
-                        $event->setResponse(new RedirectResponse($this->urlGenerator->generate('app_tou_accept', [
-                            'roomId' => $currentContext->getItemID(),
-                            'redirect' => $event->getRequest()->getRequestUri(),
-                        ])));
+                    if (!$contextUser->isRoot() && $userAcceptedData < $contextToUDate) {
+                        // Redirect to tou site
+                        if ($event->getRequest()->attributes->get('_route') !== 'app_tou_accept' &&
+                            $event->getRequest()->attributes->get('_route') !== 'app_profile_deleteroomprofile' &&
+                            $event->getRequest()->attributes->get('_route') !== 'app_logout_logout') {
+                            $event->setResponse(new RedirectResponse($this->urlGenerator->generate('app_tou_accept', [
+                                'roomId' => $currentContext->getItemID(),
+                                'redirect' => $event->getRequest()->getRequestUri(),
+                            ])));
+                        }
                     }
                 }
             }
