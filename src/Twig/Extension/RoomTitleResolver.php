9c24c36f814a5dbf3dce6a5f168d5f550e275abe
Dirk Johannßen <johannssen@effective-webwork.de>
2019-09-13 16:33:33 +0200

    #2965: Added grouprooms to the list of displayed rooms, added grouproom type information, added translated type information on community und project rooms.

--- src/Twig/Extension/RoomTitleResolver.php
+++ src/Twig/Extension/RoomTitleResolver.php
@@ -6,6 +6,7 @@ namespace App\Twig\Extension;
 use App\Services\LegacyEnvironment;
 use App\Utils\RoomService;
 use App\Utils\UserService;
+use Symfony\Contracts\Translation\TranslatorInterface;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
 
@@ -15,16 +16,19 @@ class RoomTitleResolver extends AbstractExtension
     private $legacyEnvironment;
     private $userService;
     private $roomService;
+    private $translator;
 
-    public function __construct( LegacyEnvironment $legacyEnvironment, UserService $userService, RoomService $roomService)
+    public function __construct( LegacyEnvironment $legacyEnvironment, UserService $userService, RoomService $roomService, TranslatorInterface $translator)
     {
         $this->userService = $userService;
         $this->legacyEnvironment = $legacyEnvironment;
         $this->roomService = $roomService;
+        $this->translator = $translator;
     }
 
     public function getFilters()
     {
+            // Hallo
         return [
             new TwigFilter('roomtitle', [$this, 'resolveRoomTitle']),
         ];
@@ -36,9 +40,19 @@ class RoomTitleResolver extends AbstractExtension
         $env = $this->legacyEnvironment->getEnvironment();
         $rooms = $env->getRoomManager()->getRelatedRoomListForUser($currentUser);
         $type = ' ';
-        foreach($rooms as $room){
-            if($room->getItemID() == $roomId){
+        foreach($rooms as $room) {
+            if ($room->getItemID() == $roomId) {
                 $type = $room->getType();
+                $type = $this->translator->trans($type, [], 'room');
+            } else {
+                if ($room->getType() == 'project') {
+                    $grouprooms = $room->getGroupRoomList();
+                    foreach ($grouprooms as $grouproom) {
+                        if ($grouproom->getItemID() == $roomId) {
+                            $type = $this->translator->trans('grouproom', [], 'room');
+                        }
+                    }
+                }
             }
         }
         return $this->roomService->getRoomTitle($roomId).' ('.$type.')';
