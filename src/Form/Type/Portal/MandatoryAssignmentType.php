8853cc30e201f62aae87ef2029dfaa9733116b02
Matthias Steffens <mat@extracts.de>
2020-06-26 11:10:18 +0200

    adds the lower form of the "Room categories" section in the CommSy 10 portal settings (WIP)

---
+++ src/Form/Type/Portal/MandatoryAssignmentType.php
@@ -0,0 +1,43 @@
+<?php
+namespace App\Form\Type\Portal;
+
+use App\Entity\Portal;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type as Types;
+
+class MandatoryAssignmentType extends AbstractType
+{
+    /**
+     * Builds the form.
+     *
+     * @param  FormBuilderInterface $builder The form builder
+     * @param  array                $options The options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('tagMandatory', Types\CheckboxType::class, [
+                'label' => 'Mandatory assignment',
+                'required' => false,
+            ])
+            ->add('save', Types\SubmitType::class, [
+                'label' => 'save',
+            ])
+        ;
+    }
+
+    /**
+     * Configures the options for this type.
+     *
+     * @param  OptionsResolver $resolver The resolver for the options
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Portal::class,
+            'translation_domain' => 'portal',
+        ]);
+    }
+}
