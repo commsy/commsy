cde88f540dbd1eb17824ec2328398c85a85efead
Christoph Schönfeld <schoenfeld@effective-webwork.de>
2020-06-30 17:53:24 +0200

    migrations merged / fixed

--- migrations/Version20190924140632.php
+++ migrations/Version20190924140632.php
@@ -21,7 +21,7 @@ final class Version20190924140632 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         $this->addSql('alter table auth_source add enabled tinyint default 1 not null after title;');
-        $this->addSql('alter table auth_source add type enum(\'local\', \'oidc\') default \'local\' not null after title;');
+        $this->addSql('alter table auth_source add type enum(\'local\', \'oidc\', \'ldap\') default \'local\' not null after title;');
         $this->addSql('alter table auth_source add `default` tinyint default 0 not null;');
         $this->addSql('alter table auth_source add add_account tinyint default 0 not null;');
         $this->addSql('alter table auth_source add change_username tinyint default 0 not null;');
@@ -51,6 +51,10 @@ final class Version20190924140632 extends AbstractMigration
 
                 case 'OpenID Connect':
                     $type = 'oidc';
+                    break;
+
+                case 'LDAP':
+                    $type = 'ldap';
                     break;
             }
 
