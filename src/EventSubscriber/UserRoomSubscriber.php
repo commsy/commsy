d2679916b507b58dee2075d9c9318a02061afdc8
Matthias Steffens <mat@extracts.de>
2020-07-17 15:50:43 +0200

    prevents UserRoomSubscriber methods to be always called twice
    in newer Symfony versions event listeners and subscribers get registered automatically, so there is no need to register them explicitely as services; and if they are registered as a service using "dot notation", it will get registered (and thus be called) twice

--- src/EventSubscriber/UserRoomSubscriber.php
+++ src/EventSubscriber/UserRoomSubscriber.php
@@ -31,8 +31,6 @@ class UserRoomSubscriber implements EventSubscriberInterface
 
     public function onUserJoinedRoom(UserJoinedRoomEvent $event)
     {
-        // TODO: figure out why, for a single room membership request, this callback method gets called twice
-
         $user = $event->getUser();
         $room = $event->getRoom();
 
@@ -61,15 +59,12 @@ class UserRoomSubscriber implements EventSubscriberInterface
 
     public function onRoomSettingsChanged(RoomSettingsChangedEvent $event)
     {
-        // TODO: figure out why this callback method gets called twice
-
         $oldRoom = $event->getOldRoom();
         $newRoom = $event->getNewRoom();
 
         // if the 'CREATE_USER_ROOMS' setting was just enabled, create user rooms for all existing users
         if (!$oldRoom->getShouldCreateUserRooms() && $newRoom->getShouldCreateUserRooms()) {
-            // TODO: as long as this subscriber gets called twice, below method will created duplicate user rooms & users!
-//            $this->userroomService->createUserroomsForRoomUsers($newRoom);
+            $this->userroomService->createUserroomsForRoomUsers($newRoom);
         }
     }
 }
