f0bc34215284901b99ba326a52982bd2e9dc55fe
Christoph Schönfeld <schoenfeld@effective-webwork.de>
2020-09-03 16:06:20 +0200

    zendframework/zend-soap has been abandoned

--- src/Controller/SoapController.php
+++ src/Controller/SoapController.php
@@ -1,13 +1,13 @@
 <?php
 namespace App\Controller;
 
+use Laminas\Soap\AutoDiscover;
+use Laminas\Soap\Server;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
-
-use Zend\Soap;
 
 class SoapController extends AbstractController
 {
@@ -30,56 +30,10 @@ class SoapController extends AbstractController
 
     private function handleWSDL()
     {
-// $plugin_config_file = 'etc/commsy/plugin.php';
-// $soap_functions_array = array();
-// if ( file_exists($plugin_config_file) ) {
-//   include_once($plugin_config_file);
-//   include_once('etc/cs_constants.php');
-//   include_once('etc/cs_config.php');
-//   include_once('functions/misc_functions.php');
-//   include_once('classes/cs_environment.php');
-//   $environment = new cs_environment();
-  
-//   if ( !empty($_GET['plugin']) ) {
-//     // full wsdl only for plugin
-//     $plugin_name = $_GET['plugin'];
-//     $plugin_class = $environment->getPluginClass($plugin_name);
-//     if ( !empty($plugin_class)
-//           and method_exists($plugin_class, 'getFullWSDL') 
-//        ) {
-//         $wsdl = $plugin_class->getFullWSDL();
-//         unset($plugin_class);
-//         if ( !empty($wsdl) ) {
-//            echo($wsdl);
-//            exit();
-//         }
-//     }
-//   } else {
-//     // merge plugin soap functions into CommSy soap functions
-//      $soap_functions_array = plugin_hook_output_all('getSOAPAPIArray',array(),'ARRAY');
-//   }
-// }
-
-// // soap_functions from classes
-// if ( !isset($environment) ) {
-// include_once('etc/cs_constants.php');
-// include_once('etc/cs_config.php');
-// include_once('functions/misc_functions.php');
-// include_once('classes/cs_environment.php');
-// $environment = new cs_environment();
-// }
-
-// $connection_obj = $environment->getCommSyConnectionObject();
-// if ( !empty($connection_obj) ) {
-// $soap_functions_array_from_class = $connection_obj->getSoapFunctionArray();
-// if ( !empty($soap_functions_array_from_class) ) {
-//    $soap_functions_array = array_merge($soap_functions_array,$soap_functions_array_from_class);
-// }
-// }
         $uri = $this->generateUrl('app_soap_soap', [], UrlGeneratorInterface::ABSOLUTE_URL);
 
         // auto discover
-        $autoDiscover = new Soap\AutoDiscover();
+        $autoDiscover = new AutoDiscover();
         $autoDiscover->setClass($this->get('commsy.api.soap'));
         $autoDiscover->setUri($uri);
 
@@ -99,27 +53,9 @@ class SoapController extends AbstractController
 
     private function handleSOAP()
     {
-// if ( !empty($_GET['plugin']) ) {
-//    $plugin_config_file = 'etc/commsy/plugin.php';
-//    if ( file_exists($plugin_config_file) ) {
-//       include_once($plugin_config_file);
-//       $plugin_name = $_GET['plugin'];
-//       $plugin_class = $environment->getPluginClass($plugin_name);
-//       if ( !empty($plugin_class)
-//           and method_exists($plugin_class, 'getURIforSoapServer') 
-//         ) {
-//          $uri_server = $plugin_class->getURIforSoapServer();
-//          unset($plugin_class);
-//          if ( !empty($uri_server) ) {
-//             $uri = $uri_server;
-//          }
-//       }
-//    }
-// }
-
         $uri = $this->generateUrl('app_soap_soap', [], UrlGeneratorInterface::ABSOLUTE_URL);
 
-        $soapServer = new Soap\Server(null, [
+        $soapServer = new Server(null, [
             'location' => $uri,
             'uri' => $uri,
         ]);
