c089f233f771d9f2f37eb875957bca7bb4b3d992
Matthias Steffens <mat@extracts.de>
2020-11-12 15:22:54 +0100

    #3747: adds a new "SavedSearch" entity class which represents records from the "saved_searches" table in Doctrine

---
+++ src/Entity/SavedSearch.php
@@ -0,0 +1,145 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * SavedSearch
+ *
+ * @ORM\Table(name="saved_searches", indexes={@ORM\Index(name="account_id", columns={"account_id"})})
+ * @ORM\Entity
+ */
+class SavedSearch
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="account_id", type="integer", nullable=false)
+     */
+    private $accountId;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="context_id", type="integer", nullable=false)
+     */
+    private $contextId;
+
+    /**
+     * @var int|null
+     *
+     * @ORM\Column(name="deleter_id", type="integer", nullable=true)
+     */
+    private $deleterId;
+
+    /**
+     * @var \DateTime|null
+     *
+     * @ORM\Column(name="deletion_date", type="datetime", nullable=true)
+     */
+    private $deletionDate;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=255, nullable=false)
+     */
+    private $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="search_url", type="string", length=3000, nullable=false)
+     */
+    private $searchUrl;
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getAccountId(): ?int
+    {
+        return $this->accountId;
+    }
+
+    public function setAccountId(int $accountId): self
+    {
+        $this->accountId = $accountId;
+
+        return $this;
+    }
+
+    public function getContextId(): ?int
+    {
+        return $this->contextId;
+    }
+
+    public function setContextId(int $contextId): self
+    {
+        $this->contextId = $contextId;
+
+        return $this;
+    }
+
+    public function getDeleterId(): ?int
+    {
+        return $this->deleterId;
+    }
+
+    public function setDeleterId(?int $deleterId): self
+    {
+        $this->deleterId = $deleterId;
+
+        return $this;
+    }
+
+    public function getDeletionDate(): ?\DateTimeInterface
+    {
+        return $this->deletionDate;
+    }
+
+    public function setDeletionDate(?\DateTimeInterface $deletionDate): self
+    {
+        $this->deletionDate = $deletionDate;
+
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getSearchUrl(): ?string
+    {
+        return $this->searchUrl;
+    }
+
+    public function setSearchUrl(string $searchUrl): self
+    {
+        $this->searchUrl = $searchUrl;
+
+        return $this;
+    }
+
+
+}
