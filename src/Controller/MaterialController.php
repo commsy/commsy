44c1c1112584eb862324eec6b2ef7a8e7d166c16
Christoph Schönfeld <schoenfeld@effective-webwork.de>
2020-12-18 15:44:22 +0100

    Revert "#3724: altered function to be called in order to set the buzzword list properly."
    This reverts commit 686e94b02d38af2cb2e4ad500cc6cb276e488b5b.

--- src/Controller/MaterialController.php
+++ src/Controller/MaterialController.php
@@ -282,9 +282,8 @@ class MaterialController extends BaseController
             $material = $materialService->getMaterialByVersion($itemId, $versionId);
         }
 
-        $itemController = $this->get('commsy.item_controller');
         $infoArray = $this->getDetailInfo($roomId, $itemId, $versionId);
-        $hashTags = $itemController->getLinkedHashTags($itemId, $roomId, $legacyEnvironment);
+        $hashTags = $this->getHashtags($roomId, $legacyEnvironment);
         $hashNameArray = [];
         foreach ($hashTags as $hashTag) {
             $hashNameArray[] = $labelService->getLabel($hashTag)->getName();
@@ -390,6 +389,23 @@ class MaterialController extends BaseController
             'alert' => $alert,
             'pathTopicItem' => $pathTopicItem,
         );
+    }
+
+    private function getHashtags($roomId, $legacyEnvironment) {
+        $hashtags = [];
+
+        /** @var \cs_buzzword_manager $buzzwordManager */
+        $buzzwordManager = $legacyEnvironment->getBuzzwordManager();
+        $buzzwordManager->setContextLimit($roomId);
+        $buzzwordManager->setTypeLimit('buzzword');
+        $buzzwordManager->select();
+        $buzzwordList = $buzzwordManager->get();
+        $buzzwordItem = $buzzwordList->getFirst();
+        while ($buzzwordItem) {
+            $hashtags[$buzzwordItem->getItemId()] = $buzzwordItem->getTitle();
+            $buzzwordItem = $buzzwordList->getNext();
+        }
+        return array_flip($hashtags);
     }
 
     /**
