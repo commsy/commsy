f8c6f579909cf185aab462346f420c3cbab8c22f
Matthias Steffens <mat@extracts.de>
2021-01-16 20:30:26 +0100

    #3514: avoids an exception if  or selectedCategories is null

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -228,16 +228,20 @@ class SearchController extends BaseController
         }
 
         $selectedHashtags = $searchData->getSelectedHashtags();
-        foreach ($selectedHashtags as $hashtag) {
-            if (!array_key_exists($hashtag, $countsByHashtag)) {
-                $searchData->addHashtags([$hashtag => 0]);
+        if (!empty($selectedHashtags)) {
+            foreach ($selectedHashtags as $hashtag) {
+                if (!array_key_exists($hashtag, $countsByHashtag)) {
+                    $searchData->addHashtags([$hashtag => 0]);
+                }
             }
         }
 
         $selectedCategories = $searchData->getSelectedCategories();
-        foreach ($selectedCategories as $category) {
-            if (!array_key_exists($category, $countsByCategory)) {
-                $searchData->addCategories([$category => 0]);
+        if (!empty($selectedCategories)) {
+            foreach ($selectedCategories as $category) {
+                if (!array_key_exists($category, $countsByCategory)) {
+                    $searchData->addCategories([$category => 0]);
+                }
             }
         }
 
