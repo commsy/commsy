c162d1c4b29000849b5dc9faffb9d0038793309e
Matthias Steffens <mat@extracts.de>
2021-02-03 16:46:33 +0100

    #3750: Elastic Search index: similar to 4bea3bf, fixes an issue with indexing of the "description" property for room items
    by duplicating the room's "roomDescription" to the "description" property we ensure that room items can be found when filtering search results by description

--- src/Entity/Room.php
+++ src/Entity/Room.php
@@ -691,6 +691,16 @@ class Room
     }
 
     /**
+     * Get description (which equals the room's roomDescription)
+     *
+     * @return string
+     */
+    public function getDescription(): string
+    {
+        return $this->getRoomDescription();
+    }
+
+    /**
      * Set lastlogin
      *
      * @param \DateTime $lastlogin
