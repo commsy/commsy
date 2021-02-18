f1e65e3222e3f46b6d8f2b679b9999589c5bdbe9
Christoph Schönfeld <schoenfeld@effective-webwork.de>
2021-02-18 17:45:59 +0100

    ldap authentication wip

--- src/Form/Type/Portal/AuthLdapType.php
+++ src/Form/Type/Portal/AuthLdapType.php
@@ -63,25 +63,44 @@ class AuthLdapType extends AbstractType
                 'required' => false,
             ])
             ->add('serverUrl', UrlType::class, [
+                'attr' => [
+                    'placeholder' => 'ldaps://my-server:636',
+                ],
                 'label' => 'Server URL',
                 'help' => 'ldaps://example.org',
                 'default_protocol' => 'ldaps://',
             ])
             ->add('uidKey', TextType::class, [
+                'attr' => [
+                    'placeholder' => 'sAMAccountName',
+                ],
                 'label' => 'UID Key: Username',
-                'help' => 'The entry’s key to use as its UID (sAMAccountName).',
+                'help' => 'The entry’s key to use as its UID.',
             ])
             ->add('baseDn', TextType::class, [
+                'attr' => [
+                    'placeholder' => 'dc=example,dc=com',
+                ],
                 'label' => 'Base DN',
                 'help' => 'The base DN for the directory.',
             ])
             ->add('searchDn', TextType::class, [
+                'attr' => [
+                    'placeholder' => 'cn=read-only-admin,dc=example,dc=com',
+                ],
                 'label' => 'Search DN',
                 'help' => 'The read-only user’s DN, which will be used to authenticate against the LDAP server to fetch the user’s information.',
             ])
             ->add('searchPassword', TextType::class, [
                 'label' => 'Search Password',
                 'help' => 'The read-only user’s password, which will be used to authenticate against the LDAP server to fetch the user’s information.',
+            ])
+            ->add('authDn', TextType::class, [
+                'attr' => [
+                    'placeholder' => 'uid={username},dc=example,dc=com',
+                ],
+                'label' => 'Auth DN',
+                'help' => 'This key defines the form of the string used to compose the DN of the user, from the username. The {username} string is replaced by the actual username of the person trying to authenticate.',
             ])
             ->add('createRoom', CheckboxType::class, [
                 'label' => 'Users may create rooms',
