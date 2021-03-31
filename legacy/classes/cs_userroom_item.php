68845048f00825efc89ed8fc2014bf8d413bd2f7
Andres <agf@teamq.biz>
2021-03-31 17:05:18 -0500

    Calling the correct fos_elastic method

--- legacy/classes/cs_userroom_item.php
+++ legacy/classes/cs_userroom_item.php
@@ -89,7 +89,7 @@ class cs_userroom_item extends cs_room_item
    public function updateElastic()
    {
       global $symfonyContainer;
-      $objectPersister = $symfonyContainer->get('fos_elastica.object_persister.commsy.room');
+      $objectPersister = $symfonyContainer->get('fos_elastica.object_persister.commsy_room.room');
       $em = $symfonyContainer->get('doctrine.orm.entity_manager');
       $repository = $em->getRepository('App:Room');
 
