16372dc32d0b826ff4df9365234aef889b854398
Christoph Schönfeld <schoenfeld@effective-webwork.de>
2021-10-19 10:15:10 +0200

    database checks reworked

---
+++ src/Database/GeneralCheck.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Database;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+abstract class GeneralCheck implements DatabaseCheck
+{
+    /**
+     * @var EntityManagerInterface
+     */
+    protected EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getPriority()
+    {
+        return 999;
+    }
+
+    protected function executeSQL(string $sql, SymfonyStyle $io)
+    {
+        if ($io->isVerbose()) {
+            $io->note('Executing ' . $sql);
+        }
+
+        $conn = $this->entityManager->getConnection();
+        $stmt = $conn->prepare($sql);
+        $stmt->execute();
+    }
+}
