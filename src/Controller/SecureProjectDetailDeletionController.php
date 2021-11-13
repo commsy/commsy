6bc82df2b66e3a126d8e9240d3215d9b3f39a620
Matthias Steffens <mat@extracts.de>
2021-11-13 21:10:31 +0100

    #3859: authorized users can now unlock a locked room on room detail views

--- src/Controller/SecureProjectDetailDeletionController.php
+++ src/Controller/SecureProjectDetailDeletionController.php
@@ -125,6 +125,41 @@ class SecureProjectDetailDeletionController extends AbstractController
             'relatedGroupRooms' => $relatedGroupRooms,
             'lock_form' => $lockForm->createView(),
         ];
+    }
 
+    /**
+     * @Route("/room/{roomId}/settings/unlock/{itemId}")
+     * @Template
+     * @Security("is_granted('ROOM_MODERATOR', itemId) or is_granted('PARENT_ROOM_MODERATOR', itemId)")
+     */
+    public function unlock(
+        $roomId,
+        Request $request,
+        RoomService $roomService,
+        $itemId
+    )
+    {
+        $roomItem = $roomService->getRoomItem($itemId);
+        if (!$roomItem) {
+            throw $this->createNotFoundException('No room found for id ' . $itemId);
+        }
+
+        $isGroupRoom = $roomItem->isGroupRoom();
+        $group = $isGroupRoom ? $roomItem->getLinkedGroupItem() : null;
+        $groupId = $group ? $group->getItemID() : null;
+
+        $isProjectRoom = $roomItem->isProjectRoom();
+        $communityRooms = $isProjectRoom ? $roomService->getCommunityRoomsForRoom($roomItem) : [];
+        $communityRoomIds = $roomService->getIdsForRooms($communityRooms);
+        $projectRoomIsViewedFromItsCommunityRoom = ($isProjectRoom && in_array($roomId, $communityRoomIds));
+        $detailRoute = $isGroupRoom ? 'app_group_detail' : ($projectRoomIsViewedFromItsCommunityRoom ? 'app_project_detail' : 'app_room_detail') ;
+
+        $roomItem->unlock();
+        $roomItem->save();
+
+        return $this->redirectToRoute($detailRoute, [
+            'roomId' => $roomId,
+            'itemId' => $isGroupRoom ? $groupId : $itemId,
+        ]);
     }
 }
