fbfe07e4e1e54266a9735def98756e0ed0c33d99
Christoph Schönfeld <schoenfeld@effective-webwork.de>
2022-03-28 12:28:58 +0200

    fix #4417 - shibboleth without explicit idp configuration (federation)

--- assets/uikit3/js/commsy/Login.ts
+++ assets/uikit3/js/commsy/Login.ts
@@ -1,12 +1,14 @@
 'use strict';
 
 export function handleShibIdPSelect() {
-    let $link = $('a#shib_login_link');
     let $select = $('select#shib_login_idps_select');
 
-    $link.attr('href', String($select.val()));
+    if($select.length) {
+        let $link = $('a#shib_login_link');
+        $link.attr('href', String($select.val()));
 
-    $select.on('change', function () {
-        $link.attr('href', String($(this).val()));
-    });
+        $select.on('change', function () {
+            $link.attr('href', String($(this).val()));
+        });
+    }
 }
