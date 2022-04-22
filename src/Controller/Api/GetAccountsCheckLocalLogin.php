08f0f71c68bd4b72e7dd1f770219d95549991375
Christoph Schönfeld <schoenfeld@effective-webwork.de>
2022-04-22 11:50:37 +0200

    mrbs wip

--- src/Controller/Api/GetAccountsCheckLocalLogin.php
+++ src/Controller/Api/GetAccountsCheckLocalLogin.php
@@ -3,17 +3,58 @@
 namespace App\Controller\Api;
 
 use App\Entity\Account;
+use App\Entity\AuthSource;
+use App\Entity\AuthSourceLocal;
+use App\Repository\AccountsRepository;
+use App\Repository\AuthSourceRepository;
+use App\Repository\PortalRepository;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class GetAccountsCheckLocalLogin
 {
+    /**
+     * @var PortalRepository
+     */
+    private PortalRepository $portalRepository;
+
+    /**
+     * @var AccountsRepository
+     */
+    private AccountsRepository $accountsRepository;
+
+    public function __construct(
+        PortalRepository $portalRepository,
+        AccountsRepository $accountsRepository
+    ) {
+        $this->portalRepository = $portalRepository;
+        $this->accountsRepository = $accountsRepository;
+    }
+
     public function __invoke(Account $data): Account
     {
-        $account = new Account();
-        $account->setFirstname('firstname');
-        $account->setLastname('lastname');
-        $account->setUsername('username');
-        $account->setEmail('e@mail.de');
+        var_dump($data);
 
-        return $account;
+        $portal = $this->portalRepository->findActivePortal($data->getContextId());
+
+        if ($portal) {
+            /** @var AuthSourceLocal $localSource */
+            $localSource = $portal->getAuthSources()->filter(function (AuthSource $authSource) {
+                return $authSource instanceof AuthSourceLocal;
+            })->first();
+
+            if ($localSource) {
+                $account = $this->accountsRepository->findOneByCredentials(
+                    $data->getUsername(),
+                    $data->getContextId(),
+                    $localSource
+                );
+
+                if ($account) {
+                    return $account;
+                }
+            }
+        }
+
+        throw new NotFoundHttpException('Account not found');
     }
 }
---
+++ src/Dto/LocalLoginInput.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Dto;
+
+/**
+ *
+ */
+final class LocalLoginInput
+{
+
+}
