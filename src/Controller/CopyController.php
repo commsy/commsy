5450f23b381e4ccf7833170987b9be888fff620d
Dirk Johannßen <johannssen@effective-webwork.de>
2022-05-13 16:50:02 +0200

    GUI translations

--- src/Controller/CopyController.php
+++ src/Controller/CopyController.php
@@ -139,7 +139,7 @@ class CopyController extends BaseController
         return [
             'roomId' => $roomId,
             'form' => $filterForm->createView(),
-            'module' => 'copies',
+            'module' => 'marked',
             'itemsCountArray' => $itemsCountArray,
             'usageInfo' => null,
             'roomname' => $roomItem->getTitle(),
