470f4fca95178d4ed0ab2e1c51bd934f9a41e871
Matthias Steffens <mat@extracts.de>
2022-07-01 19:35:53 +0200

    #248 more (nonfunctional WIP) changes to load custom form elements for actions that require this

--- assets/uikit2/js/commsy/actions/HashtaggingAction.ts
+++ assets/uikit2/js/commsy/actions/HashtaggingAction.ts
@@ -1,5 +1,5 @@
 import {ActionData, ActionResponse} from "./Actions";
-import {XHRAction} from "./AbstractAction";
+import {BaseAction, XHRAction} from "./AbstractAction";
 
 'use strict';
 
@@ -14,16 +14,14 @@ export class HashtaggingAction extends XHRAction {
 
     public onPostLoadCustomFormData(backendResponse: ActionResponse): Promise<ActionResponse> {
         return new Promise<ActionResponse>((resolve) => {
+            let responseHtml: string = backendResponse.html;
+
+            let $customChoicesPlaceholder = $('#commsy-select-actions-custom-choices');
+            $customChoicesPlaceholder.html(responseHtml);
+
             // TODO: register event listener onchange and store all hashtags selected by the user in $chosenHashtagIds
 
             resolve(backendResponse);
-        });
-    }
-
-    // TODO: why is this method overwrite necessary here?
-    public preExecute(actionActor: JQuery): Promise<void> {
-        return new Promise<void>((resolve) => {
-            resolve();
         });
     }
 
