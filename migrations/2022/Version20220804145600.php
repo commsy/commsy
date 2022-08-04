2eb0547ca112bdf1162f88d1090767c986266499
Christoph Schönfeld <schoenfeld@effective-webwork.de>
2022-08-04 15:04:53 +0200

    migration renamed

--- migrations/2022/Version20220804145600.php
+++ migrations/2022/Version20220804145600.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220803124700 extends AbstractMigration
+final class Version20220804145600 extends AbstractMigration
 {
     public function up(Schema $schema): void
     {
@@ -20,6 +20,6 @@ final class Version20220803124700 extends AbstractMigration
 
     public function down(Schema $schema): void
     {
-
+        $this->throwIrreversibleMigrationException();
     }
 }
