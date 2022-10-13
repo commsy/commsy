f1168c9b19b8a1c16c23e938a3598a163113d8b3
Matthias Steffens <mat@extracts.de>
2022-10-13 17:30:52 +0200

    #4604 adds input validation to the "Workspace identifier" section of the general room settings to ensure that (if an identifier has been given) it is unique among all rooms

---
+++ src/Validator/Constraints/UniqueRoomSlugValidator.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Validator\Constraints;
+
+use App\Entity\Room;
+use App\Entity\ZzzRoom;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+use Symfony\Component\Validator\Exception\UnexpectedTypeException;
+
+class UniqueRoomSlugValidator extends ConstraintValidator
+{
+    /** @var EntityManagerInterface $entityManager */
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * Checks if the passed room slug is unique (i.e., if it doesn't already exist for another room in the database).
+     *
+     * @param mixed $roomSlug The room slug that should be validated
+     * @param Constraint $constraint The constraint for the validation
+     */
+    public function validate($roomSlug, Constraint $constraint)
+    {
+        if (!$constraint instanceof UniqueRoomSlug) {
+            throw new UnexpectedTypeException($constraint, UniqueRoomSlug::class);
+        }
+
+        // custom constraints should ignore null and empty values to allow
+        // other constraints (NotBlank, NotNull, etc.) take care of that
+        if ($roomSlug === null || $roomSlug === '') {
+            return;
+        }
+
+        if (!is_string($roomSlug)) {
+            throw new UnexpectedTypeException($roomSlug, 'string');
+        }
+
+        $roomItem = $constraint->roomItem;
+
+        if ($roomItem->getSlug() === $roomSlug) {
+            // room slug hasn't changed
+            return;
+        }
+
+        $roomRepository = $this->entityManager->getRepository($roomItem->isArchived() ? ZzzRoom::class : Room::class);
+        /** @var Room|ZzzRoom|null $room */
+        $room = $roomRepository->findOneByRoomSlug($roomSlug, $roomItem->getContextId());
+
+        if ($room && $room->getItemId() !== $roomItem->getItemID()) {
+            $this->context->buildViolation($constraint->message)
+                ->addViolation();
+        }
+    }
+}
