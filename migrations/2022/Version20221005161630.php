beb371df58b690d825d633752804d7a83d277553
Matthias Steffens <mat@extracts.de>
2022-11-09 17:35:43 +0100

    #4604 similar to 2803610, adds a "slug" column/property to the "room_privat" database table & entities since this seems to be needed by legacy code

--- migrations/2022/Version20221005161630.php
+++ migrations/2022/Version20221005161630.php
@@ -14,18 +14,20 @@ final class Version20221005161630 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return 'add slug to room & zzz_room';
+        return 'add slug to room, room_privat & zzz_room';
     }
 
     public function up(Schema $schema): void
     {
         $this->addSql('ALTER TABLE room ADD slug VARCHAR(255) NULL;');
+        $this->addSql('ALTER TABLE room_privat ADD slug VARCHAR(255) NULL;');
         $this->addSql('ALTER TABLE zzz_room ADD slug VARCHAR(255) NULL;');
     }
 
     public function down(Schema $schema): void
     {
         $this->addSql('ALTER TABLE room DROP COLUMN slug');
+        $this->addSql('ALTER TABLE room_privat DROP COLUMN slug');
         $this->addSql('ALTER TABLE zzz_room DROP COLUMN slug');
     }
 }
