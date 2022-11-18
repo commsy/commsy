d79446f015dcb7f6f5aa92be879ad9bdec0adcf9
Christoph Schönfeld <schoenfeld@effective-webwork.de>
2022-11-18 22:22:23 +0100

    #4763 - error pages

---
+++ tests/Functional/ErrorCest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Tests\Functional;
+
+use App\Tests\FunctionalTester;
+
+class ErrorCest
+{
+    public function notFound(FunctionalTester $I)
+    {
+        $I->amOnPage('/_error/404');
+        $I->see('Resource not found');
+    }
+
+    public function accessForbbiden(FunctionalTester $I)
+    {
+        $I->amOnPage('/_error/403');
+        $I->see('Access forbidden');
+    }
+
+    public function generic(FunctionalTester $I)
+    {
+        $I->amOnPage('/_error/500');
+        $I->see('An error occurred');
+    }
+}
