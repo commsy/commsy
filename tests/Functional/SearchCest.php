f2bdd85c9eb1b4473faf5db6d967ce029603de58
Matthias Steffens <mat@extracts.de>
2022-11-25 17:31:23 +0100

    #4766 adds more search tests for typical user searches

--- tests/Functional/SearchCest.php
+++ tests/Functional/SearchCest.php
@@ -22,7 +22,16 @@ class SearchCest
         $legacyEnvironment = $I->grabService(LegacyEnvironment::class);
         $materialManager = $legacyEnvironment->getEnvironment()->getMaterialManager();
 
-        $titles = ['Aufgabe A', 'Aufgabe B', 'Aufgabe C', 'Aufgabe D'];
+        $titles = [
+            'Aufgabe A',
+            'Aufgabe B',
+            'Aufgabe C',
+            'Schifffahrtsmuseum',
+            'Hörverstehen',
+            'Willkommen auf dem Gymnasium!',
+            'Hausaufgaben: bitte bis heute Abend erledigen',
+            'Arbeiterunfallversicherungsgesetz',
+        ];
 
         foreach ($titles as $title) {
             $material = $materialManager->getNewItem();
@@ -42,4 +51,47 @@ class SearchCest
         $search->performSearch($this->roomId, 'Auf');
         $I->see('Aufgabe A', 'ul#search-feed article h4 a');
     }
+
+    public function findLongTitlePrefix(User $I, Search $search)
+    {
+        $search->performSearch($this->roomId, 'Schifffahrt');
+        $I->see('Schifffahrtsmuseum', 'ul#search-feed article h4 a');
+    }
+
+    public function findTitleWithUmlautExact(User $I, Search $search)
+    {
+        $search->performSearch($this->roomId, 'Hörverstehen');
+        $I->see('Hörverstehen', 'ul#search-feed article h4 a');
+    }
+
+    public function findTitleWithUmlautPrefix(User $I, Search $search)
+    {
+        $search->performSearch($this->roomId, 'Hör');
+        $I->see('Hörverstehen', 'ul#search-feed article h4 a');
+    }
+
+    public function findTitleWithUmlautPrefixTransliterated(User $I, Search $search)
+    {
+        $search->performSearch($this->roomId, 'Hoer');
+        $I->see('Hörverstehen', 'ul#search-feed article h4 a');
+    }
+
+    public function findWordInMultiWordTitle(User $I, Search $search)
+    {
+        $search->performSearch($this->roomId, 'Abend');
+        $I->see('Hausaufgaben: bitte bis heute Abend erledigen', 'ul#search-feed article h4 a');
+    }
+
+    public function findWordInMultiWordTitleWithPunctuation(User $I, Search $search)
+    {
+        $search->performSearch($this->roomId, 'Gymnasium');
+        $I->see('Willkommen auf dem Gymnasium!', 'ul#search-feed article h4 a');
+    }
+
+    // TODO: allow for prefix matches with very long prefixes (>20 chars)
+//    public function findVeryLongTitlePrefix(User $I, Search $search)
+//    {
+//        $search->performSearch($this->roomId, 'Arbeiterunfallversicherung');
+//        $I->see('Arbeiterunfallversicherungsgesetz', 'ul#search-feed article h4 a');
+//    }
 }
