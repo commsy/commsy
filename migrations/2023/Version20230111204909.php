fc1c0b515d0969b777acb95b38e76ec64ed167a6
Matthias Steffens <mat@extracts.de>
2023-01-11 21:53:33 +0100

    #3038 increases the time stamp for the migration added in f2e988a so that it's again the latest migration

--- migrations/2023/Version20230111204909.php
+++ migrations/2023/Version20230111204909.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20221207172836 extends AbstractMigration
+final class Version20230111204909 extends AbstractMigration
 {
     public function getDescription(): string
     {
