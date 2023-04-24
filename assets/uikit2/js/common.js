010300c916891a9693e41d94addd2c2bd4e19b13
Christoph Schönfeld <schoenfeld@effective-webwork.de>
2023-04-24 19:37:29 +0200

    #4201

--- assets/uikit2/js/common.js
+++ assets/uikit2/js/common.js
@@ -1,7 +1,6 @@
 // CSS
 require('jstree/dist/themes/default/style.css');
 require('nprogress/nprogress.css');
-require('fullcalendar/dist/fullcalendar.css');
 require('tooltipster/dist/css/tooltipster.bundle.css');
 require('video.js/dist/video-js.css');
 
@@ -12,36 +11,34 @@ global.$ = global.jQuery = $;
 require('jstree/dist/jstree');
 require('expose-loader?exposes=NProgress!nprogress/nprogress');
 require('moment/moment');
-require('fullcalendar/dist/fullcalendar');
-require('fullcalendar/dist/locale-all');
 require('tooltipster/dist/js/tooltipster.bundle');
 require('expose-loader?exposes=URI!urijs/src/URI');
 require('video.js/dist/video');
 require('jscolor-picker/jscolor');
 
 require(['uikit'], function () {
-    require('uikit/dist/js/components/autocomplete');
-    require('uikit/dist/js/components/search');
-    require('uikit/dist/js/components/nestable');
-    require('uikit/dist/js/components/tooltip');
-    require('uikit/dist/js/components/grid');
-    require('uikit/dist/js/components/accordion');
-    require('uikit/dist/js/components/upload');
-    require('uikit/dist/js/components/sticky');
-    require('uikit/dist/js/components/slider');
-    require('uikit/dist/js/components/lightbox');
-    require('uikit/dist/js/components/sortable');
-    require('uikit/dist/js/components/notify');
-    require('uikit/dist/js/components/parallax');
-    require('uikit/dist/js/components/datepicker');
-    require('uikit/dist/js/components/timepicker');
-    require('uikit/dist/js/components/form-select');
+  require('uikit/dist/js/components/autocomplete');
+  require('uikit/dist/js/components/search');
+  require('uikit/dist/js/components/nestable');
+  require('uikit/dist/js/components/tooltip');
+  require('uikit/dist/js/components/grid');
+  require('uikit/dist/js/components/accordion');
+  require('uikit/dist/js/components/upload');
+  require('uikit/dist/js/components/sticky');
+  require('uikit/dist/js/components/slider');
+  require('uikit/dist/js/components/lightbox');
+  require('uikit/dist/js/components/sortable');
+  require('uikit/dist/js/components/notify');
+  require('uikit/dist/js/components/parallax');
+  require('uikit/dist/js/components/datepicker');
+  require('uikit/dist/js/components/timepicker');
+  require('uikit/dist/js/components/form-select');
 
-    // import commsy modules
-    var commsyModules = require.context('./commsy', true, /\.js$/);
-    commsyModules.keys().forEach(function(key) {
-        commsyModules(key);
-    });
+  // import commsy modules
+  var commsyModules = require.context('./commsy', true, /\.js$/);
+  commsyModules.keys().forEach(function(key) {
+      commsyModules(key);
+  });
 });
 
 import {DetailActionManager} from "./commsy/actions/DetailActionManager";
@@ -59,6 +56,15 @@ portfolio.bootstrap();
 import {MathJax} from "./commsy/MathJax";
 let mathJax = new MathJax();
 mathJax.bootstrap();
+
+import '@fullcalendar/core/main.css';
+import '@fullcalendar/daygrid/main.css';
+import '@fullcalendar/timegrid/main.css';
+import deLocale from '@fullcalendar/core/locales/de';
+import enLocale from '@fullcalendar/core/locales/en-gb';
+import { setup as setupCalendar } from "./commsy/fullcalendar";
+setupCalendar([deLocale, enLocale], 'calendar');
+setupCalendar([deLocale, enLocale], 'calendarDashboard', false);
 
 // start the Stimulus application
 import '../../bootstrap';
---
+++ assets/uikit2/js/commsy/fullcalendar.ts
@@ -0,0 +1,166 @@
+import { Calendar } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import EventApi from "@fullcalendar/core/api/EventApi";
+
+export function setup(locales, id: string, editable: boolean = true): void {
+  document.addEventListener('DOMContentLoaded', function() {
+    const calendarEl = document.getElementById(id);
+    if (calendarEl) {
+      let calendar = new Calendar(calendarEl, {
+        plugins: [ dayGridPlugin, timeGridPlugin, interactionPlugin ],
+        businessHours: {
+          start: '8:00',
+          end: '16:00',
+          dow: [1, 2, 3, 4, 5]
+        },
+        defaultView: calendarEl.dataset.defaultView ?? null,
+        editable: editable,
+        events: calendarEl.dataset.eventsUrl,
+        header: {
+          left: 'dayGridMonth,timeGridWeek,timeGridDay',
+          center: 'title',
+          right: 'prevYear,prev,today,next,nextYear'
+        },
+        locales: locales,
+        locale: calendarEl.dataset.locale === 'de' ? 'de' : 'en-gb',
+        dateClick(info) {
+          if (editable) {
+            const createEl = document.getElementById('create-date');
+            if (createEl) {
+              const start = encodeURIComponent(info.date.toISOString());
+              window.location.href = calendarEl.dataset.eventsCreateUrl + '/' + start;
+            }
+          }
+        },
+        eventClick(info)  {
+          let listUrl = decodeURI(calendarEl.dataset.eventsListUrl);
+          listUrl = listUrl.replace('<roomId>', info.event.extendedProps.contextId);
+          window.location.href = listUrl + '/' + info.event.id;
+        },
+        eventMouseEnter(info) {
+          // @ts-ignore
+          $(info.el).tooltipster({
+            content: $(renderEvent(calendarEl, info.event)),
+            delay: 0,
+            animationDuration: 0,
+          }).tooltipster('show');
+        },
+        eventDrop(info) {
+          editEvent(calendarEl, info.event, info.revert);
+        },
+        eventResize(info) {
+          editEvent(calendarEl, info.event, info.revert);
+        }
+      });
+
+      calendar.render();
+
+      setupTooltips(calendarEl);
+    }
+  });
+}
+
+function setupTooltips(calendarEl: HTMLElement): void {
+  const translations = JSON.parse(calendarEl.dataset.translations);
+
+  // @ts-ignore
+  $('.fc-prevYear-button').tooltipster({
+    content: translations.prevYear,
+  });
+
+  // @ts-ignore
+  $('.fc-prev-button').tooltipster({
+    content: translations.prev,
+  });
+
+  // @ts-ignore
+  $('.fc-next-button').tooltipster({
+    content: translations.next,
+  });
+
+  // @ts-ignore
+  $('.fc-nextYear-button').tooltipster({
+    content: translations.nextYear,
+  });
+}
+
+function editEvent(calendarEl: HTMLElement, event: EventApi, revert: () => void): void {
+  UIkit.modal.confirm(calendarEl.dataset.confirmChange, function () {
+    event.setExtendedProp('description', '...');
+
+    let listUrl = decodeURI(calendarEl.dataset.eventsListUrl);
+    listUrl = listUrl.replace('<roomId>', event.extendedProps.contextId);
+
+    $.ajax({
+      url: listUrl + '/' + event.id + '/calendaredit',
+      type: 'POST',
+      data: JSON.stringify({
+        start: event.start,
+        end: event.end,
+        allDay: event.allDay
+      })
+    }).done((data) => {
+      event.setExtendedProp('description', data.description);
+
+      UIkit.notify({
+        message: data.message,
+        status: data.status,
+        timeout: data.timeout,
+        pos: 'top-center'
+      });
+    }).fail((jqXHR, textStatus) => {
+      UIkit.notify(textStatus, 'danger');
+    });
+  }, () => {
+    revert();
+  }, <any>{
+    labels: {
+      Cancel: calendarEl.dataset.confirmChangeCancel,
+      Ok: calendarEl.dataset.confirmChangeOk
+    }
+  });
+}
+
+function renderEvent(calendarEl: HTMLElement, event: EventApi): string {
+  let titleDisplay: string = '';
+  if (event.extendedProps.contextTitle != '') {
+    titleDisplay = ' / ' + event.extendedProps.contextTitle;
+  }
+
+  const translations = JSON.parse(calendarEl.dataset.translations);
+
+  let recurringDescription: string = '';
+  if (event.extendedProps.recurringDescription != '') {
+    recurringDescription = '<tr>'
+      + '<td>' + translations.recurringDate + ':</td>'
+      + '<td>' + event.extendedProps.recurringDescription + '</td>'
+      + '</tr>';
+  }
+
+  return '<div class="uk-grid">'
+    + '<table>'
+    + '<tr>'
+    + '<td colspan="2"><b>' + event.title + '</b></td>'
+    + '</tr>'
+    + '<tr>'
+    + '<td>' + translations.date + ':</td>'
+    + '<td>' + event.extendedProps.description + '</td>'
+    + '</tr>'
+    + recurringDescription
+    + '<tr>'
+    + '<td>' + translations.place + ':</td>'
+    + '<td>' + event.extendedProps.place + '</td>'
+    + '</tr>'
+    + '<tr>'
+    + '<td>' + translations.participants + ':</td>'
+    + '<td>' + event.extendedProps.participants + '</td>'
+    + '</tr>'
+    + '<tr>'
+    + '<td>' + translations.calendar + ':</td>'
+    + '<td>' + event.extendedProps.calendar + titleDisplay + '</td>'
+    + '</tr>'
+    + '</table>'
+    + '</div>';
+}
