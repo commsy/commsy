08ae0009a9fd8daa45006059aa246ddae2452f34
Christoph Schönfeld <schoenfeld@effective-webwork.de>
2023-09-13 13:47:51 +0200

    #5019 - missing space between firstname and lastname

--- src/Mail/Messages/AccountActivityDeletedMessage.php
+++ src/Mail/Messages/AccountActivityDeletedMessage.php
@@ -56,7 +56,7 @@ class AccountActivityDeletedMessage extends Message
         return [
             'hello' => $legacyTranslator->getEmailMessage(
                 'MAIL_BODY_HELLO',
-                $this->account->getFirstname().$this->account->getLastname()
+                "{$this->account->getFirstname()} {$this->account->getLastname()}"
             ),
             'content' => $legacyTranslator->getEmailMessage(
                 'EMAIL_INACTIVITY_DELETE_NOW_BODY',
