a6834eb9f2c01b28012f048f62cb32642f589ebb
Christoph Schönfeld <schoenfeld@effective-webwork.de>
2024-11-06 14:21:58 +0100

    #5241 - duplicated wopisrc parameter

--- src/WOPI/ActionUrlBuilder.php
+++ src/WOPI/ActionUrlBuilder.php
@@ -105,7 +105,7 @@ final readonly class ActionUrlBuilder
         preg_match_all('/<(.+?)=(.+?)(&?)>/', $urlSrc, $matches, PREG_SET_ORDER);
 
         // for legacy support ensure there is a WOPI_SOURCE match
-        $withSourcePart = array_search(fn (array $match) => $match[2] === 'WOPI_SOURCE', $matches);
+        $withSourcePart = array_filter($matches, fn (array $value) => $value[2] === 'WOPI_SOURCE');
         if (!$withSourcePart) {
             $matches[] = ['<wopisrc=WOPI_SOURCE&>', 'wopisrc', 'WOPI_SOURCE', '&'];
         }
--- tests/Unit/WOPI/WOPIActionUrlBuilderTest.php
+++ tests/Unit/WOPI/WOPIActionUrlBuilderTest.php
@@ -5,6 +5,7 @@ namespace Tests\Unit\WOPI;
 
 use App\WOPI\ActionUrlBuilder;
 use Codeception\Test\Unit;
+use DG\BypassFinals;
 use Tests\Support\UnitTester;
 
 class WOPIActionUrlBuilderTest extends Unit
@@ -14,6 +15,7 @@ class WOPIActionUrlBuilderTest extends Unit
 
     protected function _before()
     {
+        BypassFinals::enable();
     }
 
     public function testLegacyWopiSourceMissing()
@@ -30,5 +32,22 @@ class WOPIActionUrlBuilderTest extends Unit
             ->build($legacy365WithoutSource);
 
         $this->assertStringContainsString('wopisrc=https://host/wopi/files/abcef123', urldecode($actionUrl));
+        $this->assertEquals(1, substr_count(urldecode($actionUrl), 'wopisrc'));
+    }
+
+    public function testWopiSourceCount()
+    {
+        /**
+         * Check that there is only a single instance of the wopisrc parameter
+         */
+        $onlyOfficeWithSource = 'https://host/hosting/wopi/cell/edit?<rs=DC_LLCC&><dchat=DISABLE_CHAT&><embed=EMBEDDED&><fs=FULLSCREEN&><hid=HOST_SESSION_ID&><rec=RECORDING&><sc=SESSION_CONTEXT&><thm=THEME_ID&><ui=UI_LLCC&><wopisrc=WOPI_SOURCE&>&';
+
+        $actionUrlBuilder = new ActionUrlBuilder();
+        $actionUrl = $actionUrlBuilder
+            ->setWOPISource('https://host/wopi/files/abcef123')
+            ->build($onlyOfficeWithSource);
+
+        $this->assertStringContainsString('wopisrc=https://host/wopi/files/abcef123', urldecode($actionUrl));
+        $this->assertEquals(1, substr_count(urldecode($actionUrl), 'wopisrc'));
     }
 }
