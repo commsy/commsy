656a4720fdf909bfc87738364598895b8a66c4e8
Christoph Schönfeld <schoenfeld@effective-webwork.de>
2024-11-27 17:27:39 +0100

    test execution fails because of database table migration

--- migrations/2024/Version20241004141443.php
+++ migrations/2024/Version20241004141443.php
@@ -19,6 +19,11 @@ final class Version20241004141443 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
+        $schemaManager = $this->connection->createSchemaManager();
+        $tableNames = $schemaManager->listTableNames();
+
+        $this->skipIf(!in_array('messenger_messages', $tableNames), 'Table "messenger_messages" does not exist.');
+
         $this->addSql('ALTER TABLE messenger_messages CHANGE queue_name queue_name VARCHAR(190) NOT NULL, CHANGE created_at created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', CHANGE available_at available_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', CHANGE delivered_at delivered_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
         $this->addSql('CREATE INDEX IDX_75EA56E0FB7336F0 ON messenger_messages (queue_name)');
         $this->addSql('CREATE INDEX IDX_75EA56E0E3BD61CE ON messenger_messages (available_at)');
