3d544e631004f8971ec4e2d6c78b82da02e3e7cc
Christoph Schönfeld <schoenfeld@effective-webwork.de>
2025-03-28 10:27:26 +0100

    migration renamed

--- migrations/2025/Version20250328092623.php
+++ migrations/2025/Version20250328092623.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250130131207 extends AbstractMigration
+final class Version20250328092623 extends AbstractMigration
 {
     public function getDescription(): string
     {
