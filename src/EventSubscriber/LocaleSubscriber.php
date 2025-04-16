b3d878d9ade6406ea8a2578ff203c4580a113851
Christoph Schönfeld <schoenfeld@effective-webwork.de>
2025-04-16 17:10:34 +0200

    #5316 - error when sending mails to a recipient with "browser" locale

--- src/EventSubscriber/LocaleSubscriber.php
+++ src/EventSubscriber/LocaleSubscriber.php
@@ -15,7 +15,7 @@ namespace App\EventSubscriber;
 
 use App\Proxy\PortalProxy;
 use App\Services\LegacyEnvironment;
-use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
@@ -23,7 +23,10 @@ use Symfony\Component\HttpKernel\KernelEvents;
 final readonly class LocaleSubscriber implements EventSubscriberInterface
 {
     public function __construct(
-        private ParameterBagInterface $parameterBag,
+        #[Autowire(param: 'locale')]
+        private string $defaultLocale,
+        #[Autowire(param: 'kernel.enabled_locales')]
+        private array $enabledLocales,
         private LegacyEnvironment $legacyEnvironment
     ) {
     }
@@ -37,7 +40,7 @@ final readonly class LocaleSubscriber implements EventSubscriberInterface
 
         // If no explicit locale has been set on this request, use one from the session.
         // The value in the session is the user's preference from the account table.
-        $resolvedLocale = $request->getSession()->get('_locale', $this->parameterBag->get('locale'));
+        $resolvedLocale = $request->getSession()->get('_locale', $this->defaultLocale);
 
         // The locale might be enforced by a workspace
         $contextItem = $this->legacyEnvironment->getEnvironment()->getCurrentContextItem();
@@ -49,8 +52,7 @@ final readonly class LocaleSubscriber implements EventSubscriberInterface
 
         // Locale might be 'browser'. In this case we try to get preferred languages from the Accept-Language header.
         if ($resolvedLocale === 'browser') {
-            $enabledLocales = $this->parameterBag->get('kernel.enabled_locales');
-            $resolvedLocale = $request->getPreferredLanguage($enabledLocales);
+            $resolvedLocale = $request->getPreferredLanguage($this->enabledLocales);
         }
 
         $request->setLocale(mb_strtolower($resolvedLocale));
