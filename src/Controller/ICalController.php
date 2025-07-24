d795367e73bfad595c187c7ed4cea69b172a52c5
Christoph Schönfeld <schoenfeld@effective-webwork.de>
2025-07-24 16:14:00 +0200

    fix wrong exception thrown

--- src/Controller/ICalController.php
+++ src/Controller/ICalController.php
@@ -45,7 +45,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\ResponseHeaderBag;
-use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Contracts\Service\Attribute\Required;
@@ -102,7 +101,7 @@ class ICalController extends AbstractController
         }
 
         if (!$granted) {
-            throw new AccessDeniedHttpException();
+            throw $this->createAccessDeniedException();
         }
 
         if ($request->query->has('hid')) {
