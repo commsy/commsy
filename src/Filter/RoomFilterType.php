d619d856bf9be63607519fe6b87a06d836c39ec9
Matthias Steffens <mat@extracts.de>
2025-08-01 16:07:01 +0200

    #5345 Fix "All rooms" filter option "Hide templates" which returned zero results instead of all non-template rooms

--- src/Filter/RoomFilterType.php
+++ src/Filter/RoomFilterType.php
@@ -110,7 +110,7 @@ class RoomFilterType extends AbstractType
                     $qb = $filterQuery->getQueryBuilder();
                     $qb
                         ->andWhere('r.template = :template')
-                        ->setParameter('template', '-1');
+                        ->setParameter('template', '0');
 
                     return $qb;
                 },
