4338e2af992988f98c159d821b7683e124bf7d78
Christoph Schönfeld <schoenfeld@effective-webwork.de>
2025-08-05 17:14:33 +0200

    #5347 - workaround for https://github.com/symfony/ux/pull/2397

--- src/Form/Type/Profile/RoomProfileGeneralType.php
+++ src/Form/Type/Profile/RoomProfileGeneralType.php
@@ -42,16 +42,22 @@ class RoomProfileGeneralType extends AbstractType
                     ]);
                 }
             })
-            ->addDependent('crop', 'useProfileImage', function (DependentField $field, bool $checked) use ($options) {
-                if ($checked && file_exists($options['cropPath'])) {
-                    $field->add(CropperType::class, [
-                        'public_url' => $options['cropPublicUrl'],
-                        'cropper_options' => [
-                            'aspectRatio' => 1,
-                            'preview' => '#cropper-preview',
-                        ],
-                    ]);
-                }
+            ->addDependent('crop', 'useProfileImage', function (DependentField $field, ?bool $checked) use ($options) {
+                /**
+                 * Workaround for https://github.com/symfony/ux/pull/2397
+                 * We always add the field for now and just control the visibility.
+                 */
+//                if (!$checked || !file_exists($options['cropPath'])) {
+//                    return;
+//                }
+
+                $field->add(CropperType::class, [
+                    'public_url' => $options['cropPublicUrl'],
+                    'cropper_options' => [
+                        'aspectRatio' => 1,
+                        'preview' => '#cropper-preview',
+                    ],
+                ]);
             })
             ->add('imageChangeInAllContexts', CheckboxType::class, [
                 'label' => 'changeInAllContexts',
