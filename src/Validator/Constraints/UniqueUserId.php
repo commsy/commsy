5de0c430560dd1d1414b54785a5a2ac3efb94e02
Christoph Schönfeld <schoenfeld@effective-webwork.de>
2025-12-18 16:08:49 +0100

    deprecation fixed

--- src/Validator/Constraints/UniqueUserId.php
+++ src/Validator/Constraints/UniqueUserId.php
@@ -18,16 +18,18 @@ use Symfony\Component\Validator\Exception\MissingOptionsException;
 
 class UniqueUserId extends Constraint
 {
-    public $portalId;
+    public int $portalId;
+    public string $message = 'A user id with the same name already exists.';
 
-    public $message = 'A user id with the same name already exists.';
-
-    public function __construct($options = null)
+    public function __construct(?int $portalId = null, ?string $message = null, ?array $groups = null, mixed $payload = null)
     {
-        parent::__construct($options);
-
-        if (null === $this->portalId) {
+        if ($portalId === null) {
             throw new MissingOptionsException(sprintf('Option "portalId" must be given for constraint %s', self::class), ['portalId']);
         }
+
+        $this->portalId = $portalId;
+        $this->message = $message ?? $this->message;
+
+        parent::__construct(null, $groups, $payload);
     }
 }
